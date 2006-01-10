3633959ff5a81bdb203413e7364ca8a7f50b6002
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-01-10 14:30:31 +0000

    - add more pages for main navigation area

---
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/ShowMapsPortlet.java
@@ -0,0 +1,52 @@
+package de.ingrid.portal.portlets;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
+import javax.portlet.PortletSession;
+
+import org.apache.portals.bridges.velocity.GenericVelocityPortlet;
+import org.apache.velocity.context.Context;
+
+import de.ingrid.portal.search.PageState;
+
+
+
+public class ShowMapsPortlet extends GenericVelocityPortlet
+{
+    public void init(PortletConfig config) throws PortletException
+    {
+        super.init(config);
+    }    
+    public void doView(javax.portlet.RenderRequest request, javax.portlet.RenderResponse response) throws PortletException, IOException
+    {
+    	Context context = getContext(request);
+        
+        super.doView(request, response);
+    }
+    public void processAction(ActionRequest request, ActionResponse actionResponse) throws PortletException, IOException
+    {
+        String action = request.getParameter("action");
+
+    	PortletSession session = request.getPortletSession();
+    	PageState ps = (PageState) session.getAttribute("show_maps_portlet_page_state");
+    	if (ps == null) {
+    		ps = new PageState(this.getClass().getName());
+    		ps = initPageState(ps);
+    		session.setAttribute("show_maps_portlet_page_state", ps);
+    	}
+        
+        if (action == null) {
+        	return;
+        }
+    }    
+    
+    
+    private PageState initPageState(PageState ps) {
+		return ps;
+    }
+    
+}
