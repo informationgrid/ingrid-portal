ed1f671d991f63c59827e78f2bb2481a6e842e86
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-02-11 21:16:02 +0000

    - fix some issues with the SNS anniversary (accept dates specifies only by a year)

---
+++ ingrid-portal-apps/src/java/de/ingrid/portal/global/DateUtil.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 1997-2006 by wemove GmbH
+ */
+package de.ingrid.portal.global;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.ingrid.portal.portlets.ChronicleTeaserPortlet;
+
+/**
+ * This class contains date conversion helper functions.
+ *
+ * @author joachim@wemove.com
+ */
+public class DateUtil {
+
+    private final static Log log = LogFactory.getLog(ChronicleTeaserPortlet.class);
+    
+    
+    /**
+     * Parses a string for a date pattern (yyyy-mm-dd or yyyy) to the local date
+     * representation of the date.
+     * 
+     * @param dateStr The date string.
+     * @param locale The Locale.
+     * @return The localized date string.
+     */
+    public static String parseDateToLocale(String dateStr, Locale locale) {
+        String result = null;
+        SimpleDateFormat df;
+        SimpleDateFormat portalFormat;
+        if (dateStr == null)
+            return result;
+        if (dateStr.matches("[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]")) {
+            try {
+                df = new SimpleDateFormat("yyyy-MM-dd");
+                portalFormat = new SimpleDateFormat("d MMM yyyy", locale);
+                result = portalFormat.format(df.parse((String) dateStr));
+            } catch (ParseException e) {
+                log.warn("error parsing from date.", e);
+            }
+        } else if (dateStr.matches("[0-9][0-9][0-9][0-9]")) {
+            try {
+                df = new SimpleDateFormat("yyyy");
+                portalFormat = new SimpleDateFormat("yyyy", locale);
+                result = portalFormat.format(df.parse((String) dateStr));
+            } catch (ParseException e) {
+                log.warn("error parsing from date.", e);
+            }
+        } 
+        return result;
+    }
+    
+    /** Returns the years between dateStrFrom and dateStringTo. Both can have the formats
+     * yyyy-mm-dd or yyyy.
+     * 
+     * @param dateStrFrom The String representing from.
+     * @param dateStrTo The String representing to.
+     * @return The years between the dates.
+     */
+    public static int yearsBetween(String dateStrFrom, String dateStrTo) {
+        Date from = DateUtil.parseDateString(dateStrFrom);
+        Date to = DateUtil.parseDateString(dateStrTo);
+        if (from != null && to != null) {
+            Calendar cal = new GregorianCalendar();
+            cal.setTime(from);
+            int fromYear = cal.get(Calendar.YEAR);
+            cal.setTime(to);
+            int toYear = cal.get(Calendar.YEAR);
+            return toYear - fromYear;
+        } else {
+            return -1;
+        }
+    }
+    
+    /**
+     * Parses a date string with the format 'yyyy-mm-dd' of 'yyyy'
+     * to a Date.
+     * 
+     * @param dateString The String representing the date.
+     * 
+     * @return The Date.
+     */
+    public static Date parseDateString(String dateString) {
+        Date result = null;
+        SimpleDateFormat df;
+        if (dateString.matches("[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]")) {
+            try {
+                df = new SimpleDateFormat("yyyy-MM-dd");
+                result = df.parse((String) dateString);
+            } catch (ParseException e) {
+                log.warn("error parsing from date.", e);
+            }
+        } else if (dateString.matches("[0-9][0-9][0-9][0-9]")) {
+            try {
+                df = new SimpleDateFormat("yyyy");
+                result = df.parse((String) dateString);
+            } catch (ParseException e) {
+                log.warn("error parsing from date.", e);
+            }
+        }
+        return result;
+    }
+
+}
--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/ChronicleTeaserPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/ChronicleTeaserPortlet.java
@@ -1,7 +1,6 @@
 package de.ingrid.portal.portlets;
 
 import java.io.IOException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -11,17 +10,14 @@ import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletException;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.portals.bridges.velocity.AbstractVelocityMessagingPortlet;
 import org.apache.velocity.context.Context;
 
 import de.ingrid.iplug.sns.utils.DetailedTopic;
+import de.ingrid.portal.global.DateUtil;
 import de.ingrid.portal.interfaces.impl.SNSInterfaceImpl;
 
 public class ChronicleTeaserPortlet extends AbstractVelocityMessagingPortlet {
-
-    private final static Log log = LogFactory.getLog(ChronicleTeaserPortlet.class);
 
     public void init(PortletConfig config) throws PortletException {
         // set our message "scope" for inter portlet messaging
@@ -39,29 +35,15 @@ public class ChronicleTeaserPortlet extends AbstractVelocityMessagingPortlet {
         HashMap result = new HashMap();
         if (detail != null) {
             result.put("title", detail.get("topicName"));
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-            SimpleDateFormat portalFormat = new SimpleDateFormat("d MMM yyyy", request.getLocale());
-            Date untilDate = null;
-            Date fromDate = null;
             if (detail.get("from") != null) {
-                try {
-                    fromDate = df.parse((String) detail.get("from"));
-                    result.put("from", portalFormat.format(fromDate));
-                } catch (ParseException e) {
-                    log.warn("error parsing from date.", e);
-                }
+                result.put("from", DateUtil.parseDateToLocale(detail.get("from").toString(), request.getLocale()));
             }
             if (detail.get("until") != null) {
-                try {
-                    untilDate = df.parse((String) detail.get("until"));
-                    result.put("until", portalFormat.format(untilDate));
-                } catch (ParseException e) {
-                    log.warn("error parsing until date.", e);
-                }
+                result.put("until", DateUtil.parseDateToLocale(detail.get("until").toString(), request.getLocale()));
             }
             result.put("topicId", detail.get("topicId"));
-            if (fromDate != null) {
-                int years = new Date().getYear() - fromDate.getYear();
+            if (detail.get("from") != null) {
+                int years = DateUtil.yearsBetween(detail.get("from").toString(), new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
                 result.put("years", new Integer(years));
             }
         }
