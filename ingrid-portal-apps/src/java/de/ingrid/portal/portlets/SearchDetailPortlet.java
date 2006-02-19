dd24a1c1e238573fbe962dc4563deee55ad755d5
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-02-19 15:30:54 +0000

    - add more flexible date field formating

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/SearchDetailPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/SearchDetailPortlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Locale;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -32,10 +33,19 @@ public class SearchDetailPortlet extends GenericVelocityPortlet
     private final static String TEMPLATE_DETAIL_GENERIC = "/WEB-INF/templates/search_detail_generic.vm";
     private final static String TEMPLATE_ECS = "/WEB-INF/templates/search_detail.vm";
     
+    // ecs fields that represent a date, used for date parsing and formating
+    private ArrayList dateFields = new ArrayList();
+    
     
     public void init(PortletConfig config) throws PortletException
     {
         super.init(config);
+
+        dateFields.add("t01_object.mod_time");
+        dateFields.add("t01_object.time_to");
+        dateFields.add("t01_object.time_from");
+        dateFields.add("t0113_dataset_reference.reference_date");
+        
     }    
     public void doView(javax.portlet.RenderRequest request, javax.portlet.RenderResponse response) throws PortletException, IOException
     {
@@ -58,17 +68,19 @@ public class SearchDetailPortlet extends GenericVelocityPortlet
             Record record = ibus.getRecord(hit);
             context.put("record", record);
             HashMap recordMap = new HashMap();
-
+            
             // serch for column
             Column[] columns = record.getColumns();
             for (int i = 0; i < columns.length; i++) {
                 if (record.getValueAsString(columns[i]).trim().length() > 0) {
-                    recordMap.put(columns[i].getTargetName().toLowerCase(), record.getValueAsString(columns[i]).trim());
+                    if (dateFields.contains(columns[i].getTargetName().toLowerCase())) {
+                        recordMap.put(columns[i].getTargetName().toLowerCase(), DateUtil.parseDateToLocale(record.getValueAsString(columns[i]).trim(), request.getLocale()));
+                    } else {
+                        recordMap.put(columns[i].getTargetName().toLowerCase(), record.getValueAsString(columns[i]).trim());
+                    }
                 }
             }
-            addSubRecords(record, recordMap);
-            
-            recordMap.put("t01_object.mod_time", DateUtil.parseDateString((String)recordMap.get("t01_object.mod_time")));
+            addSubRecords(record, recordMap, request.getLocale());
             
             context.put("rec", recordMap);
         } catch(Throwable t){
@@ -83,12 +95,12 @@ public class SearchDetailPortlet extends GenericVelocityPortlet
 
     }
     
-    private void addSubRecords(Record record, HashMap map) {
-        addSubRecords(record, map, 0);
+    private void addSubRecords(Record record, HashMap map, Locale locale) {
+        addSubRecords(record, map, locale, 0);
     }
     
     
-    private void addSubRecords(Record record, HashMap map, int level) {
+    private void addSubRecords(Record record, HashMap map, Locale locale, int level) {
         level++;
         Column[] columns;
         ArrayList subRecordList;
@@ -99,7 +111,11 @@ public class SearchDetailPortlet extends GenericVelocityPortlet
             columns = subRecords[i].getColumns();
             for (int j = 0; j < columns.length; j++) {
                 if (subRecords[i].getValueAsString(columns[j]).trim().length() > 0) {
-                    subRecordMap.put(columns[j].getTargetName().toLowerCase(), subRecords[i].getValueAsString(columns[j]).trim());
+                    if (dateFields.contains(columns[j].getTargetName().toLowerCase())) {
+                        subRecordMap.put(columns[j].getTargetName().toLowerCase(), DateUtil.parseDateToLocale(subRecords[i].getValueAsString(columns[j]).trim(), locale));
+                    } else {
+                        subRecordMap.put(columns[j].getTargetName().toLowerCase(), subRecords[i].getValueAsString(columns[j]).trim());
+                    }
                 }
             }
             String targetName = columns[0].getTargetName().toLowerCase();
@@ -111,7 +127,7 @@ public class SearchDetailPortlet extends GenericVelocityPortlet
             }
             subRecordList.add(subRecordMap);
             // add subrecords
-            addSubRecords(subRecords[i], subRecordMap, level);
+            addSubRecords(subRecords[i], subRecordMap, locale, level);
         }
         
     }
