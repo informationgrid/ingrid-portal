bf2be85aeb17070fe9a2af7fd625b1697ef9d4a9
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-03 09:19:15 +0000

    - increase timeout - fix testcase

--- ingrid-portal-apps/src/test/de/ingrid/portal/search/net/ThreadedQueryControllerTest.java
+++ ingrid-portal-apps/src/test/de/ingrid/portal/search/net/ThreadedQueryControllerTest.java
@@ -2,10 +2,11 @@ package de.ingrid.portal.search.net;
 
 import java.util.HashMap;
 
+import junit.framework.TestCase;
 import de.ingrid.utils.query.IngridQuery;
 import de.ingrid.utils.queryparser.ParseException;
 import de.ingrid.utils.queryparser.QueryStringParser;
-import junit.framework.TestCase;
+import de.ingrid.portal.search.net.ThreadedQueryController;
 
 public class ThreadedQueryControllerTest extends TestCase {
 
@@ -28,12 +29,13 @@ public class ThreadedQueryControllerTest extends TestCase {
             controller.addQuery("1", qd);
             query = QueryStringParser.parse("wasser ranking:off");
             qd = new QueryDescriptor(query, 10, 1, 10, 5000, true, null);
+            controller.setTimeout(10000);
             controller.addQuery("2", qd);
             
             HashMap map = controller.search();
-            assertEquals(2, map.size());
-            assertNotNull(map.get("1"));
-            assertNotNull(map.get("2"));
+            assertEquals("expected hashmap size: 2, actual size: " + map.size(), 2, map.size());
+            assertNotNull("map entry is null for map entry 1", map.get("1"));
+            assertNotNull("map entry is null for map entry 2", map.get("2"));
         } catch (ParseException e) {
             e.printStackTrace();
         }
