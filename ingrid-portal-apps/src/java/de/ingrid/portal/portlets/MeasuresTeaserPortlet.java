611252ec7704e7b184d94a5f05e20a82b354a8fa
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-06 12:07:08 +0000

    - added MeasuresTeaserPortlet which calls Measures page now via Action method to guarantee URL rewriting (+ generates URL params for bookmarking)

---
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/MeasuresTeaserPortlet.java
@@ -0,0 +1,44 @@
+package de.ingrid.portal.portlets;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
+
+import org.apache.portals.bridges.velocity.GenericVelocityPortlet;
+import org.apache.velocity.context.Context;
+
+import de.ingrid.portal.global.IngridResourceBundle;
+import de.ingrid.portal.global.Settings;
+import de.ingrid.portal.search.SearchState;
+
+public class MeasuresTeaserPortlet extends GenericVelocityPortlet {
+
+    public final static String TITLE_KEY = "teaser.measures.title";
+
+    public void init(PortletConfig config) throws PortletException {
+        super.init(config);
+    }
+
+    public void doView(javax.portlet.RenderRequest request, javax.portlet.RenderResponse response)
+            throws PortletException, IOException {
+        Context context = getContext(request);
+
+        IngridResourceBundle messages = new IngridResourceBundle(getPortletConfig().getResourceBundle(
+                request.getLocale()));
+        context.put("MESSAGES", messages);
+
+        response.setTitle(messages.getString(TITLE_KEY));
+
+        super.doView(request, response);
+    }
+
+    public void processAction(ActionRequest request, ActionResponse actionResponse) throws PortletException,
+            IOException {
+
+        // redirect to our page with URL parameters for bookmarking
+        actionResponse.sendRedirect(Settings.PAGE_MEASURES + SearchState.getURLParamsMeasures(request, null));
+    }
+}
