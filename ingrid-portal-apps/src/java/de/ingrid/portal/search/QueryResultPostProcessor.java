9565a2bd84b34cfbe4ed6b23c35d8ea0ec5f9159
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-09 18:52:54 +0000

    - added further checks and try catch clause in post processing of ranked hits !

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/QueryResultPostProcessor.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/QueryResultPostProcessor.java
@@ -27,50 +27,67 @@ public class QueryResultPostProcessor {
         try {
             IngridHit[] hitArray = hits.getHits();
             String tmpString = null;
+            IngridHit hit = null;
+            IngridHitDetail detail = null;
 
             for (int i = 0; i < hitArray.length; i++) {
-                IngridHit hit = hitArray[i];
-                IngridHitDetail detail = (IngridHitDetail) hit.get("detail");
-                UtilsSearch.transferHitDetails(hit, detail);
-                tmpString = detail.getIplugClassName();
-                if (tmpString == null) {
-                    tmpString = "";
+                try {
+                    hit = hitArray[i];
+                    detail = (IngridHitDetail) hit.get("detail");
+
+                    // if no detail, skip processing OF THIS HIT !
+                    if (detail == null) {
+                        if (log.isErrorEnabled()) {
+                            log.error("Hit Detail is NULL !!!!!!!!!!!!!!!!! hit=" + hit + ", detail=" + detail);
+                        }
+                        continue;
+                    }
+                    
+                    UtilsSearch.transferHitDetails(hit, detail);
+                    tmpString = detail.getIplugClassName();
+                    if (tmpString == null) {
+                        tmpString = "";
+                        if (log.isErrorEnabled()) {
+                            log.error("Hit Detail has no IplugClassName !!!! hit=" + hit + ", detail=" + detail);
+                        }
+                    }
+
+                    if (tmpString.equals("de.ingrid.iplug.dsc.index.DSCSearcher")) {
+                        hit.put(Settings.RESULT_KEY_TYPE, "dsc");
+
+                        // read for all dsc iplugs
+                        tmpString = UtilsSearch.getDetailValue(detail, Settings.HIT_KEY_WMS_URL);
+                        if (tmpString.length() > 0) {
+                            try {
+                                hit.put(Settings.RESULT_KEY_WMS_URL, URLEncoder.encode(tmpString, "UTF-8"));
+                            } catch (UnsupportedEncodingException e) {
+                                log.error("Error url encoding wms URL!", e);
+                            }
+                        }
+
+                        // check our selected "data source" and read
+                        // according data
+                        if (ds.equals(Settings.PARAMV_DATASOURCE_ENVINFO)) {
+                            tmpString = UtilsSearch.getDetailValue(detail, Settings.HIT_KEY_UDK_CLASS);
+                            if (tmpString.length() > 0) {
+                                hit.put(Settings.RESULT_KEY_UDK_CLASS, tmpString);
+                            }
+                        } else if (ds.equals(Settings.PARAMV_DATASOURCE_ADDRESS)) {
+                            tmpString = UtilsSearch.getDetailValue(detail, Settings.HIT_KEY_ADDRESS_CLASS);
+                            if (tmpString.length() > 0) {
+                                // add as udk class, template renders correctly
+                                hit.put(Settings.RESULT_KEY_UDK_CLASS, tmpString);
+                            }
+                        }
+                    } else if (tmpString.equals("de.ingrid.iplug.se.NutchSearcher")) {
+                        hit.put(Settings.RESULT_KEY_TYPE, "nutch");
+                    } else {
+                        hit.put(Settings.RESULT_KEY_TYPE, "unknown");
+                    }
+                } catch (Exception ex) {
                     if (log.isErrorEnabled()) {
-                        log.error("Hit Detail has no IplugClassName !!!! hit=" + hit + ", detail=" + detail);
+                        log.error("Problems processing Hit ! hit = " + hit + ", detail=" + detail, ex);
                     }
-                }
-
-                if (tmpString.equals("de.ingrid.iplug.dsc.index.DSCSearcher")) {
-                    hit.put(Settings.RESULT_KEY_TYPE, "dsc");
-
-                    // read for all dsc iplugs
-                    tmpString = UtilsSearch.getDetailValue(detail, Settings.HIT_KEY_WMS_URL);
-                    if (tmpString.length() > 0) {
-                        try {
-                            hit.put(Settings.RESULT_KEY_WMS_URL, URLEncoder.encode(tmpString, "UTF-8"));
-                        } catch (UnsupportedEncodingException e) {
-                            log.error("Error url encoding wms URL!", e);
-                        }
-                    }
-
-                    // check our selected "data source" and read
-                    // according data
-                    if (ds.equals(Settings.PARAMV_DATASOURCE_ENVINFO)) {
-                        tmpString = UtilsSearch.getDetailValue(detail, Settings.HIT_KEY_UDK_CLASS);
-                        if (tmpString.length() > 0) {
-                            hit.put(Settings.RESULT_KEY_UDK_CLASS, tmpString);
-                        }
-                    } else if (ds.equals(Settings.PARAMV_DATASOURCE_ADDRESS)) {
-                        tmpString = UtilsSearch.getDetailValue(detail, Settings.HIT_KEY_ADDRESS_CLASS);
-                        if (tmpString.length() > 0) {
-                            // add as udk class, template renders correctly
-                            hit.put(Settings.RESULT_KEY_UDK_CLASS, tmpString);
-                        }
-                    }
-                } else if (tmpString.equals("de.ingrid.iplug.se.NutchSearcher")) {
-                    hit.put(Settings.RESULT_KEY_TYPE, "nutch");
-                } else {
-                    hit.put(Settings.RESULT_KEY_TYPE, "unknown");
                 }
             }
         } catch (Exception ex) {
