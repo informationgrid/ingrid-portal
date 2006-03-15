2374fcacfe558c95e2cbeee283069955a1dd496f
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-15 13:34:30 +0000

    - add user attribute access (salutation at myportal overview)

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/MyPortalOverviewPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/MyPortalOverviewPortlet.java
@@ -3,7 +3,20 @@
  */
 package de.ingrid.portal.portlets;
 
+import java.io.IOException;
+import java.util.prefs.Preferences;
+
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.apache.jetspeed.CommonPortletServices;
+import org.apache.jetspeed.security.SecurityException;
+import org.apache.jetspeed.security.User;
+import org.apache.jetspeed.security.UserManager;
 import org.apache.portals.bridges.velocity.AbstractVelocityMessagingPortlet;
+import org.apache.velocity.context.Context;
 
 /**
  * TODO Describe your created type (class, etc.) here.
@@ -11,5 +24,40 @@ import org.apache.portals.bridges.velocity.AbstractVelocityMessagingPortlet;
  * @author joachim@wemove.com
  */
 public class MyPortalOverviewPortlet extends AbstractVelocityMessagingPortlet {
+
+    private UserManager userManager;
+    
+    /**
+     * @see org.apache.portals.bridges.velocity.GenericVelocityPortlet#init(javax.portlet.PortletConfig)
+     */
+    public void init(PortletConfig config) throws PortletException {
+        super.init(config);
+        
+        userManager = (UserManager)getPortletContext().getAttribute(CommonPortletServices.CPS_USER_MANAGER_COMPONENT);
+        if (null == userManager)
+        {
+            throw new PortletException("Failed to find the User Manager on portlet initialization");
+        }
+    }
+
+    /**
+     * @see org.apache.portals.bridges.velocity.GenericVelocityPortlet#doView(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
+     */
+    public void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException {
+        Context context = getContext(request);
+        String userName = request.getUserPrincipal().getName();
+        User user = null;
+        try {
+            user = userManager.getUser(userName);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        
+        if (user != null) {
+            context.put("userAttributes", user.getUserAttributes());
+        }
+        
+        super.doView(request, response);
+    }
 
 }
