4db5d18574d9f064f68fa64a2ec440484b85a6be
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-16 18:29:38 +0000

    - added wizard portlet and templates to search-ext-env-area-contents page

---
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/SearchExtEnvArea.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2006 wemove digital solutions. All rights reserved.
+ */
+package de.ingrid.portal.portlets;
+
+import java.io.IOException;
+
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+/**
+ * This portlet is the abstract base class of all "wizard" portlets in the EnvironmentInfo/Searcharea
+ * tab of the extended search. Encapsulates common stuff.
+ *
+ * @author martin@wemove.com
+ */
+abstract class SearchExtEnvArea extends SearchExtEnvBase {
+
+    // TAB values from action request (request parameter)
+
+    protected final static String PARAMV_TAB_CONTENTS = "5";
+
+    protected final static String PARAMV_TAB_SOURCES = "6";
+
+    protected final static String PARAMV_TAB_PARTNER = "7";
+
+    // PAGES FOR TABS
+
+    protected final static String PAGE_CONTENTS = "/ingrid-portal/portal/search-extended/search-ext-env-area-contents.psml";
+
+    protected final static String PAGE_SOURCES = "/ingrid-portal/portal/search-extended/search-ext-env-area-sources.psml";
+
+    protected final static String PAGE_PARTNER = "/ingrid-portal/portal/search-extended/search-ext-env-area-partner.psml";
+
+    protected void processTab(ActionResponse actionResponse, String tab) throws PortletException, IOException {
+        if (tab.equals(PARAMV_TAB_CONTENTS)) {
+            actionResponse.sendRedirect(PAGE_CONTENTS);
+        } else if (tab.equals(PARAMV_TAB_SOURCES)) {
+            actionResponse.sendRedirect(PAGE_SOURCES);
+        } else if (tab.equals(PARAMV_TAB_PARTNER)) {
+            actionResponse.sendRedirect(PAGE_PARTNER);
+        } else {
+            processMainTab(actionResponse, tab);
+        }
+    }
+}
---
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/SearchExtEnvAreaContentsPortlet.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2006 wemove digital solutions. All rights reserved.
+ */
+package de.ingrid.portal.portlets;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+import org.apache.velocity.context.Context;
+
+import de.ingrid.portal.global.Settings;
+
+/**
+ * This portlet handles the fragment of "Contents" input in the extended search
+ *
+ * @author martin@wemove.com
+ */
+public class SearchExtEnvAreaContentsPortlet extends SearchExtEnvArea {
+
+    public void doView(javax.portlet.RenderRequest request, javax.portlet.RenderResponse response)
+            throws PortletException, IOException {
+        Context context = getContext(request);
+
+        // set positions in main and sub tab
+        context.put("tab", TAB_AREA);
+        context.put("subtab", PARAMV_TAB_CONTENTS);
+
+        super.doView(request, response);
+    }
+
+    public void processAction(ActionRequest request, ActionResponse actionResponse) throws PortletException,
+            IOException {
+        String action = request.getParameter(Settings.PARAM_ACTION);
+        if (action == null) {
+            action = "";
+        }
+        String submittedAddToQuery = request.getParameter("submitAddToQuery");
+
+        // TODO: implement functionality
+        if (submittedAddToQuery != null) {
+
+            // Zur Suchanfrage hinzufuegen
+
+        } else if (action.equalsIgnoreCase(Settings.PARAMV_ACTION_CHANGE_TAB)) {
+            String newTab = request.getParameter(Settings.PARAM_TAB);
+            processTab(actionResponse, newTab);
+        }
+    }
+}
