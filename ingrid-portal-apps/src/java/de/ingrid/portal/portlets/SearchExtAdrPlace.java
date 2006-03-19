6a9fc71fed80eea7b346049a6e1d43b4e2fe6e55
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-19 19:25:49 +0000

    - added search-ext-adr-place-reference.psml page, has already all UI stuff (portlet, tabs, templates)

---
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/SearchExtAdrPlace.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2006 wemove digital solutions. All rights reserved.
+ */
+package de.ingrid.portal.portlets;
+
+import java.io.IOException;
+
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+/**
+ * This portlet is the abstract base class of all "wizard" portlets in the Address/Place
+ * tab of the extended search. Encapsulates common stuff.
+ *
+ * @author martin@wemove.com
+ */
+abstract class SearchExtAdrPlace extends SearchExtAdr {
+
+    // TAB values from action request (request parameter)
+
+    protected final static String PARAMV_TAB_REFERENCE = "4";
+
+    // PAGES FOR TABS
+
+    protected final static String PAGE_REFERENCE = "/ingrid-portal/portal/search-extended/search-ext-adr-place-reference.psml";
+
+    protected void processTab(ActionResponse actionResponse, String tab) throws PortletException, IOException {
+        if (tab.equals(PARAMV_TAB_REFERENCE)) {
+            actionResponse.sendRedirect(PAGE_REFERENCE);
+        } else {
+            super.processTab(actionResponse, tab);
+        }
+    }
+}
