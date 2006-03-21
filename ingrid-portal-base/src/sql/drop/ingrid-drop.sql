b6a80cd279f3d4920865447520a031d8718e5cf1
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-21 16:46:38 +0000

    - add drops for user management tables

--- ingrid-portal-base/src/sql/drop/ingrid-drop.sql
+++ ingrid-portal-base/src/sql/drop/ingrid-drop.sql
@@ -2,3 +2,8 @@ drop table ingrid_partner;
 drop table ingrid_rss_source;
 drop table ingrid_rss_store;
 drop table ingrid_anniversary;
+
+drop table users;
+drop table roles;
+drop table groups;
+drop table usergrouprole;
