28baa20ecb5f05680fca53c92fdb18f4952603b4
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-22 14:16:25 +0000

    - add test for queryToString Method

---
+++ ingrid-portal-apps/src/test/de/ingrid/portal/search/IngridQueryUtilsTest.java
@@ -0,0 +1,25 @@
+package de.ingrid.portal.search;
+
+import de.ingrid.utils.query.ClauseQuery;
+import de.ingrid.utils.query.FieldQuery;
+import de.ingrid.utils.query.IngridQuery;
+import de.ingrid.utils.query.TermQuery;
+import de.ingrid.utils.queryparser.ParseException;
+import de.ingrid.utils.queryparser.QueryStringParser;
+import junit.framework.TestCase;
+
+public class IngridQueryUtilsTest extends TestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    public void testRenderQuery() throws ParseException {
+        
+        IngridQuery q = QueryStringParser.parse("(gemüse OR garten T02:test) wemove T01:alles datatype:sns");
+        String qStr = UtilsSearch.queryToString(q);
+        assertEquals("wemove (gemüse OR garten t02:test) t01:alles datatype:sns", qStr);
+        
+    }
+
+}
