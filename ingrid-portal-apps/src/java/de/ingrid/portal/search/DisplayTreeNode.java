6d9ae999f2584fb832e5ae9eb4eb7dac5b79a865
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-05-13 09:44:12 +0000

    - refactor DisplayTreeNode to be extended from HashMap (makes it more flexible)

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/DisplayTreeNode.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/DisplayTreeNode.java
@@ -6,8 +6,8 @@
  */
 package de.ingrid.portal.search;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 /**
@@ -16,79 +16,43 @@ import java.util.Iterator;
  * TODO To change the template for this generated type comment go to Window -
  * Preferences - Java - Code Style - Code Templates
  */
-public class DisplayTreeNode implements Serializable {
+public class DisplayTreeNode extends HashMap {
 
     private static final long serialVersionUID = 76464278469123L;
 
-    
     public static final int ROOT = 0;
     public static final int GENERIC = 1;
     public static final int SEARCH_TERM = 2;
     public static final int SNS_TERM = 3;
     
 	
-	/**
-	 * TODO: Comment for <code>name</code>
-	 */
-	private String name;
-
-	/**
-	 * TODO: Comment for <code>isOpen</code>
-	 */
 	private boolean isOpen = false;
-
-    /**
-     * TODO: Comment for <code>isLoading</code>
-     */
     private boolean isLoading = false;
-
-	/**
-	 * TODO: Comment for <code>children</code>
-	 */
-	private ArrayList children = new ArrayList();
-
-	/**
-	 * TODO: Comment for <code>id</code>
-	 */
-	private String id;
-
-	/**
-	 * TODO: Comment for <code>depth</code>
-	 */
-	private int depth;
-    
     private int type;
-    
-    private DisplayTreeNode parent;
-
 
 	/**
 	 * @param n
 	 */
 	public void addChild(DisplayTreeNode n) {
-		children.add(n);
+        getChildren().add(n);
 	}
 
-	public DisplayTreeNode(String id, String name, boolean isOpen, int depth) {
-		this(id, name, isOpen);
-		this.depth = depth;
-	}
 	/**
 	 * @param name
 	 * @param isOpen
 	 */
 	public DisplayTreeNode(String id, String name, boolean isOpen) {
 		super();
-		this.name = name;
-		this.isOpen = isOpen;
-		this.id = id;
+		this.put("name", name);
+        this.put("isOpen", new Boolean(isOpen));
+        this.put("id", id);
 	}
 
 	/**
 	 * @return Returns the isOpen.
 	 */
 	public boolean isOpen() {
-		return isOpen;
+        return this.isOpen;
 	}
 
 	/**
@@ -96,14 +60,14 @@ public class DisplayTreeNode implements Serializable {
 	 *            The isOpen to set.
 	 */
 	public void setOpen(boolean isOpen) {
-		this.isOpen = isOpen;
+        this.isOpen = isOpen;
 	}
 
 	/**
 	 * @return Returns the name.
 	 */
 	public String getName() {
-		return name;
+		return (String)this.get("name");
 	}
 
 	/**
@@ -111,21 +75,24 @@ public class DisplayTreeNode implements Serializable {
 	 *            The name to set.
 	 */
 	public void setName(String name) {
-		this.name = name;
+        this.put("name", name);
 	}
 
 	/**
 	 * @return Returns the children.
 	 */
 	public ArrayList getChildren() {
-		return children;
+        if (get("children") == null) {
+            put("children", new ArrayList());
+        }
+		return (ArrayList)get("children");
 	}
 
 	/**
 	 * @return Returns the id.
 	 */
 	public String getId() {
-		return id;
+		return (String)this.get("id");
 	}
 
 	/**
@@ -133,42 +100,14 @@ public class DisplayTreeNode implements Serializable {
 	 *            The id to set.
 	 */
 	public void setId(String id) {
-		this.id = id;
+        this.put("id", id);
 	}
-
-	/**
-	 * @return Returns the depth.
-	 */
-	public int getDepth() {
-		return depth;
-	}
-
-	/**
-	 * @param depth The depth to set.
-	 */
-	public void setDepth(int depth) {
-		this.depth = depth;
-	}
-
-    /**
-     * @return Returns the type.
-     */
-    public int getType() {
-        return type;
-    }
-
-    /**
-     * @param type The type to set.
-     */
-    public void setType(int type) {
-        this.type = type;
-    }
 
     public DisplayTreeNode getChild(String nodeId) {
         return getChild(this, nodeId);
     }
     
-    private DisplayTreeNode getChild(DisplayTreeNode node, String nodeId) {
+    private static DisplayTreeNode getChild(DisplayTreeNode node, String nodeId) {
         ArrayList c = node.getChildren();
         for (int i=0; i<c.size(); i++) {
             DisplayTreeNode aNode = (DisplayTreeNode)c.get(i);
@@ -184,7 +123,7 @@ public class DisplayTreeNode implements Serializable {
         return getAllChildren(this);
     }
     
-    private ArrayList getAllChildren(DisplayTreeNode node) {
+    private static ArrayList getAllChildren(DisplayTreeNode node) {
         ArrayList result = new ArrayList();
         Iterator it = node.getChildren().iterator();
         while (it != null && it.hasNext()) {
@@ -198,14 +137,14 @@ public class DisplayTreeNode implements Serializable {
      * @return Returns the parent.
      */
     public DisplayTreeNode getParent() {
-        return parent;
+        return (DisplayTreeNode)this.get("parent");
     }
 
     /**
      * @param parent The parent to set.
      */
     public void setParent(DisplayTreeNode parent) {
-        this.parent = parent;
+        this.put("parent", parent);
     }
 
     /**
@@ -221,5 +160,19 @@ public class DisplayTreeNode implements Serializable {
     public void setLoading(boolean isLoading) {
         this.isLoading = isLoading;
     }
-	
+
+    /**
+     * @return Returns the type.
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * @param type The type to set.
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
 }
