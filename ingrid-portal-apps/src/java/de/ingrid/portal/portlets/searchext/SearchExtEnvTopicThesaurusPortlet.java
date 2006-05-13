f15d76a8933a19766839c989ec479e2c22e19d11
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-05-13 09:45:21 +0000

    - first implementation of thesaurus search in extended search

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/searchext/SearchExtEnvTopicThesaurusPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/searchext/SearchExtEnvTopicThesaurusPortlet.java
@@ -8,11 +8,26 @@ import java.io.IOException;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
+import javax.portlet.PortletSession;
 
+import org.apache.pluto.core.impl.PortletSessionImpl;
 import org.apache.velocity.context.Context;
 
+import de.ingrid.iplug.sns.utils.Topic;
+import de.ingrid.portal.forms.SearchExtEnvPlaceGeothesaurusForm;
+import de.ingrid.portal.forms.SearchExtEnvTopicThesaurusForm;
 import de.ingrid.portal.global.Settings;
 import de.ingrid.portal.global.Utils;
+import de.ingrid.portal.interfaces.impl.SNSSimilarTermsInterfaceImpl;
+import de.ingrid.portal.search.DisplayTreeFactory;
+import de.ingrid.portal.search.DisplayTreeNode;
+import de.ingrid.portal.search.PageState;
+import de.ingrid.portal.search.UtilsSearch;
+import de.ingrid.utils.IngridHit;
+import de.ingrid.utils.query.IngridQuery;
+import de.ingrid.utils.query.TermQuery;
+import de.ingrid.utils.queryparser.ParseException;
+import de.ingrid.utils.queryparser.QueryStringParser;
 
 /**
  * This portlet handles the fragment of the thesaurus input in the extended search.
@@ -38,6 +53,9 @@ public class SearchExtEnvTopicThesaurusPortlet extends SearchExtEnvTopic {
     private final static String PARAMV_VIEW_BROWSE = "2";
 
     private final static String PARAMV_VIEW_SYNONYM = "3";
+    
+    private final static String TOPICS = "topics";
+    
 
     public void doView(javax.portlet.RenderRequest request, javax.portlet.RenderResponse response)
             throws PortletException, IOException {
@@ -49,6 +67,9 @@ public class SearchExtEnvTopicThesaurusPortlet extends SearchExtEnvTopic {
 
         setDefaultViewPage(TEMPLATE_START);
 
+        SearchExtEnvTopicThesaurusForm f = (SearchExtEnvTopicThesaurusForm) Utils.getActionForm(request, SearchExtEnvTopicThesaurusForm.SESSION_KEY, SearchExtEnvTopicThesaurusForm.class);        
+        context.put("actionForm", f);
+        
         // ----------------------------------
         // check for passed RENDER PARAMETERS and react
         // ----------------------------------
@@ -57,6 +78,17 @@ public class SearchExtEnvTopicThesaurusPortlet extends SearchExtEnvTopic {
             action = "";
         }
         if (action.equals(PARAMV_VIEW_RESULTS)) {
+            // TODO remove page state in future, when separate portlets
+            // use messages and render parameters instead !!!
+            PortletSession session = request.getPortletSession();
+            PageState ps = (PageState) session.getAttribute("portlet_state");
+            if (ps == null) {
+                ps = new PageState(this.getClass().getName());
+                ps = initPageState(ps);
+                session.setAttribute("portlet_state", ps);
+            }
+            context.put("ps", ps);
+            
             setDefaultViewPage(TEMPLATE_RESULTS);
         } else if (action.equals(PARAMV_VIEW_BROWSE)) {
             setDefaultViewPage(TEMPLATE_BROWSE);
@@ -81,15 +113,71 @@ public class SearchExtEnvTopicThesaurusPortlet extends SearchExtEnvTopic {
         String submittedSearch = request.getParameter("submitSearch");
         String submittedAddToQuery = request.getParameter("submitAddToQuery");
 
+        PortletSession session = request.getPortletSession();
+        PageState ps = (PageState) session.getAttribute("portlet_state");
+        if (ps == null) {
+            ps = new PageState(this.getClass().getName());
+            ps = initPageState(ps);
+            session.setAttribute("portlet_state", ps);
+        }
+        
         // TODO: implement functionality
+        DisplayTreeNode similarRoot = null;
         if (submittedSearch != null) {
-
             // In Thesaurus suchen
-
+            SearchExtEnvTopicThesaurusForm f = (SearchExtEnvTopicThesaurusForm) Utils.getActionForm(request, SearchExtEnvTopicThesaurusForm.SESSION_KEY, SearchExtEnvTopicThesaurusForm.class);        
+            f.clearErrors();
+            f.populate(request);
+            if (f.validate()) {
+                // get terms from querystring
+                IngridQuery query = null;
+                try {
+                    query = QueryStringParser.parse(f.getInput(SearchExtEnvTopicThesaurusForm.FIELD_SEARCH_TERM));
+                } catch (ParseException e) {
+                }
+                if (query == null) {
+                    f.setError("", "searchExtEnvTopicThesaurus.error.no_term_found");
+                } else {
+                    ps.putBoolean("isSimilarOpen", true);
+                    if (similarRoot == null) {
+                        similarRoot = DisplayTreeFactory.getTreeFromQueryTerms(query);
+                        if (similarRoot.getChildren().size() > 0) {
+                            session.setAttribute("similarRoot", similarRoot);
+                            if (similarRoot.getChildren().size() == 1) {
+                                openNode(similarRoot, ((DisplayTreeNode) similarRoot.getChildren().get(0)).getId());
+                            }
+                        } else {
+                            f.setError("", "searchExtEnvTopicThesaurus.error.no_term_found");
+                        }
+                    }
+                }
+                ps.put("similarRoot", similarRoot);
+            }
+            
             // redirect to same page with view param setting view !
             String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, PARAMV_VIEW_RESULTS);
             actionResponse.sendRedirect(PAGE_THESAURUS + urlViewParam);
 
+        } else if (action.equalsIgnoreCase("doOpenNode")) {
+            similarRoot = (DisplayTreeNode) session.getAttribute("similarRoot");
+            if (similarRoot != null) {
+                openNode(similarRoot, request.getParameter("nodeId"));
+                ps.put("similarRoot", similarRoot);
+            }
+            // redirect to same page with view param setting view !
+            String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, PARAMV_VIEW_RESULTS);
+            actionResponse.sendRedirect(PAGE_THESAURUS + urlViewParam);
+        } else if (action.equalsIgnoreCase("doCloseNode")) {
+            similarRoot = (DisplayTreeNode) session.getAttribute("similarRoot");
+            if (similarRoot != null) {
+                DisplayTreeNode node = similarRoot.getChild(request.getParameter("nodeId"));
+                if (node != null) {
+                    node.setOpen(false);
+                }
+            }
+            // redirect to same page with view param setting view !
+            String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, PARAMV_VIEW_RESULTS);
+            actionResponse.sendRedirect(PAGE_THESAURUS + urlViewParam);
         } else if (submittedAddToQuery != null) {
 
             // Zur Suchanfrage hinzufuegen
@@ -109,6 +197,9 @@ public class SearchExtEnvTopicThesaurusPortlet extends SearchExtEnvTopic {
         } else if (action.equalsIgnoreCase("doBrowse")) {
 
             // SNS Deskriptor browsen
+            String topicID = request.getParameter("topicID");
+//            IngridHit[] similarHits = SNSSimilarTermsInterfaceImpl.getInstance().ge.getTopicSimilarLocationsFromTopic(topicId);
+            
 
             // redirect to same page with view param setting view !
             String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, PARAMV_VIEW_BROWSE);
@@ -128,4 +219,36 @@ public class SearchExtEnvTopicThesaurusPortlet extends SearchExtEnvTopic {
             processTab(actionResponse, newTab);
         }
     }
+
+    private PageState initPageState(PageState ps) {
+        ps.putBoolean("isSimilarOpen", false);
+        ps.put("similarRoot", null);
+        return ps;
+    }
+    
+    private void openNode(DisplayTreeNode rootNode, String nodeId) {
+        DisplayTreeNode node = rootNode.getChild(nodeId);
+        node.setOpen(true);
+        if (node != null && node.getType() == DisplayTreeNode.SEARCH_TERM && node.getChildren().size() == 0
+                && !node.isLoading()) {
+            node.setLoading(true);
+            IngridHit[] hits = SNSSimilarTermsInterfaceImpl.getInstance().getTopicsFromText(node.getName(), "/thesa");
+            if (hits != null && hits.length > 1) {
+                for (int i=0; i<hits.length; i++) {
+                    Topic hit = (Topic) hits[i];
+                    if (!hit.getTopicName().equalsIgnoreCase(node.getName())) {
+                        DisplayTreeNode snsNode = new DisplayTreeNode(node.getId() + i, hit.getTopicName(), false);
+                        snsNode.setType(DisplayTreeNode.SNS_TERM);
+                        snsNode.setParent(node);
+                        snsNode.put("topicID", hit.getTopicID());
+                        node.addChild(snsNode);
+                    }
+                }
+            } else {
+                // TODO remove node from display tree
+            }
+            node.setLoading(false);
+        }
+    }
+    
 }
