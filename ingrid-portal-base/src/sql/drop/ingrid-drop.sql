6da9832cc915d8a8d53446d4a9ec949add16f214
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-05-16 16:53:04 +0000

    - add url to providers - reimport from newer provider list

--- ingrid-portal-base/src/sql/drop/ingrid-drop.sql
+++ ingrid-portal-base/src/sql/drop/ingrid-drop.sql
@@ -1,4 +1,5 @@
 drop table ingrid_partner;
+drop table ingrid_provider;
 drop table ingrid_env_topic;
 drop table ingrid_env_funct_category;
 drop table ingrid_service_rubric;
@@ -8,11 +9,6 @@ drop table ingrid_chronicle_eventtypes;
 drop table ingrid_rss_source;
 drop table ingrid_rss_store;
 drop table ingrid_anniversary;
-
-drop table users;
-drop table roles;
-drop table groups;
-drop table usergrouprole;
 
 drop table ingrid_newsletter_data;
 
