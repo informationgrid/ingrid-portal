d575d58e0dd1b7211b86b7ccacccad307a676e32
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-05-22 12:28:47 +0000

    - added functionality to parse parameters into message !

--- ingrid-portal-apps/src/java/de/ingrid/portal/global/IngridResourceBundle.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/global/IngridResourceBundle.java
@@ -3,6 +3,8 @@
  */
 package de.ingrid.portal.global;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
@@ -15,6 +17,11 @@ public class IngridResourceBundle {
     ResourceBundle r = null;
 
     /**
+     * encapsulates parameters which will be formatted into message !
+     */
+    ArrayList parameters = null;
+
+    /**
      * @param r
      */
     public IngridResourceBundle(ResourceBundle r) {
@@ -22,6 +29,29 @@ public class IngridResourceBundle {
         this.r = r;
     }
 
+    /**
+     * Reset the parameters which will be formatted into message
+     */
+    public void resetMsgParams() {
+        parameters = null;
+    }
+
+    /**
+     * Set a parameter which will be formatted into a message
+     * @param param
+     */
+    public void setMsgParam(String param) {
+        if (this.parameters == null) {
+            parameters = new ArrayList();
+        }
+        parameters.add(param);
+    }
+
+    /**
+     * Get a message from encapsulated resource, also takes CommonResources into account
+     * @param key
+     * @return
+     */
     public String getString(String key) {
         try {
             return r.getString(key);
@@ -30,12 +60,33 @@ public class IngridResourceBundle {
         }
     }
 
+    /**
+     * Get a message only from CommonResources
+     * @param key
+     * @return
+     */
     public String getCommonString(String key) {
         try {
-            ResourceBundle commonRes = ResourceBundle.getBundle("de.ingrid.portal.resources.CommonResources", r.getLocale());
+            ResourceBundle commonRes = ResourceBundle.getBundle("de.ingrid.portal.resources.CommonResources", r
+                    .getLocale());
             return commonRes.getString(key);
         } catch (Exception e) {
             return key;
+        }
+    }
+
+    /**
+     * Get a message from encapsulated resource with encapsulated parameters formatted into message !
+     * @param key
+     * @return
+     */
+    public String getStringWithMsgParams(String key) {
+        String message = r.getString(key);
+        try {
+            MessageFormat mf = new MessageFormat(message);
+            return mf.format(this.parameters.toArray());
+        } catch (Exception e) {
+            return message;
         }
     }
 
