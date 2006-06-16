28b1c5b5c02cede1e1a27a124b8cb34f43223b24
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-06-16 15:39:32 +0000

    - add personalize overview portlet that dynamically generates shortcuts to the personalize portlets   (work in progress)

---
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/myportal/MyPortalPersonalizeOverviewPortlet.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2006 wemove digital solutions. All rights reserved.
+ */
+package de.ingrid.portal.portlets.myportal;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
+import javax.portlet.PortletPreferences;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.apache.jetspeed.CommonPortletServices;
+import org.apache.jetspeed.om.page.Fragment;
+import org.apache.jetspeed.om.page.Page;
+import org.apache.jetspeed.om.preference.FragmentPreference;
+import org.apache.jetspeed.page.PageManager;
+import org.apache.jetspeed.page.PageNotFoundException;
+import org.apache.jetspeed.page.document.NodeException;
+import org.apache.portals.bridges.velocity.GenericVelocityPortlet;
+import org.apache.velocity.context.Context;
+
+import de.ingrid.portal.global.IngridResourceBundle;
+
+/**
+ * TODO Describe your created type (class, etc.) here.
+ *
+ * @author joachim@wemove.com
+ */
+public class MyPortalPersonalizeOverviewPortlet extends GenericVelocityPortlet {
+
+    
+    private PageManager pageManager;
+    
+    
+    /**
+     * @see org.apache.portals.bridges.velocity.GenericVelocityPortlet#init(javax.portlet.PortletConfig)
+     */
+    public void init(PortletConfig config) throws PortletException {
+        super.init(config);
+        pageManager = (PageManager)getPortletContext().getAttribute(CommonPortletServices.CPS_PAGE_MANAGER_COMPONENT);
+        if (null == pageManager)
+        {
+            throw new PortletException("Failed to find the Page Manager on portlet initialization");
+        }
+    }
+
+    /**
+     * @see org.apache.portals.bridges.velocity.GenericVelocityPortlet#doView(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
+     */
+    public void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException {
+        // TODO Auto-generated method stub
+        List fragments = null;
+        ArrayList portletTitleKeys = new ArrayList();
+        try {
+            Page myPage = pageManager.getPage("myportal-personalize.psml");
+            Fragment root = myPage.getRootFragment();
+            fragments = root.getFragments();
+            for (int i=0; i<fragments.size(); i++) {
+                Fragment f = (Fragment)fragments.get(i);
+                if (f.getLayoutColumn() == 0 && f.getLayoutRow() > 0) {
+                    List prefs = f.getPreferences();
+                    for (int j=0; j<prefs.size(); j++) {
+                        FragmentPreference pref = (FragmentPreference)prefs.get(j);
+                        String name = pref.getName();
+                        if (name.equals("titleKey")) {
+                            portletTitleKeys.add(f.getLayoutRow()-1, pref.getValueList().get(0));
+                        }
+                    }
+                }
+            }
+        } catch (PageNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NodeException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Context context = getContext(request);
+        IngridResourceBundle messages = new IngridResourceBundle(getPortletConfig().getResourceBundle(request.getLocale()));
+        context.put("MESSAGES", messages);
+
+        PortletPreferences prefs = request.getPreferences();
+        String titleKey = prefs.getValue("titleKey", "searchSettings.title.rankingAndGrouping");
+        response.setTitle(messages.getString(titleKey));
+        
+        context.put("portletTitleKeys", portletTitleKeys);
+        
+        super.doView(request, response);
+    }
+
+    /**
+     * @see org.apache.portals.bridges.velocity.GenericVelocityPortlet#processAction(javax.portlet.ActionRequest, javax.portlet.ActionResponse)
+     */
+    public void processAction(ActionRequest request, ActionResponse actionResponse) throws PortletException, IOException {
+        // TODO Auto-generated method stub
+        super.processAction(request, actionResponse);
+    }
+
+    
+    
+}
