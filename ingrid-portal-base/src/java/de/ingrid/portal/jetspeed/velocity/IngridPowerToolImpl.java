e2e399570d902c98a6c23b45d5061b686bde070c
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-06-30 10:40:34 +0000

    - add user logged in status to the decorator

--- ingrid-portal-base/src/java/de/ingrid/portal/jetspeed/velocity/IngridPowerToolImpl.java
+++ ingrid-portal-base/src/java/de/ingrid/portal/jetspeed/velocity/IngridPowerToolImpl.java
@@ -1,24 +1,36 @@
 package de.ingrid.portal.jetspeed.velocity;
 
+import java.security.Principal;
 import java.util.Iterator;
 import java.util.List;
+import java.util.prefs.Preferences;
 
+import javax.portlet.PortletException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.jetspeed.Jetspeed;
 import org.apache.jetspeed.om.page.ContentFragment;
 import org.apache.jetspeed.om.preference.FragmentPreference;
 import org.apache.jetspeed.request.RequestContext;
+import org.apache.jetspeed.security.SecurityException;
+import org.apache.jetspeed.security.User;
+import org.apache.jetspeed.security.UserManager;
 import org.apache.jetspeed.services.title.DynamicTitleService;
 import org.apache.jetspeed.velocity.JetspeedPowerToolImpl;
 import org.apache.pluto.om.common.Preference;
 import org.apache.pluto.om.common.PreferenceSet;
 
-public class IngridPowerToolImpl extends JetspeedPowerToolImpl
-{
+public class IngridPowerToolImpl extends JetspeedPowerToolImpl {
+    private static UserManager userManager = null;
 
-    public IngridPowerToolImpl(RequestContext requestContext, DynamicTitleService titleService) throws Exception
-    {
+    public IngridPowerToolImpl(RequestContext requestContext, DynamicTitleService titleService) throws Exception {
         super(requestContext, titleService);
+        if (userManager == null) {
+            userManager = (UserManager) Jetspeed.getComponentManager().getComponent(UserManager.class.getName());
+            if (null == userManager) {
+                throw new PortletException("Failed to find the User Manager on powertool initialization");
+            }
+        }
     }
 
     public Object getPreferenceFirstValue(ContentFragment f, String key) {
@@ -30,39 +42,51 @@ public class IngridPowerToolImpl extends JetspeedPowerToolImpl
                 return pref.getValueList().get(0);
             }
         }
-        
-        // no preference in fragment found, try to get the preference from the portlet preferences
-        try
-        {
-            PreferenceSet ps = windowAccess.getPortletWindow(f).getPortletEntity().getPortletDefinition().getPreferenceSet();
+
+        // no preference in fragment found, try to get the preference from the
+        // portlet preferences
+        try {
+            PreferenceSet ps = windowAccess.getPortletWindow(f).getPortletEntity().getPortletDefinition()
+                    .getPreferenceSet();
             Preference p = ps.get(key);
             if (p != null) {
-                return (String)p.getValues().next();
+                return (String) p.getValues().next();
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             handleError(e, "JetspeedPowerTool failed to retreive the current PortletEntity.  " + e.toString(),
                     getCurrentFragment());
         }
         return "";
     }
 
-    public String getAbsoluteUrl(String relativePath)
-    {
+    public String getAbsoluteUrl(String relativePath) {
         // only rewrite a non-absolute url
-        if (relativePath != null && relativePath.indexOf("://") == -1 && relativePath.indexOf("mailto:") == -1)
-        {
+        if (relativePath != null && relativePath.indexOf("://") == -1 && relativePath.indexOf("mailto:") == -1) {
             HttpServletRequest request = getRequestContext().getRequest();
             StringBuffer path = new StringBuffer();
             return renderResponse.encodeURL(path.append(request.getScheme()).append("://").append(
                     request.getServerName()).append(":").append(request.getServerPort()).append(
                     request.getContextPath()).append(request.getServletPath()).append(relativePath).toString());
-        }
-        else
-        {
+        } else {
             return relativePath;
         }
     }
-    
+
+    public Preferences getUserProperties() {
+        Principal principal = requestContext.getRequest().getUserPrincipal();
+        if (principal != null) {
+            String userName = principal.getName();
+            User user = null;
+            try {
+                user = userManager.getUser(userName);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+            if (user != null) {
+                return user.getUserAttributes();
+            }
+        }
+        return null;
+    }
+
 }
