528ea1dc35c389da4fc4c3dddde4510bfedf3a0c
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-08-02 09:16:35 +0000

    - just formatting and removal of unnecessary imports ...

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/SearchSettingsPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/SearchSettingsPortlet.java
@@ -14,7 +14,6 @@ import org.apache.portals.bridges.velocity.AbstractVelocityMessagingPortlet;
 import org.apache.velocity.context.Context;
 
 import de.ingrid.portal.config.IngridSessionPreferences;
-import de.ingrid.portal.forms.CreateAccountForm;
 import de.ingrid.portal.forms.SearchSettingsForm;
 import de.ingrid.portal.global.IngridResourceBundle;
 import de.ingrid.portal.global.Settings;
@@ -43,23 +42,29 @@ public class SearchSettingsPortlet extends AbstractVelocityMessagingPortlet {
             throws PortletException, IOException {
 
         Context context = getContext(request);
-        
-        IngridResourceBundle messages = new IngridResourceBundle(getPortletConfig().getResourceBundle(request.getLocale()));
+
+        IngridResourceBundle messages = new IngridResourceBundle(getPortletConfig().getResourceBundle(
+                request.getLocale()));
         context.put("MESSAGES", messages);
 
-        SearchSettingsForm f = (SearchSettingsForm) Utils.getActionForm(request, SearchSettingsForm.SESSION_KEY, SearchSettingsForm.class);
+        SearchSettingsForm f = (SearchSettingsForm) Utils.getActionForm(request, SearchSettingsForm.SESSION_KEY,
+                SearchSettingsForm.class);
 
         String cmd = request.getParameter("cmd");
         if (cmd == null) {
             f.clear();
-            IngridSessionPreferences sessionPrefs = Utils.getSessionPreferences(request, IngridSessionPreferences.SESSION_KEY, IngridSessionPreferences.class);
-            f.setInput(SearchSettingsForm.FIELD_RANKING, (String)sessionPrefs.get(IngridSessionPreferences.SEARCH_SETTING_RANKING));
-            f.setInput(SearchSettingsForm.FIELD_GROUPING, (String)sessionPrefs.get(IngridSessionPreferences.SEARCH_SETTING_GROUPING));
-            f.setInput(SearchSettingsForm.FIELD_INCL_META, (String)sessionPrefs.get(IngridSessionPreferences.SEARCH_SETTING_INCL_META));
+            IngridSessionPreferences sessionPrefs = Utils.getSessionPreferences(request,
+                    IngridSessionPreferences.SESSION_KEY, IngridSessionPreferences.class);
+            f.setInput(SearchSettingsForm.FIELD_RANKING, (String) sessionPrefs
+                    .get(IngridSessionPreferences.SEARCH_SETTING_RANKING));
+            f.setInput(SearchSettingsForm.FIELD_GROUPING, (String) sessionPrefs
+                    .get(IngridSessionPreferences.SEARCH_SETTING_GROUPING));
+            f.setInput(SearchSettingsForm.FIELD_INCL_META, (String) sessionPrefs
+                    .get(IngridSessionPreferences.SEARCH_SETTING_INCL_META));
         }
-        
+
         context.put("actionForm", f);
-        
+
         super.doView(request, response);
     }
 
@@ -68,31 +73,35 @@ public class SearchSettingsPortlet extends AbstractVelocityMessagingPortlet {
 
         String action = request.getParameter("action");
         actionResponse.setRenderParameter("cmd", action);
-        
+
         if (action == null) {
             action = "";
         }
-        SearchSettingsForm f = (SearchSettingsForm) Utils.getActionForm(request, SearchSettingsForm.SESSION_KEY, SearchSettingsForm.class);
-        
+        SearchSettingsForm f = (SearchSettingsForm) Utils.getActionForm(request, SearchSettingsForm.SESSION_KEY,
+                SearchSettingsForm.class);
+
         if (action.equalsIgnoreCase(Settings.PARAMV_ACTION_SUBMIT)) {
 
             f.clearErrors();
-            
+
             f.populate(request);
             if (!f.validate()) {
                 return;
             }
-            
+
             f.setError("", "searchSettings.message.settings.stored");
         } else if (action.equalsIgnoreCase(Settings.PARAMV_ACTION_ORIGINAL_SETTINGS)) {
             f.init();
             f.setError("", "searchSettings.message.settings.resetted");
         }
 
-        IngridSessionPreferences sessionPrefs = Utils.getSessionPreferences(request, IngridSessionPreferences.SESSION_KEY, IngridSessionPreferences.class);
+        IngridSessionPreferences sessionPrefs = Utils.getSessionPreferences(request,
+                IngridSessionPreferences.SESSION_KEY, IngridSessionPreferences.class);
         sessionPrefs.put(IngridSessionPreferences.SEARCH_SETTING_RANKING, f.getInput(SearchSettingsForm.FIELD_RANKING));
-        sessionPrefs.put(IngridSessionPreferences.SEARCH_SETTING_GROUPING, f.getInput(SearchSettingsForm.FIELD_GROUPING));
-        sessionPrefs.put(IngridSessionPreferences.SEARCH_SETTING_INCL_META, f.getInput(SearchSettingsForm.FIELD_INCL_META));
-    
+        sessionPrefs.put(IngridSessionPreferences.SEARCH_SETTING_GROUPING, f
+                .getInput(SearchSettingsForm.FIELD_GROUPING));
+        sessionPrefs.put(IngridSessionPreferences.SEARCH_SETTING_INCL_META, f
+                .getInput(SearchSettingsForm.FIELD_INCL_META));
+
     }
 }
