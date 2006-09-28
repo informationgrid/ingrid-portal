6ab7449a5287f98ceb9e7b52e3f8c932d963b0b2
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-09-28 09:59:53 +0000

    - change behaviour of the database admin portlets:   disable ability to edit multiple records at once - add field content checks for rss administration - add required fields

---
+++ ingrid-portal-apps/src/java/de/ingrid/portal/forms/AdminRSSForm.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2006 wemove digital solutions. All rights reserved.
+ */
+package de.ingrid.portal.forms;
+
+import java.util.List;
+
+import javax.portlet.PortletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import de.ingrid.portal.global.UtilsDB;
+import de.ingrid.portal.hibernate.HibernateUtil;
+import de.ingrid.portal.om.IngridRSSSource;
+
+/**
+ * Form Handler for Content Management of Partners.
+ * 
+ * @author Martin Maidhof
+ */
+public class AdminRSSForm extends ActionForm {
+
+    private final static Log log = LogFactory.getLog(AdminRSSForm.class);
+
+    private static final long serialVersionUID = 8335389649101260301L;
+
+    public static final String FIELD_PROVIDER = "provider";
+
+    public static final String FIELD_DESCRIPTION = "description";
+
+    public static final String FIELD_URL = "url";
+
+    public static final String FIELD_LANGUAGE = "language";
+
+    public static final String FIELD_CATEGORIES = "categories";
+
+    public static final String PARAM_ID = "id";
+
+    /**
+     * @see de.ingrid.portal.forms.ActionForm#init()
+     */
+    public void init() {
+        clearInput();
+    }
+
+    /**
+     * @see de.ingrid.portal.forms.ActionForm#populate(javax.portlet.PortletRequest)
+     */
+    public void populate(PortletRequest request) {
+        clear();
+        String[] ids = request.getParameterValues(PARAM_ID);
+        if (ids != null) {
+            setInput("numEntities", new Integer(ids.length).toString());
+            for (int i = 0; i < ids.length; i++) {
+                setInput(PARAM_ID + i, ids[i]);
+                setInput(FIELD_PROVIDER + i, request.getParameter(FIELD_PROVIDER + i));
+                setInput(FIELD_DESCRIPTION + i, request.getParameter(FIELD_DESCRIPTION + i));
+                setInput(FIELD_URL + i, request.getParameter(FIELD_URL + i));
+                setInput(FIELD_LANGUAGE + i, request.getParameter(FIELD_LANGUAGE + i));
+                if (request.getParameter(FIELD_CATEGORIES + i) == null
+                        || request.getParameter(FIELD_CATEGORIES + i).length() == 0) {
+                    setInput(FIELD_CATEGORIES + i, "all");
+                } else {
+                    setInput(FIELD_CATEGORIES + i, request.getParameter(FIELD_CATEGORIES + i));
+                }
+            }
+        }
+    }
+
+    /**
+     * @see de.ingrid.portal.forms.ActionForm#validate()
+     */
+    public boolean validate() {
+        boolean allOk = true;
+        clearErrors();
+
+        // check input
+        try {
+            String numEntities = getInput("numEntities");
+            if (numEntities != null) {
+                int intNumEntities = new Integer(numEntities).intValue();
+                String fieldName = "";
+                for (int i = 0; i < intNumEntities; i++) {
+
+                    fieldName = FIELD_PROVIDER + i;
+                    if (!hasInput(fieldName)) {
+                        setError(fieldName, "admin.rss.error.missing.provider");
+                        allOk = false;
+                    }
+                    fieldName = FIELD_URL + i;
+                    if (!hasInput(fieldName)) {
+                        setError(fieldName, "admin.rss.error.missing.url");
+                        allOk = false;
+                    } else {
+                        // check database whether other record (other id) has
+                        // same ident (kuerzel)
+                        Long id = null;
+                        try {
+                            id = new Long(getInput(PARAM_ID + i));
+                        } catch (Exception ex) {
+                        }
+                        String url = getInput(fieldName);
+                        Session session = HibernateUtil.currentSession();
+                        Criteria crit = session.createCriteria(IngridRSSSource.class).add(
+                                Restrictions.eq(FIELD_URL, url));
+                        if (id != null) {
+                            crit.add(Restrictions.ne(PARAM_ID, id));
+                        }
+                        List foundURLs = UtilsDB.getValuesFromDB(crit, session, null, true);
+                        if (foundURLs != null && !foundURLs.isEmpty()) {
+                            setError(fieldName, "admin.rss.error.url.exists");
+                            allOk = false;
+                        }
+                    }
+                    fieldName = FIELD_LANGUAGE + i;
+                    if (!hasInput(fieldName)) {
+                        setError(fieldName, "admin.rss.error.missing.language");
+                        allOk = false;
+                    }
+                }
+            }
+        } catch (Throwable t) {
+            if (log.isErrorEnabled()) {
+                log.error("Error validating input.", t);
+            }
+            allOk = false;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+
+        return allOk;
+    }
+}
--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/admin/ContentRSSPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/admin/ContentRSSPortlet.java
@@ -5,20 +5,24 @@ package de.ingrid.portal.portlets.admin;
 
 import java.lang.reflect.Array;
 
+import javax.portlet.ActionRequest;
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
 
+import de.ingrid.portal.forms.AdminRSSForm;
+import de.ingrid.portal.global.Utils;
 import de.ingrid.portal.om.IngridRSSSource;
 
 /**
  * Portlet handling content management of RSS feeds
- *
+ * 
  * @author martin@wemove.com
  */
 public class ContentRSSPortlet extends ContentPortlet {
 
-    //    private final static Log log = LogFactory.getLog(ContentRSSPortlet.class);
+    // private final static Log log =
+    // LogFactory.getLog(ContentRSSPortlet.class);
 
     /**
      * @see javax.portlet.Portlet#init(javax.portlet.PortletConfig)
@@ -36,6 +40,7 @@ public class ContentRSSPortlet extends ContentPortlet {
 
     /**
      * Set up Entity from parameters in request.
+     * 
      * @param request
      * @return
      */
@@ -53,9 +58,45 @@ public class ContentRSSPortlet extends ContentPortlet {
                 dbEntities[i].setUrl(request.getParameter("url" + i));
                 dbEntities[i].setLanguage(request.getParameter("language" + i));
                 dbEntities[i].setCategories(request.getParameter("categories" + i));
+                if (dbEntities[i].getCategories() == null || dbEntities[i].getCategories().length() == 0) {
+                    dbEntities[i].setCategories("all");
+                }
             }
         }
 
         return dbEntities;
     }
+
+    /**
+     * Redefine method, we have to check stuff.
+     * 
+     * @see de.ingrid.portal.portlets.admin.ContentPortlet#doActionUpdate(javax.portlet.ActionRequest)
+     */
+    protected void doActionUpdate(ActionRequest request) {
+        AdminRSSForm af = (AdminRSSForm) Utils.getActionForm(request, KEY_ACTION_FORM, AdminRSSForm.class);
+        af.populate(request);
+        af.validate();
+        if (af.hasErrors()) {
+            return;
+        }
+
+        super.doActionUpdate(request);
+    }
+
+    /**
+     * Redefine method, we have to check stuff.
+     * 
+     * @see de.ingrid.portal.portlets.admin.ContentPortlet#doActionSave(javax.portlet.ActionRequest)
+     */
+    protected void doActionSave(ActionRequest request) {
+        AdminRSSForm af = (AdminRSSForm) Utils.getActionForm(request, KEY_ACTION_FORM, AdminRSSForm.class);
+        af.populate(request);
+        af.validate();
+        if (af.hasErrors()) {
+            return;
+        }
+
+        super.doActionSave(request);
+    }
+
 }
