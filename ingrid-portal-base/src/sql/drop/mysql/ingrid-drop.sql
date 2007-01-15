1cf488315ca085cfc5f10d05973c2f1ed1915e7f
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2007-01-15 16:21:06 +0000

    - change portal database handling for the use with oracle   CAUTION: this update changes table names!!! please rename the following tables:   * ingrid_principal_preference -> ingrid_principal_pref   * ingrid_chronicle_eventtypes -> ingrid_chron_eventtypes   * ingrid_sys_codelist -> ingrid_codelist   * ingrid_sys_codelist_domain -> ingrid_codelist_domain

---
+++ ingrid-portal-base/src/sql/drop/mysql/ingrid-drop.sql
@@ -0,0 +1,21 @@
+drop table ingrid_partner;
+drop table ingrid_provider;
+drop table ingrid_env_topic;
+drop table ingrid_env_funct_category;
+drop table ingrid_service_rubric;
+drop table ingrid_measures_rubric;
+drop table ingrid_chron_eventtypes;
+
+drop table ingrid_rss_source;
+drop table ingrid_rss_store;
+drop table ingrid_anniversary;
+
+drop table ingrid_newsletter_data;
+
+drop table ingrid_codelist_domain;
+drop table ingrid_codelist;
+
+drop table ingrid_principal_pref;
+drop table ingrid_lookup;
+drop table ingrid_cms_item;
+drop table ingrid_cms;
---
+++ ingrid-portal-base/src/sql/drop/oracle/ingrid-drop.sql
@@ -0,0 +1,40 @@
+drop table ingrid_partner;
+drop sequence ingrid_partner_seq;
+drop table ingrid_provider;
+drop sequence ingrid_provider_seq;
+drop table ingrid_env_topic;
+drop sequence ingrid_env_topic_seq;
+drop table ingrid_env_funct_category;
+drop sequence ingrid_env_funct_category_seq;
+drop table ingrid_service_rubric;
+drop sequence ingrid_service_rubric_seq;
+drop table ingrid_measures_rubric;
+drop sequence ingrid_measures_rubric_seq;
+drop table ingrid_chron_eventtypes;
+drop sequence ingrid_chron_eventtypes_seq;
+
+drop table ingrid_rss_source;
+drop sequence ingrid_rss_source_seq;
+drop table ingrid_rss_store;
+drop sequence ingrid_rss_store_seq;
+drop table ingrid_anniversary;
+drop sequence ingrid_anniversary_seq;
+
+drop table ingrid_newsletter_data;
+drop sequence ingrid_newsletter_data_seq;
+
+drop table ingrid_codelist_domain;
+drop sequence ingrid_codelist_domain_seq;
+drop table ingrid_codelist;
+drop sequence ingrid_codelist_seq;
+
+drop table ingrid_principal_pref;
+drop sequence ingrid_principal_pref_seq;
+
+drop table ingrid_lookup;
+drop sequence ingrid_lookup_seq;
+
+drop table ingrid_cms_item;
+drop sequence ingrid_cms_item_seq;
+drop table ingrid_cms;
+drop sequence ingrid_cms_seq;
