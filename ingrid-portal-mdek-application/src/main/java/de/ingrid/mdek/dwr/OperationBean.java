b749c8e1371adb53dc1fa8163b65226990708424
mbenz <mbenz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-01-16 21:11:42 +0000

    - Added frontend load & store for object class 3 values. (Mapping for Operation Table still missing)

---
+++ ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/dwr/OperationBean.java
@@ -0,0 +1,20 @@
+package de.ingrid.mdek.dwr;
+
+public class OperationBean {
+	String name;
+	String description;
+
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
