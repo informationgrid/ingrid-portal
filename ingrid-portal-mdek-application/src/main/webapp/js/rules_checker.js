af9ad77f96c0ae86bae7d945a8a79015be5e7afc
mbenz <mbenz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-01-18 09:38:05 +0000

    - Implemented rules_checker functions to check if an object can be published

--- ingrid-portal-mdek-application/src/main/webapp/js/rules_checker.js
+++ ingrid-portal-mdek-application/src/main/webapp/js/rules_checker.js
@@ -10,18 +10,72 @@ dojo.addOnLoad(function()
 	}
 });
 
-var notEmptyFields = ["objectName", "objectClass", "generalDesc", "extraInfoLangData", "extraInfoLangMetaData"];
+var notEmptyFields = ["objectName", "objectClass", "generalDesc", "extraInfoLangData",
+					  "extraInfoLangMetaData"];
+
 var notEmptyFieldsClass1 = ["ref1DataSet"]; 
 var notEmptyFieldsClass3 = ["ref3ServiceType"];
 var notEmptyFunc = function(element) {return (this[element] && this[element] != "");}
 
+var notEmptyTables = ["generalAddressTable", "thesaurusTopicsList"];
+var notEmptyTableFunc = function(element) {return (this[element].length > 0);}
 
 function isObjectPublishable(idcObject) {
-	if (dojo.lang.every(notEmptyFields, notEmptyFunc, idcObject)) {
-		// All the fields that need to be filled contain data
-		// Continue checking
-		return true;
-	} else {
+	// Check if all the required fields and tables contain data
+	if (!dojo.lang.every(notEmptyFields, notEmptyFunc, idcObject)) {
 		return false;
 	}
+	if (!dojo.lang.every(notEmptyTables, notEmptyTableFunc, idcObject)) {
+		return false;
+	}
+	
+	// Check if one of the 'Raumbezug' tables has an entry with a bounding box
+	var snsData = idcObject.spatialRefAdminUnitTable;
+	var freeData = idcObject.spatialRefLocationTable;
+	var hasBB = function(item) {return (item.longitude1 && item.longitude2 && item.latitude1 && item.latitude2);};
+	if ( !(dojo.lang.some(snsData, hasBB) || dojo.lang.some(freeData, hasBB)) ) {
+		return false;
+	}
+
+	// Check if the thesaurus table has at least three entries
+	if (idcObject.thesaurusTermsTable.length < 3) {
+		return false;
+	}
+
+	// If one of the 'Umweltthemen' contains an entry, both of them need to contain at least one entry
+	if ((idcObject.thesaurusEnvTopicsList.length > 0 && idcObject.thesaurusEnvCatsList.length == 0)
+	  ||(idcObject.thesaurusEnvTopicsList.length == 0 && idcObject.thesaurusEnvCatsList.length > 0)) {
+		return false;
+	}
+
+	// Check the required fields per object class:
+	switch (idcObject.objectClass)
+	{
+		case '0':
+			// No additional required fields for object class 0
+			break;
+		case '1':
+			if (!dojo.lang.every(notEmptyFieldsClass1, notEmptyFunc, idcObject)) {
+				return false;
+			}
+			break;
+		case '2':
+			// No additional required fields for object class 2
+			break;
+		case '3':
+			if (!dojo.lang.every(notEmptyFieldsClass3, notEmptyFunc, idcObject)) {
+				return false;
+			}
+			break;
+		case '4':
+			// No additional required fields for object class 4
+			break;
+		case '5':
+			// No additional required fields for object class 5
+			break;
+		default:
+			break;
+	}
+
+	return true;
 }
