aa58d3212eb8921b23edd03461a68bc10535ebef
mbenz <mbenz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-01-23 17:23:17 +0000

    - Added correct select list for the operation dialog

---
+++ ingrid-portal-mdek-application/src/main/webapp/js/data/ref3OperationDirection.js
@@ -0,0 +1,10 @@
+/*
+ * Objektklasse Dienst/Anwendung/Informationssystem
+ * Operation: Direction
+ */
+[
+	["Eingabe", "Eingabe"],
+	["Ausgabe", "Ausgabe"],
+	["Ein -und Ausgabe", "Ein -und Ausgabe"]
+]
+ 
---
+++ ingrid-portal-mdek-application/src/main/webapp/js/data/ref3OperationOptional.js
@@ -0,0 +1,9 @@
+/*
+ * Objektklasse Dienst/Anwendung/Informationssystem
+ * Operation: Optional / Multiple
+ */
+[
+	["Nein", "Nein"],
+	["Ja", "Ja"]
+]
+ 
