f1add1e76bbe9c9406791fab1e7c65484aa1cb09
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-01-30 17:38:11 +0000

    - add more extensions - add test in selenium format for reproducibility

---
+++ ingrid-portal-test/src/selenium.ide.extensions/capture_click_xy.js
@@ -0,0 +1,6 @@
+Recorder.removeEventHandler('click');
+Recorder.addEventHandler('clickAt', 'click', function(event) {
+                var x = event.clientX - editor.seleniumAPI.Selenium.prototype.getElementPositionLeft(event.target);
+                var y = event.clientY - editor.seleniumAPI.Selenium.prototype.getElementPositionTop(event.target);
+                this.record('clickAt', this.findLocator(event.target), x + ',' + y);
+        }, { capture: true });
