0c9fef771c6ced1ee7ea54c5d34dec0c3da0719c
mbenz <mbenz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-04-10 08:11:06 +0000

    - Updated PageNavigation to behave similar to PortalU's result navigation (http://jira.media-style.com/browse/INGRIDII-128)

--- ingrid-portal-mdek-application/src/main/webapp/js/utilities.js
+++ ingrid-portal-mdek-application/src/main/webapp/js/utilities.js
@@ -1,12 +1,13 @@
 /**
- * Page Navigation obj
+ * Indirect Page Navigation obj
+ * DEPRECATED
  *  The purpose of this object is to display a simple page navigation for a given result set.
  *  It is initialised with the number of displayed results per page and the info/paging span where
  *  the navigation should be displayed. 
  *  To display/update the nav bar, use setTotalNumHits to set the number of hits that should be navigated.
  *  When the user selects a page, the 'onPageSelected' function is called with the selected page number as argument.
  */
-function PageNavigation(args) {
+function IndirectPageNavigation(args) {
 	this.resultsPerPage = args.resultsPerPage;
 	this.infoSpan = args.infoSpan;
 	this.pagingSpan = args.pagingSpan;
@@ -157,6 +158,169 @@ function PageNavigation(args) {
 		this.startHit = (pageIndex-1) * this.resultsPerPage;
 	}
 }
+
+
+/**
+ * Page Navigation obj
+ *  The purpose of this object is to display a simple page navigation for a given result set.
+ *  It behaves similar to 'IndirectPageNavigation' with the following difference:
+ *    If the user clicks the '<<', '<', '>' or '>>' button, an onPageSelected event is fired for the next page
+ */
+function PageNavigation(args) {
+	this.resultsPerPage = args.resultsPerPage;
+	this.infoSpan = args.infoSpan;
+	this.pagingSpan = args.pagingSpan;
+
+	this.startHit = 0;
+	this.totalNumHits = 0;
+
+	this.reset = function() {
+		this.startHit = 0;
+		this.totalNumHits = 0;
+	}
+
+	this.setTotalNumHits = function(numHits) {
+		this.totalNumHits = numHits;
+	}
+
+	this.getStartHit = function() {
+		return this.startHit;
+	}
+
+	this.updateDomNodes = function() {
+		var _this = this;
+		var totalNumHits = this.totalNumHits;
+		var startHit = this.startHit;
+		var endHit = startHit+this.resultsPerPage-1;
+
+		if (totalNumHits <= endHit)
+			endHit = totalNumHits - 1;
+	
+		var currentPage = Math.floor(startHit/this.resultsPerPage) + 1;
+		var lastPage = Math.ceil(totalNumHits/this.resultsPerPage);
+
+		// startPage determines which page should be shown first
+		var startPage = currentPage - 2;
+		if (startPage+4 > lastPage) {
+			startPage = lastPage-4;
+		}
+		if (startPage < 1) {
+			startPage = 1;
+		}
+/*
+		dojo.debug("totalNumHits: "+totalNumHits);
+		dojo.debug("startHit: "+startHit);
+		dojo.debug("endHit: "+endHit);
+		dojo.debug("currentPage: "+currentPage);
+		dojo.debug("lastPage: "+lastPage);
+*/
+
+		// Update the dom elements
+		if (totalNumHits == 0) {
+			this.infoSpan.innerHTML = "0 Treffer";
+		} else if (totalNumHits == 1) {
+			this.infoSpan.innerHTML = ""+(startHit+1)+"-"+(endHit+1)+" von "+totalNumHits+" Treffer";
+		} else {
+			this.infoSpan.innerHTML = ""+(startHit+1)+"-"+(endHit+1)+" von "+totalNumHits+" Treffern";
+		}
+	
+		// Clear the paging span
+		while (this.pagingSpan.hasChildNodes())
+			this.pagingSpan.removeChild(this.pagingSpan.firstChild);
+	
+		// Add info text to the paging span
+		if (totalNumHits == 0) {
+			this.pagingSpan.appendChild(document.createTextNode("(Seite 1 von 1)"));
+		} else {
+			this.pagingSpan.appendChild(document.createTextNode("(Seite "+currentPage+" von "+lastPage+")"));
+		}
+
+		// Add navigation to the next set of page navigation links
+		if (startPage > 1) {
+			// Add '<<' jump to page 1
+			this.pagingSpan.appendChild(document.createTextNode(" "));
+			var link = document.createElement("a");
+			link.onclick = function() {
+				_this.onPageSelected(1);
+			}
+			link.setAttribute("href", "javascript:void(0);");
+			link.setAttribute("title", "Erste Seite");
+			link.innerHTML = "&lt;&lt;";
+			this.pagingSpan.appendChild(link);
+
+			// Add '<' jump to currentPage - 1
+			this.pagingSpan.appendChild(document.createTextNode(" "));
+			var link = document.createElement("a");
+			link.previousPage = currentPage - 1;
+			link.onclick = function() {
+				_this.onPageSelected(this.previousPage);
+			}
+			link.setAttribute("href", "javascript:void(0);");
+			link.setAttribute("title", "zurueck");
+			link.innerHTML = "&lt;";
+			this.pagingSpan.appendChild(link);
+		}
+	
+		// Add the page selection links
+		for (var i = startPage; i < startPage+5; ++i) {
+	//		dojo.debug("i: "+i);
+	//		dojo.debug("pageSelectorStart+4: "+pageSelectorStart+4);
+
+			if (i < 1)
+				continue;
+
+			if (i > lastPage)
+				break;
+	
+			this.pagingSpan.appendChild(document.createTextNode(" "));
+	
+			if (i == currentPage) {
+				var textNode = document.createElement("em");
+				textNode.innerHTML = i;
+				this.pagingSpan.appendChild(textNode);
+			} else {		
+				var link = document.createElement("a");
+				link.onclick = function() {
+					_this.onPageSelected(this.innerHTML);
+				}
+				link.setAttribute("href", "javascript:void(0);");
+				link.setAttribute("title", "Seite "+i);
+				link.innerHTML = i;
+				this.pagingSpan.appendChild(link);
+			}
+		}
+	
+		// Add navigation to the next set of page navigation links
+		if (startPage+4 < lastPage) {
+			this.pagingSpan.appendChild(document.createTextNode(" "));
+			var link = document.createElement("a");
+			link.nextPage = currentPage + 1;
+			link.onclick = function() {
+				_this.onPageSelected(this.nextPage);
+			}
+			link.setAttribute("href", "javascript:void(0);");
+			link.setAttribute("title", "weiter");
+			link.innerHTML = "&gt;";
+			this.pagingSpan.appendChild(link);
+
+			this.pagingSpan.appendChild(document.createTextNode(" "));
+			var link = document.createElement("a");
+			link.lastPage = lastPage;
+			link.onclick = function() {
+				_this.onPageSelected(this.lastPage);
+			}
+			link.setAttribute("href", "javascript:void(0);");
+			link.setAttribute("title", "Letzte Seite");
+			link.innerHTML = "&gt;&gt;";
+			this.pagingSpan.appendChild(link);
+		}
+	}
+
+	this.onPageSelected = function(pageIndex) {
+		this.startHit = (pageIndex-1) * this.resultsPerPage;
+	}
+}
+
 
 /*
  * General Helper Functions
