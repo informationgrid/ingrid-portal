ff6beba52881837c053e6e52d44ac0e254e513fa
mbenz <mbenz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-10-17 10:22:12 +0000

    - Removed select box height limitation (long entries were truncated) - http://88.198.11.89/jira/browse/INGRIDII-340 - Modified import page style - Added zip/unzip test to ImportService

--- ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/dwr/services/ImportServiceImpl.java
+++ ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/dwr/services/ImportServiceImpl.java
@@ -1,6 +1,15 @@
 package de.ingrid.mdek.dwr.services;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import org.apache.log4j.Logger;
 import org.directwebremoting.io.FileTransfer;
@@ -14,7 +23,65 @@ public class ImportServiceImpl {
 		log.debug("uploadFile called.");
 		log.debug("fileName: "+fileTransfer.getName());
 		log.debug("mimeType: "+fileTransfer.getMimeType());
-//      return new FileTransfer(fileTransfer.getName(), fileTransfer.getMimeType(), fileTransfer.getInputStream());
-		return fileTransfer;
+
+		// decompress if zip, otherwise compress
+		if (fileTransfer.getMimeType().equals("application/zip")) {
+			ByteArrayOutputStream baos = decompress(fileTransfer.getInputStream());
+			String fileName = fileTransfer.getName().replace(".zip", "");
+			return new FileTransfer(fileName, "application/octet-stream", baos.toByteArray());
+
+		} else {
+			ByteArrayOutputStream output = compress(fileTransfer.getInputStream());
+			return new FileTransfer(fileTransfer.getName()+".zip", "application/zip", output.toByteArray());
+		}
+	}
+
+	// Compress (zip) any data on InputStream and write it to a ByteArrayOutputStream
+	public static ByteArrayOutputStream compress(InputStream is) throws IOException {
+		BufferedInputStream bin = new BufferedInputStream(is);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		GZIPOutputStream gzout = new GZIPOutputStream(new BufferedOutputStream(out));
+
+		final int BUFFER = 2048;
+		int count;
+		byte data[] = new byte[BUFFER];
+		while((count = bin.read(data, 0, BUFFER)) != -1) {
+		   gzout.write(data, 0, count);
+		}
+
+		gzout.close();
+		return out;
+	}
+
+	// Decompress (unzip) data on InputStream (has to contain zipped data) and write it to a ByteArrayOutputStream
+	public static ByteArrayOutputStream decompress(InputStream is) throws IOException {
+		GZIPInputStream gzin = new GZIPInputStream(new BufferedInputStream(is));
+		ByteArrayOutputStream baout = new ByteArrayOutputStream();
+		BufferedOutputStream out = new BufferedOutputStream(baout);
+
+		final int BUFFER = 2048;
+		int count;
+		byte data[] = new byte[BUFFER];
+		while((count = gzin.read(data, 0, BUFFER)) != -1) {
+		   out.write(data, 0, count);
+		}
+
+		out.close();
+		return baout;
+	}
+
+
+	public static void channelCopy(final ReadableByteChannel src, final WritableByteChannel dst) throws IOException {
+		final ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
+		while (src.read(buffer) != -1) {
+			buffer.flip();
+			dst.write(buffer);
+			buffer.compact();
+		}
+		buffer.flip();
+
+		while (buffer.hasRemaining()) {
+			dst.write(buffer);
+		}
 	}
 }
