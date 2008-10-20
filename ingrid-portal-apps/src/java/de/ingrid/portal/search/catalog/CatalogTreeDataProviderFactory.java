d509c952f00e652ecb311415242d12f411c2217c
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-10-20 10:20:23 +0000

    - add support for IDC 1.0.3 databases - improve logging

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/catalog/CatalogTreeDataProviderFactory.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/catalog/CatalogTreeDataProviderFactory.java
@@ -3,6 +3,9 @@
  */
 package de.ingrid.portal.search.catalog;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import de.ingrid.portal.search.IPlugVersionInspector;
 
 /**
@@ -11,9 +14,13 @@ import de.ingrid.portal.search.IPlugVersionInspector;
  */
 public class CatalogTreeDataProviderFactory {
 
+    private final static Log log = LogFactory.getLog(CatalogTreeDataProviderFactory.class);	
+	
 	public static CatalogTreeDataProvider getDetailDataPreparer(String version) {
 		
 		if (version.equals(IPlugVersionInspector.VERSION_IDC_1_0_2_DSC_OBJECT)) {
+			return new CatalogTreeDataProvider_IDC_1_0_2();
+		} else if (version.equals(IPlugVersionInspector.VERSION_IDC_1_0_3_DSC_OBJECT)) {
 			return new CatalogTreeDataProvider_IDC_1_0_2();
 		} else if (version.equals(IPlugVersionInspector.VERSION_IDC_1_0_2_DSC_ADDRESS)) {
 			return new CatalogTreeDataProvider_IDC_1_0_2();
@@ -23,6 +30,8 @@ public class CatalogTreeDataProviderFactory {
 			return new CatalogTreeDataProvider_UDK_5_0();
 		} else if (version.equals(IPlugVersionInspector.VERSION_UNKNOWN)) {
 			return new CatalogTreeDataProvider_IDC_1_0_2();
+		} else {
+			log.error("No CatalogTreeDataProvider could be found for iPlug version '" + version + "'.");
 		}
 		
 		return null;
