1d55409f16436f8a032fb6545f44c1c23b2387b0
mbenz <mbenz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-12-19 12:52:37 +0000

    ExportService now correctly returns a gzip compressed file 'export.xml.gz'

--- ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/dwr/services/ExportServiceImpl.java
+++ ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/dwr/services/ExportServiceImpl.java
@@ -1,14 +1,7 @@
 package de.ingrid.mdek.dwr.services;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.zip.GZIPInputStream;
-
 import org.apache.log4j.Logger;
 import org.directwebremoting.io.FileTransfer;
-import org.directwebremoting.io.OutputStreamLoader;
 
 import de.ingrid.mdek.beans.ExportInfoBean;
 import de.ingrid.mdek.handler.CatalogRequestHandler;
@@ -47,30 +40,6 @@ public class ExportServiceImpl {
 	public FileTransfer getLastExportFile() {
 		ExportInfoBean exportInfo = catalogRequestHandler.getExportInfo(true);
 
-		FileTransfer fileTransfer = new FileTransfer("test.xml", "application/xml", new SimpleOutputStreamLoader(exportInfo.getResult()));
-//		FileTransfer fileTransfer = new FileTransfer("export.zip", "application/zip", exportInfo.getResult());
-		return fileTransfer;
-	}
-}
-
-class SimpleOutputStreamLoader implements OutputStreamLoader {
-
-	private final byte[] content;
-	
-	public SimpleOutputStreamLoader(byte[] content) {
-		this.content = content;
-	}
-
-	public void close() throws IOException {
-		// TODO Auto-generated method stub
-	}
-
-	public void load(OutputStream out) throws IOException {
-		InputStream in = new GZIPInputStream(new ByteArrayInputStream(content));
-		int c;
-		while ((c = in.read()) != -1) {
-			out.write(c);
-		}
-		in.close();
+		return new FileTransfer("export.xml.gz", "x-gzip", exportInfo.getResult());
 	}
 }
