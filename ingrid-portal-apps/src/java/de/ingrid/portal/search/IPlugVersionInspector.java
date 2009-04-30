a0102aaf09d0c5501a44dde0bba0d75d8a7e41c4
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-04-30 14:25:50 +0000

    - add IGC 1.0.5 based address iPlug detection

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/IPlugVersionInspector.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/IPlugVersionInspector.java
@@ -27,6 +27,8 @@ public class IPlugVersionInspector {
 	public static final String VERSION_UDK_5_0_DSC_OBJECT = "VERSION_UDK_5_0_DSC_OBJECT";
 
 	public static final String VERSION_IDC_1_0_2_DSC_ADDRESS = "VERSION_IDC_1_0_2_DSC_ADDRESS";
+
+	public static final String VERSION_IDC_1_0_5_DSC_ADDRESS = "VERSION_IDC_1_0_5_DSC_ADDRESS";
 	
 	public static final String VERSION_UDK_5_0_DSC_ADDRESS = "VERSION_UDK_5_0_DSC_ADDRESS";
 	
@@ -37,7 +39,8 @@ public class IPlugVersionInspector {
 		if (plugDescription == null) {
 			return VERSION_UNKNOWN;
 		}
-		ArrayList fields = (ArrayList)plugDescription.get(PlugDescription.FIELDS);
+		@SuppressWarnings("unchecked")
+		ArrayList<String> fields = (ArrayList<String>)plugDescription.get(PlugDescription.FIELDS);
 		
 		// try to get the right iPlug Type (object/adress/generic)
 		if (fields != null && fields.contains("t01_object.data_language_key") && fields.contains("t01_object.metadata_language_key") && fields.contains("t02_address.country_key")) {
@@ -46,6 +49,8 @@ public class IPlugVersionInspector {
 			return VERSION_IDC_1_0_3_DSC_OBJECT;
 		} else if (fields != null && fields.contains("t01_object.obj_id") && fields.contains("parent.object_node.obj_uuid")) {
 			return VERSION_IDC_1_0_2_DSC_OBJECT;
+		} else if (fields != null && fields.contains("t02_address.adr_id") && fields.contains("parent.address_node.addr_uuid") && fields.contains("t02_address.country_key")) {
+			return VERSION_IDC_1_0_5_DSC_ADDRESS;
 		} else if (fields != null && fields.contains("t02_address.adr_id") && fields.contains("parent.address_node.addr_uuid")) {
 			return VERSION_IDC_1_0_2_DSC_ADDRESS;
 		} else if (IPlugHelper.hasDataType(plugDescription, Settings.QVALUE_DATATYPE_IPLUG_DSC_ECS)
