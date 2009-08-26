e1ffdbeaf6304b0f5115d02a28156bf1e8763fc5
ktt <ktt@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-08-26 09:26:07 +0000

    - add reload button in contact portlet for jcaptcha image

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/ContactPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/ContactPortlet.java
@@ -40,7 +40,7 @@ import de.ingrid.portal.om.IngridNewsletterData;
 public class ContactPortlet extends GenericVelocityPortlet {
 
     private final static Log log = LogFactory.getLog(ContactPortlet.class);
-
+    
     private final static String TEMPLATE_FORM_INPUT = "/WEB-INF/templates/contact.vm";
 
     private final static String TEMPLATE_SUCCESS = "/WEB-INF/templates/contact_success.vm";
@@ -49,6 +49,9 @@ public class ContactPortlet extends GenericVelocityPortlet {
 
     public final static String PARAMV_ACTION_SUCCESS = "doSuccess";
 
+    private final static String PARAMV_ACTION_DB_DO_REFRESH = "doRefresh";
+
+    
     public void init(PortletConfig config) throws PortletException {
         super.init(config);
     }
@@ -102,121 +105,130 @@ public class ContactPortlet extends GenericVelocityPortlet {
     public void processAction(ActionRequest request, ActionResponse actionResponse) throws PortletException,
             IOException {
         // check form input
-    	Boolean isResponseCorrect = false;
-        
-    	ContactForm cf = (ContactForm) Utils.getActionForm(request, ContactForm.SESSION_KEY, ContactForm.class);
-        cf.populate(request);
-        if (!cf.validate()) {
-            // add URL parameter indicating that portlet action was called
-            // before render request
-        	 	
-            String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, Settings.MSGV_TRUE);
-            actionResponse.sendRedirect(Settings.PAGE_CONTACT + urlViewParam);
+    	
+    	if (request.getParameter(PARAMV_ACTION_DB_DO_REFRESH) != null) {
+    		 
+    		ContactForm cf = (ContactForm) Utils.getActionForm(request, ContactForm.SESSION_KEY, ContactForm.class);
+            cf.populate(request);
             
-            return;
-        }
-
-        //remenber that we need an id to validate!
-        String sessionId = request.getPortletSession().getId();
-        //retrieve the response
-        String response = request.getParameter("jcaptcha");
-        // Call the Service method
-         try {
-             isResponseCorrect = CaptchaServiceSingleton.getInstance().validateResponseForID(sessionId,
-                     response);
-         } catch (CaptchaServiceException e) {
-              //should not happen, may be thrown if the id is not valid
-         }
-         
-         
-        if (isResponseCorrect){
-        	 try {
-                 IngridResourceBundle messages = new IngridResourceBundle(getPortletConfig().getResourceBundle(
-                         request.getLocale()));
-
-                 HashMap mailData = new HashMap();
-                 mailData.put("user.name.given", cf.getInput(ContactForm.FIELD_FIRSTNAME));
-                 mailData.put("user.name.family", cf.getInput(ContactForm.FIELD_LASTNAME));
-                 mailData.put("user.employer", cf.getInput(ContactForm.FIELD_COMPANY));
-                 mailData.put("user.business-info.telecom.telephone", cf.getInput(ContactForm.FIELD_PHONE));
-                 mailData.put("user.business-info.online.email", cf.getInput(ContactForm.FIELD_EMAIL));
-                 mailData.put("user.area.of.profession", messages.getString("contact.report.email.area.of.profession."
-                         + cf.getInput(ContactForm.FIELD_ACTIVITY)));
-                 mailData.put("user.interest.in.enviroment.info", messages
-                         .getString("contact.report.email.interest.in.enviroment.info."
-                                 + cf.getInput(ContactForm.FIELD_INTEREST)));
-                 if (cf.hasInput(ContactForm.FIELD_NEWSLETTER)) {
-                     Session session = HibernateUtil.currentSession();
-                     Transaction tx = null;
-
-                     try {
-
-                         mailData.put("user.subscribed.to.newsletter", "yes");
-                         // check for email address in newsletter list
-                         tx = session.beginTransaction();
-                         List newsletterDataList = session.createCriteria(IngridNewsletterData.class).add(
-                                 Restrictions.eq("emailAddress", cf.getInput(ContactForm.FIELD_EMAIL))).list();
-                         tx.commit();
-                         // register for newsletter if not already registered
-                         if (newsletterDataList.isEmpty()) {
-                             IngridNewsletterData data = new IngridNewsletterData();
-                             data.setFirstName(cf.getInput(ContactForm.FIELD_FIRSTNAME));
-                             data.setLastName(cf.getInput(ContactForm.FIELD_LASTNAME));
-                             data.setEmailAddress(cf.getInput(ContactForm.FIELD_EMAIL));
-                             data.setDateCreated(new Date());
-
-                             tx = session.beginTransaction();
-                             session.save(data);
-                             tx.commit();
-                         }
-                     } catch (Throwable t) {
-                         if (tx != null) {
-                             tx.rollback();
-                         }
-                         throw new PortletException(t.getMessage());
-                     } finally {
-                         HibernateUtil.closeSession();
-                     }
-                 } else {
-                     mailData.put("user.subscribed.to.newsletter", "no");
-                 }
-                 mailData.put("message.body", cf.getInput(ContactForm.FIELD_MESSAGE));
-
-                 Locale locale = request.getLocale();
-
-                 String language = locale.getLanguage();
-                 String localizedTemplatePath = EMAIL_TEMPLATE;
-                 int period = localizedTemplatePath.lastIndexOf(".");
-                 if (period > 0) {
-                     String fixedTempl = localizedTemplatePath.substring(0, period) + "_" + language + "."
-                             + localizedTemplatePath.substring(period + 1);
-                     if (new File(getPortletContext().getRealPath(fixedTempl)).exists()) {
-                         localizedTemplatePath = fixedTempl;
-                     }
-                 }
-
-                 String emailSubject = messages.getString("contact.report.email.subject");
-
-                 String from = cf.getInput(ContactForm.FIELD_EMAIL);
-                 String to = PortalConfig.getInstance().getString(PortalConfig.EMAIL_CONTACT_FORM_RECEIVER, "foo@bar.com");
-
-                 String text = Utils.mergeTemplate(getPortletConfig(), mailData, "map", localizedTemplatePath);
-                 Utils.sendEmail(from, emailSubject, new String[] { to }, text, null);
-             } catch (Throwable t) {
-                 cf.setError("", "Error sending mail from contact form.");
-                 log.error("Error sending mail from contact form.", t);
-             }
-
-             // temporarily show same page with content
-             String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, PARAMV_ACTION_SUCCESS);
+    		String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, Settings.MSGV_TRUE);
              actionResponse.sendRedirect(Settings.PAGE_CONTACT + urlViewParam);
-        }else{
-       		cf.setErrorCaptcha();
-       		String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, Settings.MSGV_TRUE);
-            actionResponse.sendRedirect(Settings.PAGE_CONTACT + urlViewParam);
-            return;
-        }
-        
-       
+             return;
+    	}else{
+    		Boolean isResponseCorrect = false;
+            
+        	ContactForm cf = (ContactForm) Utils.getActionForm(request, ContactForm.SESSION_KEY, ContactForm.class);
+            cf.populate(request);
+            if (!cf.validate()) {
+                // add URL parameter indicating that portlet action was called
+                // before render request
+            	 	
+                String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, Settings.MSGV_TRUE);
+                actionResponse.sendRedirect(Settings.PAGE_CONTACT + urlViewParam);
+                
+                return;
+            }
+
+            //remenber that we need an id to validate!
+            String sessionId = request.getPortletSession().getId();
+            //retrieve the response
+            String response = request.getParameter("jcaptcha");
+            // Call the Service method
+             try {
+                 isResponseCorrect = CaptchaServiceSingleton.getInstance().validateResponseForID(sessionId,
+                         response);
+             } catch (CaptchaServiceException e) {
+                  //should not happen, may be thrown if the id is not valid
+             }
+             
+             
+            if (isResponseCorrect){
+            	 try {
+                     IngridResourceBundle messages = new IngridResourceBundle(getPortletConfig().getResourceBundle(
+                             request.getLocale()));
+
+                     HashMap mailData = new HashMap();
+                     mailData.put("user.name.given", cf.getInput(ContactForm.FIELD_FIRSTNAME));
+                     mailData.put("user.name.family", cf.getInput(ContactForm.FIELD_LASTNAME));
+                     mailData.put("user.employer", cf.getInput(ContactForm.FIELD_COMPANY));
+                     mailData.put("user.business-info.telecom.telephone", cf.getInput(ContactForm.FIELD_PHONE));
+                     mailData.put("user.business-info.online.email", cf.getInput(ContactForm.FIELD_EMAIL));
+                     mailData.put("user.area.of.profession", messages.getString("contact.report.email.area.of.profession."
+                             + cf.getInput(ContactForm.FIELD_ACTIVITY)));
+                     mailData.put("user.interest.in.enviroment.info", messages
+                             .getString("contact.report.email.interest.in.enviroment.info."
+                                     + cf.getInput(ContactForm.FIELD_INTEREST)));
+                     if (cf.hasInput(ContactForm.FIELD_NEWSLETTER)) {
+                         Session session = HibernateUtil.currentSession();
+                         Transaction tx = null;
+
+                         try {
+
+                             mailData.put("user.subscribed.to.newsletter", "yes");
+                             // check for email address in newsletter list
+                             tx = session.beginTransaction();
+                             List newsletterDataList = session.createCriteria(IngridNewsletterData.class).add(
+                                     Restrictions.eq("emailAddress", cf.getInput(ContactForm.FIELD_EMAIL))).list();
+                             tx.commit();
+                             // register for newsletter if not already registered
+                             if (newsletterDataList.isEmpty()) {
+                                 IngridNewsletterData data = new IngridNewsletterData();
+                                 data.setFirstName(cf.getInput(ContactForm.FIELD_FIRSTNAME));
+                                 data.setLastName(cf.getInput(ContactForm.FIELD_LASTNAME));
+                                 data.setEmailAddress(cf.getInput(ContactForm.FIELD_EMAIL));
+                                 data.setDateCreated(new Date());
+
+                                 tx = session.beginTransaction();
+                                 session.save(data);
+                                 tx.commit();
+                             }
+                         } catch (Throwable t) {
+                             if (tx != null) {
+                                 tx.rollback();
+                             }
+                             throw new PortletException(t.getMessage());
+                         } finally {
+                             HibernateUtil.closeSession();
+                         }
+                     } else {
+                         mailData.put("user.subscribed.to.newsletter", "no");
+                     }
+                     mailData.put("message.body", cf.getInput(ContactForm.FIELD_MESSAGE));
+
+                     Locale locale = request.getLocale();
+
+                     String language = locale.getLanguage();
+                     String localizedTemplatePath = EMAIL_TEMPLATE;
+                     int period = localizedTemplatePath.lastIndexOf(".");
+                     if (period > 0) {
+                         String fixedTempl = localizedTemplatePath.substring(0, period) + "_" + language + "."
+                                 + localizedTemplatePath.substring(period + 1);
+                         if (new File(getPortletContext().getRealPath(fixedTempl)).exists()) {
+                             localizedTemplatePath = fixedTempl;
+                         }
+                     }
+
+                     String emailSubject = messages.getString("contact.report.email.subject");
+
+                     String from = cf.getInput(ContactForm.FIELD_EMAIL);
+                     String to = PortalConfig.getInstance().getString(PortalConfig.EMAIL_CONTACT_FORM_RECEIVER, "foo@bar.com");
+
+                     String text = Utils.mergeTemplate(getPortletConfig(), mailData, "map", localizedTemplatePath);
+                     Utils.sendEmail(from, emailSubject, new String[] { to }, text, null);
+                 } catch (Throwable t) {
+                     cf.setError("", "Error sending mail from contact form.");
+                     log.error("Error sending mail from contact form.", t);
+                 }
+
+                 // temporarily show same page with content
+                 String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, PARAMV_ACTION_SUCCESS);
+                 actionResponse.sendRedirect(Settings.PAGE_CONTACT + urlViewParam);
+            }else{
+           		cf.setErrorCaptcha();
+           		String urlViewParam = "?" + Utils.toURLParam(Settings.PARAM_ACTION, Settings.MSGV_TRUE);
+                actionResponse.sendRedirect(Settings.PAGE_CONTACT + urlViewParam);
+                return;
+            }
+    	}    
     }
 }
