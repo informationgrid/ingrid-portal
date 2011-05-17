8204f64d1a9e1b529b351bf67ca7731401f1e421
ktt <ktt@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2011-05-17 14:30:47 +0000

    [PORTAL]: Update Thesaurus-Navigator
    - Add "t04_search.type:G" to search query.

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/searchcatalog/SearchCatalogThesaurusResultPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/searchcatalog/SearchCatalogThesaurusResultPortlet.java
@@ -177,7 +177,7 @@ public class SearchCatalogThesaurusResultPortlet extends GenericVelocityPortlet 
     	if (selectedDS.equals(Settings.PARAMV_DATASOURCE_ENVINFO)) {
     		fullQueryZeigeAlle = "(";
     		fullQueryZeigeAlle += "t04_search.searchterm:\"" + UtilsString.escapeChars(queryThesaurusTerm, "\"") + "\"";
-    		fullQueryZeigeAlle += " (t04_search.type:2 OR t04_search.type:T)";
+    		fullQueryZeigeAlle += " (t04_search.type:2 OR t04_search.type:T OR t04_search.type:G)";
     		fullQueryZeigeAlle += getSearchDataTypes();
     		fullQueryZeigeAlle += ")";
     		fullQueryZeigeAlle += " OR ";
@@ -188,7 +188,7 @@ public class SearchCatalogThesaurusResultPortlet extends GenericVelocityPortlet 
     	}
         else {
             fullQueryZeigeAlle = "t04_search.searchterm:\"" + UtilsString.escapeChars(queryThesaurusTerm, "\"") + "\"";
-        	fullQueryZeigeAlle += " (t04_search.type:4 OR t04_search.type:T)";
+        	fullQueryZeigeAlle += " (t04_search.type:4 OR t04_search.type:T OR t04_search.type:G)";
         	fullQueryZeigeAlle += " "+Settings.QFIELD_DATATYPE+":"+Settings.QVALUE_DATATYPE_IPLUG_DSC_ECS_ADDRESS;
         }
         SearchState.adaptSearchState(request, Settings.PARAM_QUERY_STRING, fullQueryZeigeAlle, SEARCH_STATE_TOPIC);
