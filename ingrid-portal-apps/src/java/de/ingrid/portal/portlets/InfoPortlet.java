50490ab18a4c8ebe697745fd95b13fccd187d480
ktt <ktt@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-01-26 08:12:30 +0000

    [PORTAL]: BAW disable/remove Newsletter.

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/InfoPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/InfoPortlet.java
@@ -62,14 +62,14 @@ public class InfoPortlet extends GenericVelocityPortlet {
             context.put("enableChronicle", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_CHRONICLE, Boolean.FALSE));
             context.put("enableSearchCatalog", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_SEARCH_CATALOG, Boolean.FALSE));
             context.put("enableSearchCatalogThesaurus", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_SEARCH_CATALOG_THESAURUS, Boolean.FALSE));
-            context.put("enableNewsletter", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_NEWSLETTER, Boolean.FALSE));
-            context.put("enableFeature", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_FEATURE_TYPE, Boolean.FALSE));
             context.put("enableSearchSimpleOptionalLinks", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_SEARCH_SIMPLE_OPTIONAL_LINKS, Boolean.TRUE));
-            context.put("enableApplication", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_APPLICATION, Boolean.FALSE));
         }
         if(myView.indexOf("search_cat_thesaurus_info_sns.vm") > -1 || myView.indexOf("search_settings_info.vm") > -1){
         	context.put("thesaurusLink", PortalConfig.getInstance().getString(PortalConfig.THESAURUS_INFO_LINK, ""));
         }
+        context.put("enableApplication", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_APPLICATION, Boolean.FALSE));
+        context.put("enableNewsletter", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_NEWSLETTER, Boolean.FALSE));
+        context.put("enableFeature", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_FEATURE_TYPE, Boolean.FALSE));
         context.put("enableFeatureType", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_FEATURE_TYPE, false));
         super.doView(request, response);
     }
