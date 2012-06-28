9de0505f9675bcb5d9c4feaf819f9d49bae00014
ktt <ktt@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-06-28 08:03:32 +0000

    [PORTAL]: Update ranked hit detail link.

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/UtilsSearch.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/UtilsSearch.java
@@ -240,7 +240,11 @@ public class UtilsSearch {
             }
             result.put(Settings.RESULT_KEY_ABSTRACT, UtilsString.cutString(summary.replaceAll("\\<.*?\\>",
                     ""), 400));
-            result.put(Settings.RESULT_KEY_DOC_ID, new Integer(result.getHit().getDocumentId()));
+            if(PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_MAPS, false)){
+	              if(UtilsSearch.getDetailValue(detail, "kml").length() > 0){
+	            	  result.put(Settings.RESULT_KEY_DOC_ID, new Integer(result.getHit().getDocumentId()));
+	              }
+            }
             
             // use internal provider instead of one set in plugdescription
             // e.g. results from Opensearch might support partner and provider
