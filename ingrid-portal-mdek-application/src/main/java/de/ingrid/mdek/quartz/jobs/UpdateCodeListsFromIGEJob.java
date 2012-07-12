17d6a668dee0837dada40e4ef74d0493d3232787
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-07-12 10:44:46 +0000

    - only try to update codelists if at least one iplug is connected

--- ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/quartz/jobs/UpdateCodeListsFromIGEJob.java
+++ ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/quartz/jobs/UpdateCodeListsFromIGEJob.java
@@ -28,21 +28,33 @@ public class UpdateCodeListsFromIGEJob extends UpdateCodeListsJob {
         
         CodeListService clService = getClServiceFromBean(jobExecutionContext);
         Long timestamp = getLastModifiedTimestampFromDb();
-        boolean success = clService.updateFromServer(timestamp);
         
-        // update db with initial codelists from service if no codelist could be fetched
-        // and there never has been an update to the db (timestamp does not exist!)
-        if (success == false && timestamp == -1) {
-            clService.persistToAll(clService.getCodeLists());
-        }
-        
-        if (log.isDebugEnabled()) {
-            log.debug("UpdateCodeListsFromIGEJob finished! (successful = " + success + ")");
+        // if at least one iPlug was found/connected
+        if (timestamp != null) {
+            boolean success = clService.updateFromServer(timestamp);
+            
+            // update db with initial codelists from service if no codelist could be fetched
+            // and there never has been an update to the db (timestamp does not exist!)
+            if (success == false && timestamp == -1) {
+                clService.persistToAll(clService.getCodeLists());
+            }
+            
+            if (log.isDebugEnabled()) {
+                log.debug("UpdateCodeListsFromIGEJob finished! (successful = " + success + ")");
+            }
+        } else {
+            if (log.isDebugEnabled()) {
+                log.debug("No iPlug connected to update codelists to!");
+            }
         }
     }
 
     private Long getLastModifiedTimestampFromDb() {
         List<String> iplugList = connectionFacade.getMdekClientCaller().getRegisteredIPlugs();
+        
+        if (iplugList.isEmpty())
+            return null;
+        
         Long lowestTimestamp = null;
         for (String iplug : iplugList) {
             IngridDocument response = connectionFacade.getMdekCallerCatalog().getLastModifiedTimestampOfSyslists(iplug, getCatAdminUuid(iplug));
