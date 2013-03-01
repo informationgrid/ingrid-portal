e502b863bfd06b318d0f1abf7241ea28f259f4c1
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2013-03-01 15:35:58 +0000

    - removed not continued project

--- ingrid-portal-test/src/selenium.ide.extensions/capture_click.js
+++
@@ -1,6 +0,0 @@
-Recorder.removeEventHandler('clickLocator');
-Recorder.addEventHandler('clickLocator', 'click', function(event) {
-if (event.button == 0) {
-this.clickLocator = this.findLocator(event.target);
-}
-}, { capture: true });
