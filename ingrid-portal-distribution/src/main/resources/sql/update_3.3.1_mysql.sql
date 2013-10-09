9836dd9cdfd0640fcae5d1ca46d82e5ecd3ee246
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2013-10-09 09:38:49 +0000

    portal-distribution: - Merged Rev. 22736 from [BRANCH-3.3.x] + correction ! -> splitted/renamed sql files in 3.4 and 3.3.1 to separate between 3.3.1 and 3.4 release ! New help will be added to mdek.sql in 3.3 branch, see https://redmine.wemove.com/issues/118

--- ingrid-portal-distribution/src/main/resources/sql/update_3.3.1_mysql.sql
+++ ingrid-portal-distribution/src/main/resources/sql/update_3.3.1_mysql.sql
@@ -1,5 +1,5 @@
 -- DB Version erhöhen
-UPDATE ingrid_lookup SET item_value = '3.4.0', item_date = SYSDATE WHERE item_key ='ingrid_db_version';
+UPDATE ingrid_lookup SET item_value = '3.3.1', item_date = NOW() WHERE item_key ='ingrid_db_version';
 
 -- Fix link to wemove page in Impressum Content
 UPDATE ingrid_cms_item SET item_value = replace(item_value, 'www.wemove.com/contact.php', 'www.wemove.com/') WHERE item_title = 'Impressum';
