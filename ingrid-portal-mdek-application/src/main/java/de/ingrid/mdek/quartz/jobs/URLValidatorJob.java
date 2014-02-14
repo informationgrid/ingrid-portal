4c0da1a58da3812f7e89d2fa0a67c5fa18c641c7
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-02-14 17:42:33 +0000

    merge from 3.3.x branch:
    - IGE: Fix url validation if a proxy has to be used on the installed system.

--- ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/quartz/jobs/URLValidatorJob.java
+++ ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/quartz/jobs/URLValidatorJob.java
@@ -40,7 +40,7 @@ import de.ingrid.utils.capabilities.CapabilitiesUtils;
 
 public class URLValidatorJob extends QuartzJobBean implements MdekJob, InterruptableJob {
 
-	private final static Logger log = Logger.getLogger(URLValidatorJob.class);	
+    private final static Logger log = Logger.getLogger(URLValidatorJob.class);
 
 	private static final String JOB_BASE_NAME = "urlValidatorJob_";
 	private static final String JOB_LISTENER_SUFFIX = "_listener";
@@ -51,49 +51,48 @@ public class URLValidatorJob extends QuartzJobBean implements MdekJob, Interrupt
 	public static final String END_TIME = "endTime";
 	public static final int NUM_THREADS = 10;
 
-	private final static int CONNECTION_TIMEOUT = 5000;
-	private final static int SOCKET_TIMEOUT = 5000;
+    private final static int CONNECTION_TIMEOUT = 5000;
+    private final static int SOCKET_TIMEOUT = 5000;
 
-	private final String plugId;
-	private final String jobName; 
-	private final IMdekCallerQuery mdekCallerQuery;
-	private final JobDetail jobDetail;
-	private final URLValidatorJobListener jobListener;
-	private Scheduler scheduler;
+    private final String plugId;
+    private final String jobName;
+    private final IMdekCallerQuery mdekCallerQuery;
+    private final JobDetail jobDetail;
+    private final URLValidatorJobListener jobListener;
+    private Scheduler scheduler;
 
-	// Flag that signals if the job execution should be interrupted
-	private boolean cancelJob;
+    // Flag that signals if the job execution should be interrupted
+    private boolean cancelJob;
 
+    // No args constructor is required for the job to be scheduled by quartz
+    public URLValidatorJob() {
+        this.plugId = null;
+        this.jobName = null;
+        this.mdekCallerQuery = null;
+        this.jobDetail = null;
+        this.jobListener = null;
+        this.cancelJob = false;
+    }
 
-	// No args constructor is required for the job to be scheduled by quartz
-	public URLValidatorJob() {
-		this.plugId = null;
-		this.jobName = null;
-		this.mdekCallerQuery = null;
-		this.jobDetail = null;
-		this.jobListener = null;
-		this.cancelJob = false;
-	}
+    public URLValidatorJob(ConnectionFacade connectionFacade) {
+        this.mdekCallerQuery = connectionFacade.getMdekCallerQuery();
+        this.plugId = connectionFacade.getCurrentPlugId();
+        String currentUserUuid = MdekSecurityUtils.getCurrentUserUuid();
+        jobName = createJobName(plugId);
+        String jobListenerName = createJobListenerName(plugId);
+        jobListener = new URLValidatorJobListener(jobListenerName, connectionFacade.getMdekCallerCatalog(), currentUserUuid, plugId);
+        jobDetail = createJobDetail();
+    }
 
-	public URLValidatorJob(ConnectionFacade connectionFacade) {
-		this.mdekCallerQuery = connectionFacade.getMdekCallerQuery();
-		this.plugId = connectionFacade.getCurrentPlugId();
-		String currentUserUuid = MdekSecurityUtils.getCurrentUserUuid();
-		jobName = createJobName(plugId);
-		String jobListenerName = createJobListenerName(plugId);
-		jobListener = new URLValidatorJobListener(jobListenerName, connectionFacade.getMdekCallerCatalog(), currentUserUuid, plugId);
-		jobDetail = createJobDetail();
-	}
+    public static String createJobName(String plugId) {
+        return (JOB_BASE_NAME + plugId);
+    }
 
-	public static String createJobName(String plugId) {
-		return (JOB_BASE_NAME + plugId);
-	}
+    private static String createJobListenerName(String plugId) {
+        return (JOB_BASE_NAME + plugId + JOB_LISTENER_SUFFIX);
+    }
 
-	private static String createJobListenerName(String plugId) {
-		return (JOB_BASE_NAME + plugId + JOB_LISTENER_SUFFIX);
-	}
-
-	private JobDetail createJobDetail() {
+    private JobDetail createJobDetail() {
 		List<URLObjectReference> urlObjectReferences = fetchLinkUrls();
 		List<URLObjectReference> capabilitiesReferences = fetchCapabilitiesUrls();
 		Map<String, URLState> urlMap = createUrlMap(urlObjectReferences);
@@ -105,24 +104,23 @@ public class URLValidatorJob extends QuartzJobBean implements MdekJob, Interrupt
 		dataMap.put(URLValidatorJob.URL_OBJECT_REFERENCES, urlObjectReferences);
 		dataMap.put(URLValidatorJob.CAPABILITIES_REFERENCES, capabilitiesReferences);
 
-		JobDetail jobDetail = new JobDetail(jobName, Scheduler.DEFAULT_GROUP, URLValidatorJob.class);
-		JobDataMap jobDataMap = new JobDataMap(dataMap);
-		jobDetail.setJobDataMap(jobDataMap);
-		jobDetail.addJobListener(jobListener.getName());
+        JobDetail jobDetail = new JobDetail(jobName, Scheduler.DEFAULT_GROUP, URLValidatorJob.class);
+        JobDataMap jobDataMap = new JobDataMap(dataMap);
+        jobDetail.setJobDataMap(jobDataMap);
+        jobDetail.addJobListener(jobListener.getName());
 
-		return jobDetail;
-	}
+        return jobDetail;
+    }
+
+    private Map<String, URLState> createUrlMap(List<URLObjectReference> urlObjectReferences) {
+        Map<String, URLState> urlMap = new HashMap<String, URLState>();
+        for (URLObjectReference ref : urlObjectReferences) {
+            urlMap.put(ref.getUrlState().getUrl(), ref.getUrlState());
+        }
+        return urlMap;
+    }
 
 
-	private Map<String, URLState> createUrlMap(List<URLObjectReference> urlObjectReferences) {
-		Map<String, URLState> urlMap = new HashMap<String, URLState>();
-		for (URLObjectReference ref : urlObjectReferences) {
-			urlMap.put(ref.getUrlState().getUrl(), ref.getUrlState());
-		}
-		return urlMap;
-	}
-
-	
 	private List<URLObjectReference> fetchCapabilitiesUrls() {
 	    // Fetch all URLs for objects that are published and not modified (objIdPub = objId)
 	    //SELECT connect_pont FROM `t011_obj_serv_op_connpoint` CP, t011_obj_serv_operation OP where CP.obj_serv_op_id=OP.id AND OP.name_key=1
@@ -202,155 +200,157 @@ public class URLValidatorJob extends QuartzJobBean implements MdekJob, Interrupt
 			scheduler.triggerJobWithVolatileTrigger(jobName, Scheduler.DEFAULT_GROUP);
 			return true;
 
-		} else {
-			return false;
-		}
-	}
+        } else {
+            return false;
+        }
+    }
 
-	@Override
+    @Override
 	protected void executeInternal(JobExecutionContext jobExecutionContext)
 			throws JobExecutionException {
 
-		ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
-		JobDataMap mergedJobDataMap = jobExecutionContext.getMergedJobDataMap();
-		Map<String, URLState> urlMap = (Map<String, URLState>) mergedJobDataMap.get(URL_MAP);
-		List<URLValidator> validatorTasks = new ArrayList<URLValidator>(urlMap.size());
-		
+        ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
+        JobDataMap mergedJobDataMap = jobExecutionContext.getMergedJobDataMap();
+        Map<String, URLState> urlMap = (Map<String, URLState>) mergedJobDataMap.get(URL_MAP);
+        List<URLValidator> validatorTasks = new ArrayList<URLValidator>(urlMap.size());
+
 		Map<String, URLState> capabilitiesMap = (Map<String, URLState>) mergedJobDataMap.get(CAP_URL_MAP);
 		List<CapabilitiesValidator> capabilitiesValidatorTasks = new ArrayList<CapabilitiesValidator>(capabilitiesMap.size());
 
-		HttpClientParams httpClientParams = new HttpClientParams();
-		httpClientParams.setConnectionManagerTimeout(0);
-		httpClientParams.setSoTimeout(SOCKET_TIMEOUT);
-		HttpConnectionParams httpParams = new HttpConnectionParams();
-		httpParams.setConnectionTimeout(CONNECTION_TIMEOUT);
-		httpClientParams.setDefaults(httpParams);
-		HttpClient httpClient = new HttpClient(httpClientParams, new MultiThreadedHttpConnectionManager());
-		for (URLState urlState : urlMap.values()) {
-			validatorTasks.add(new URLValidator(httpClient, urlState));
-		}
+        HttpClientParams httpClientParams = new HttpClientParams();
+        httpClientParams.setConnectionManagerTimeout(0);
+        httpClientParams.setSoTimeout(SOCKET_TIMEOUT);
+        HttpConnectionParams httpParams = new HttpConnectionParams();
+        httpParams.setConnectionTimeout(CONNECTION_TIMEOUT);
+        httpClientParams.setDefaults(httpParams);
+        HttpClient httpClient = new HttpClient(httpClientParams, new MultiThreadedHttpConnectionManager());
+        if (System.getProperty("http.proxyHost") != null && System.getProperty("http.proxyPort") != null) {
+            httpClient.getHostConfiguration().setProxy(System.getProperty("http.proxyHost"), Integer.parseInt(System.getProperty("http.proxyPort")));
+        }
+        for (URLState urlState : urlMap.values()) {
+            validatorTasks.add(new URLValidator(httpClient, urlState));
+        }
 		for (URLState urlState : capabilitiesMap.values()) {
 		    capabilitiesValidatorTasks.add(new CapabilitiesValidator(httpClient, urlState));
 		}
 
-		log.debug("Starting url validation...");
-		long startTime = System.currentTimeMillis();
-		List<Future<URLState>> resultFutureList = new ArrayList<Future<URLState>>();
-		for (URLValidator validator : validatorTasks) {
-			resultFutureList.add(executorService.submit(validator));
-		}
+        log.debug("Starting url validation...");
+        long startTime = System.currentTimeMillis();
+        List<Future<URLState>> resultFutureList = new ArrayList<Future<URLState>>();
+        for (URLValidator validator : validatorTasks) {
+            resultFutureList.add(executorService.submit(validator));
+        }
 		for (CapabilitiesValidator validator : capabilitiesValidatorTasks) {
 		    resultFutureList.add(executorService.submit(validator));
 		}
 
-		for (Future<URLState> future : resultFutureList) {
-			try {
-				if (!cancelJob) {
-					future.get();
+        for (Future<URLState> future : resultFutureList) {
+            try {
+                if (!cancelJob) {
+                    future.get();
 
-				} else {
-					log.debug("forcing shutdown of executor service...");
-					executorService.shutdownNow();
-					break;
-				}
+                } else {
+                    log.debug("forcing shutdown of executor service...");
+                    executorService.shutdownNow();
+                    break;
+                }
+		
+            } catch (Exception ex) {
+                log.debug("Exception while fetching result from future.", ex);
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        log.debug("URL Validation took " + (endTime - startTime) + " ms.");
 
-			} catch (Exception ex) {
-				log.debug("Exception while fetching result from future.", ex);
-			}
-		}
-		long endTime = System.currentTimeMillis();
-		log.debug("URL Validation took "+(endTime - startTime)+" ms.");
+        executorService.shutdown();
 
-		executorService.shutdown();
-
-		// Only store if job was not cancelled
-		if (!cancelJob) {
+        // Only store if job was not cancelled
+        if (!cancelJob) {
 		    Map<String, List<URLObjectReference>> map = new HashMap<String, List<URLObjectReference>>();
 		    map.put(MdekKeys.URL_RESULT, (List<URLObjectReference>)mergedJobDataMap.get(URL_OBJECT_REFERENCES));
 		    map.put(MdekKeys.CAP_RESULT, (List<URLObjectReference>)mergedJobDataMap.get(CAPABILITIES_REFERENCES));
 			jobExecutionContext.setResult(map);
-		}
-	}
+        }
+    }
 
-	// Retrieves the current JobExecutionContext
-	// If the job is not running, null is returned
-	private JobExecutionContext getJobExecutionContext() {
-		try {
-			if (scheduler != null) {
-				List<JobExecutionContext> executionContextList = scheduler.getCurrentlyExecutingJobs();
-				for (JobExecutionContext executionContext : executionContextList) {
-					if (jobName.equals(executionContext.getJobDetail().getName())) {
-						return executionContext;
-					}
-				}
-			}
-		} catch (SchedulerException ex) {			
-			log.debug("Error while fetching job execution context.", ex);
-		}
-		return null;
-	}
+    // Retrieves the current JobExecutionContext
+    // If the job is not running, null is returned
+    private JobExecutionContext getJobExecutionContext() {
+        try {
+            if (scheduler != null) {
+                List<JobExecutionContext> executionContextList = scheduler.getCurrentlyExecutingJobs();
+                for (JobExecutionContext executionContext : executionContextList) {
+                    if (jobName.equals(executionContext.getJobDetail().getName())) {
+                        return executionContext;
+                    }
+                }
+            }
+        } catch (SchedulerException ex) {
+            log.debug("Error while fetching job execution context.", ex);
+        }
+        return null;
+    }
 
-	public JobInfoBean getRunningJobInfo() {
-		JobExecutionContext executionContext = getJobExecutionContext();
+    public JobInfoBean getRunningJobInfo() {
+        JobExecutionContext executionContext = getJobExecutionContext();
 
-		if (executionContext != null) {
-			Map<String, URLState> urlMap = (Map<String, URLState>) executionContext.getMergedJobDataMap().get(URLValidatorJob.URL_MAP);
+        if (executionContext != null) {
+            Map<String, URLState> urlMap = (Map<String, URLState>) executionContext.getMergedJobDataMap().get(URLValidatorJob.URL_MAP);
 			Map<String, URLState> capMap = (Map<String, URLState>) executionContext.getMergedJobDataMap().get(URLValidatorJob.CAP_URL_MAP);
 			
 			// merge capabilities urls with normal urls
 			urlMap.putAll(capMap);
 			
-			int totalNumberOfUrls = urlMap.size();
-			int numberOfProcessedUrls = 0;
-			for (URLState urlState : urlMap.values()) {
-				if (State.NOT_CHECKED != urlState.getState()) {
-					++numberOfProcessedUrls;
-				}
-			}
+            int totalNumberOfUrls = urlMap.size();
+            int numberOfProcessedUrls = 0;
+            for (URLState urlState : urlMap.values()) {
+                if (State.NOT_CHECKED != urlState.getState()) {
+                    ++numberOfProcessedUrls;
+                }
+            }
 
-			JobInfoBean jobInfoResult = new JobInfoBean();
-			jobInfoResult.setDescription(jobName);
-			jobInfoResult.setEntityType(EntityType.URL);
-			jobInfoResult.setNumEntities(totalNumberOfUrls);
-			jobInfoResult.setNumProcessedEntities(numberOfProcessedUrls);
-			jobInfoResult.setStartTime(executionContext.getFireTime());
-			return jobInfoResult;
+            JobInfoBean jobInfoResult = new JobInfoBean();
+            jobInfoResult.setDescription(jobName);
+            jobInfoResult.setEntityType(EntityType.URL);
+            jobInfoResult.setNumEntities(totalNumberOfUrls);
+            jobInfoResult.setNumProcessedEntities(numberOfProcessedUrls);
+            jobInfoResult.setStartTime(executionContext.getFireTime());
+            return jobInfoResult;
 
-		} else {
-			return null;
-		}
-	}
+        } else {
+            return null;
+        }
+    }
 
+    public boolean isRunning() {
+        if (scheduler != null) {
+            return getJobExecutionContext() != null;
 
-	public boolean isRunning() {
-		if (scheduler != null) {
-			return getJobExecutionContext() != null;
+        } else {
+            return false;
+        }
+    }
 
-		} else {
-			return false;
-		}
-	}
+    public void stop() {
+        try {
+            log.debug("trying to interrupt job via scheduler...");
+            scheduler.interrupt(jobName, Scheduler.DEFAULT_GROUP);
+            scheduler.deleteJob(jobName, Scheduler.DEFAULT_GROUP);
 
-	public void stop() {
-		try {
-			log.debug("trying to interrupt job via scheduler...");
-			scheduler.interrupt(jobName, Scheduler.DEFAULT_GROUP);
-			scheduler.deleteJob(jobName, Scheduler.DEFAULT_GROUP);
+        } catch (UnableToInterruptJobException ex) {
+            log.error("Error interrupting URL Validation job.", ex);
 
-		} catch (UnableToInterruptJobException ex) {
-			log.error("Error interrupting URL Validation job.", ex);
-		
-		} catch (SchedulerException ex) {
-			log.error("Error interrupting URL Validation job.", ex);
-		}
- 
-	}
+        } catch (SchedulerException ex) {
+            log.error("Error interrupting URL Validation job.", ex);
+        }
 
-	public String getName() {
-		return jobName;
-	}
+    }
 
-	public void interrupt() throws UnableToInterruptJobException {
-		cancelJob = true;
-	}
+    public String getName() {
+        return jobName;
+    }
+
+    public void interrupt() throws UnableToInterruptJobException {
+        cancelJob = true;
+    }
 }
