d36c4a6642a424bba27b6aac7f7a24282dbc05c8
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-05-06 16:19:55 +0000

    - fixed openData behavior: when openData selected and object class changed then categories also have to disappear - fixed wrong behavior of openData when loading another object with a different openData state (event difference between onChange and onClick!!! -> onChange only does view changes and onClick only changes on data)

--- ingrid-portal-mdek-application/src/main/webapp/js/rules_required.js
+++ ingrid-portal-mdek-application/src/main/webapp/js/rules_required.js
@@ -218,12 +218,61 @@ function applyRuleOpenData() {
     dojo.subscribe("/onObjectClassChange", function(data) { 
         if (data.objClass === "Class0" || data.objClass === "Class4") {
             dojo.addClass(dojo.byId("uiElement6010"), "hide");
+            // also uncheck openData checkbox, so that categories table must not be
+            // displayed according to the state
+            dijit.byId("isOpenData").set("checked", false);
         } else {
             dojo.removeClass(dojo.byId("uiElement6010"), "hide");
         }
     });
     
-    // TODO: change to onClick!!! otherwise it will be called when object is loaded!
+    // this event will also execute when object is loaded
+    // here we have to make sure the categories table is shown/removed correctly
+    dojo.connect(dijit.byId("isOpenData"), "onChange", function(isChecked) {
+        // get link for use constraints which will be changed depending on the
+        // state of the open data checkbox
+        var link = dojo.byId("availabilityUseConstraintsLink");
+        var onclickValue = link.attributes.onclick.nodeValue;
+        
+        if (this.checked) {
+            // show categories
+            dojo.removeClass("uiElement6020", "hide");
+            
+            // make field mandatory
+            dojo.addClass("uiElement6020", "required");
+            
+            // change codelist for 'availabilityUseConstraints'
+            link.attributes.onclick.nodeValue = onclickValue.replace(/(listId:).*'}/, "$1 '6500'}");
+            
+            // add check for url reference of type download when publishing
+            // we check name and not id cause is combo box ! id not adapted yet if not saved !
+            openDataLinkCheck = dojo.subscribe("/onBeforeObjectPublish", function(/*Array*/notPublishableIDs) {
+                // get name of codelist entry for entry-id "9990" = "Download of data"/"Datendownload"
+                var entryNameDownload = UtilSyslist.getSyslistEntryName(2000, 9990);
+                var data = UtilGrid.getTableData("linksTo");
+                var containsDownloadLink = dojo.some(data, function(item) { if (item.relationTypeName == entryNameDownload) return true; });
+                if (!containsDownloadLink)
+                    notPublishableIDs.push("linksTo");
+            });
+            
+        } else {
+            // change codelist for 'availabilityUseConstraints'
+            link.attributes.onclick.nodeValue = onclickValue.replace(/(listId:).*'}/, "$1 '6020'}");
+            
+            // hide categories
+            dojo.addClass("uiElement6020", "hide");
+            
+            // revert field mandatory
+            dojo.removeClass("uiElement6020", "required");
+            
+            // unregister from check for download link
+            if (openDataLinkCheck)
+                dojo.unsubscribe(openDataLinkCheck);
+        }
+    });
+    
+    // behavior when the checkbox is actively clicked by a user
+    // only actions that shall modify data once and has nothing to do with the view!
     dojo.connect(dijit.byId("isOpenData"), "onClick", function(isChecked) {
         // get link for use constraints which will be changed depending on the
         // state of the open data checkbox
@@ -261,44 +310,12 @@ function applyRuleOpenData() {
             def.then(function() {
                 // automatically replace access constraint with "keine"
                 var data = [{title: UtilSyslist.getSyslistEntryName(6010, 1) }];
-                UtilGrid.setTableData('availabilityAccessConstraints', data);
-                
-                // change codelist for 'availabilityUseConstraints'
-                link.attributes.onclick.nodeValue = onclickValue.replace(/(listId:).*'}/, "$1 '6500'}");
-                
-                // show categories
-                dojo.removeClass("uiElement6020", "hide");
-                
-                // make field mandatory
-                dojo.addClass("uiElement6020", "required");
-                
-                // add check for url reference of type download when publishing
-                // we check name and not id cause is combo box ! id not adapted yet if not saved !
-                openDataLinkCheck = dojo.subscribe("/onBeforeObjectPublish", function(/*Array*/notPublishableIDs) {
-                    // get name of codelist entry for entry-id "9990" = "Download of data"/"Datendownload"
-                    var entryNameDownload = UtilSyslist.getSyslistEntryName(2000, 9990);
-                    var data = UtilGrid.getTableData("linksTo");
-                    var containsDownloadLink = dojo.some(data, function(item) { if (item.relationTypeName == entryNameDownload) return true; });
-                    if (!containsDownloadLink)
-                        notPublishableIDs.push("linksTo");
-                });
+                UtilGrid.setTableData('availabilityAccessConstraints', data);                
             });
+            
         } else {
-            // change codelist for 'availabilityUseConstraints'
-            link.attributes.onclick.nodeValue = onclickValue.replace(/(listId:).*'}/, "$1 '6020'}");
-            
-            // hide categories
-            dojo.addClass("uiElement6020", "hide");
-            
             // remove all categories
             UtilGrid.setTableData("categoriesOpenData", []);
-            
-            // revert field mandatory
-            dojo.removeClass("uiElement6020", "required");
-            
-            // unregister from check for download link
-            if (openDataLinkCheck)
-                dojo.unsubscribe(openDataLinkCheck);
         }
     });
 }
