1505f1adeb66096a73026f4e000ded5f50dde17b
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-09-16 12:44:38 +0000

    portal-base[branch 3.5_REDMINE-304]: - adapt XXSUrlAttackFilter.java to JS23, see https://redmine.wemove.com/issues/304

--- ingrid-portal-base/src/java/org/apache/jetspeed/engine/servlet/XXSUrlAttackFilter.java
+++ ingrid-portal-base/src/java/org/apache/jetspeed/engine/servlet/XXSUrlAttackFilter.java
@@ -1,9 +1,10 @@
 /*
- * Copyright 2007 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the  "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -30,7 +31,7 @@ import org.apache.log4j.Logger;
 
 /**
  * Simple XXS Url attack protection blocking access whenever the request url contains a &lt; or &gt; character.
- * @version $Id: XXSUrlAttackFilter.java 513987 2007-03-02 22:06:45Z ate $
+ * @version $Id: XXSUrlAttackFilter.java 1070715 2011-02-15 01:16:24Z woonsan $
  * 
  */
 public class XXSUrlAttackFilter implements Filter
