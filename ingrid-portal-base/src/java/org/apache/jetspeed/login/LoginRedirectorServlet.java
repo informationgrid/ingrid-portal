7ae0f611f74336221a9ee4c8eca077b29bbb29b8
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-09-16 14:02:54 +0000

    portal-base[branch 3.5_REDMINE-304]: - do NOT remove locale from session after login/logout, adapt to js2.3, see https://redmine.wemove.com/issues/304

--- ingrid-portal-base/src/java/org/apache/jetspeed/login/LoginRedirectorServlet.java
+++ ingrid-portal-base/src/java/org/apache/jetspeed/login/LoginRedirectorServlet.java
@@ -1,17 +1,18 @@
 /*
- * Copyright 2000-2004 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.jetspeed.login;
 
@@ -23,11 +24,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.jetspeed.Jetspeed;
+import org.apache.jetspeed.audit.AuditActivity;
+import org.apache.jetspeed.cache.UserContentCacheManager;
+import org.apache.jetspeed.components.ComponentManager;
+
 /**
  * LoginRedirectorServlet
  * 
  * @author <a href="mailto:ate@douma.nu">Ate Douma </a>
- * @version $Id: LoginRedirectorServlet.java 378256 2006-02-16 14:40:47Z shinsuke $
+ * @version $Id: LoginRedirectorServlet.java 1536817 2013-10-29 17:11:29Z taylor $
  */
 public class LoginRedirectorServlet extends HttpServlet
 {
@@ -43,12 +49,24 @@ public class LoginRedirectorServlet extends HttpServlet
         else
             session.removeAttribute(LoginConstants.DESTINATION);
 
+        String username = (String)session.getAttribute(LoginConstants.USERNAME);
+        
         session.removeAttribute(LoginConstants.USERNAME);
         session.removeAttribute(LoginConstants.PASSWORD);
         session.removeAttribute(LoginConstants.RETRYCOUNT);
-        
-        /* Jetspeed remove locale after login */
+
+        /* wemove: Jetspeed removes locale after login. We keep it. */
         //session.removeAttribute(PortalReservedParameters.PREFERED_LOCALE_ATTRIBUTE);
+
+        ComponentManager cm = Jetspeed.getComponentManager();
+        UserContentCacheManager userContentCacheManager = cm.lookupComponent("userContentCacheManager");
+        userContentCacheManager.evictUserContentCache(username, session.getId());
+        AuditActivity audit = cm.lookupComponent("org.apache.jetspeed.audit.AuditActivity");
+        if (audit != null)
+        {
+            audit.logUserActivity(username, request.getRemoteAddr(), AuditActivity.AUTHENTICATION_SUCCESS, "Active Authentication");
+        }
+        
         response.sendRedirect(response.encodeURL(destination));
     }
 
@@ -56,5 +74,5 @@ public class LoginRedirectorServlet extends HttpServlet
             HttpServletResponse response) throws IOException, ServletException
     {
         doGet(request, response);
-    }
+    }    
 }
--- ingrid-portal-base/src/java/org/apache/jetspeed/login/LogoutServlet.java
+++ ingrid-portal-base/src/java/org/apache/jetspeed/login/LogoutServlet.java
@@ -1,22 +1,24 @@
 /*
- * Copyright 2000-2004 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.jetspeed.login;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,27 +26,44 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.jetspeed.PortalReservedParameters;
+import org.apache.jetspeed.security.impl.cas.CASPortalFilter;
 
 /**
  * LogoutServlet
  * 
  * @author <a href="mailto:ate@douma.nu">Ate Douma </a>
- * @version $Id: LogoutServlet.java 394066 2006-04-14 11:58:18Z ate $
+ * @version $Id: LogoutServlet.java 926342 2010-03-22 21:18:19Z taylor $
  */
 public class LogoutServlet extends HttpServlet
 {
+	private String casLogoutUrl = null; 
 
+	public void init(ServletConfig config) throws ServletException  
+	{
+	    super.init(config);
+	    casLogoutUrl = config.getInitParameter("casLogoutUrl"); // will return null if not existing
+	  }
+	
     public void doGet(HttpServletRequest request,
             HttpServletResponse response) throws IOException, ServletException
     {
         String destination = request.getParameter(LoginConstants.DESTINATION);
+        
+        if (casLogoutUrl != null)
+        {
+	        String casUserName = (String) request.getSession().getAttribute(CASPortalFilter.CAS_FILTER_USER);
+	        if (casUserName != null)
+	        {
+	        	destination = this.casLogoutUrl;
+	        }
+        }
+
+        /* wemove: Jetspeed removes locale after logout (JS 2.1). We keep 2.1 code setting locale again. */
         HttpSession session =  request.getSession(true);
-       
-        /* Jetspeed remove locale after logout */
         Object locale =  session.getAttribute(PortalReservedParameters.PREFERED_LOCALE_ATTRIBUTE);
         session.invalidate();
         request.getSession(true).setAttribute(PortalReservedParameters.PREFERED_LOCALE_ATTRIBUTE, locale);
-        
+
         if (destination == null)
         {
             destination = request.getContextPath() + "/"; 
