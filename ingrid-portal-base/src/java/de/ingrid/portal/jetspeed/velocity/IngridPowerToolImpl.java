fdee3b537bc68334f5b2f37615f7cd59809422c6
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-09-23 16:37:54 +0000

    portal-base[branch 3.5_REDMINE-304]: - adapt IngridPowerToolImpl.java to JS 2.3, add to assembly/override/jetspeed-spring.xml, see https://redmine.wemove.com/issues/304

--- ingrid-portal-base/src/java/de/ingrid/portal/jetspeed/velocity/IngridPowerToolImpl.java
+++ ingrid-portal-base/src/java/de/ingrid/portal/jetspeed/velocity/IngridPowerToolImpl.java
@@ -4,37 +4,38 @@ import java.security.Principal;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.prefs.Preferences;
 
+import javax.portlet.PortletConfig;
 import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.jetspeed.Jetspeed;
 import org.apache.jetspeed.aggregator.PortletRenderer;
-import org.apache.jetspeed.om.common.GenericMetadata;
 import org.apache.jetspeed.om.page.ContentFragment;
-import org.apache.jetspeed.om.page.PageLocalizedFieldImpl;
-import org.apache.jetspeed.om.page.impl.PageMetadataLocalizedFieldImpl;
+import org.apache.jetspeed.om.portlet.GenericMetadata;
+import org.apache.jetspeed.om.portlet.LocalizedField;
+import org.apache.jetspeed.om.portlet.Preference;
+import org.apache.jetspeed.om.portlet.Preferences;
 import org.apache.jetspeed.om.preference.FragmentPreference;
 import org.apache.jetspeed.request.RequestContext;
 import org.apache.jetspeed.security.SecurityException;
 import org.apache.jetspeed.security.User;
 import org.apache.jetspeed.security.UserManager;
-import org.apache.jetspeed.services.title.DynamicTitleService;
 import org.apache.jetspeed.velocity.JetspeedPowerToolImpl;
-import org.apache.pluto.om.common.Preference;
-import org.apache.pluto.om.common.PreferenceSet;
 
 import de.ingrid.portal.global.IngridResourceBundle;
 
 public class IngridPowerToolImpl extends JetspeedPowerToolImpl {
     private static UserManager userManager = null;
 
-    public IngridPowerToolImpl(RequestContext requestContext, DynamicTitleService titleService,PortletRenderer renderer) throws Exception {
-        super(requestContext, titleService, renderer);
+    public IngridPowerToolImpl(RequestContext requestContext, PortletConfig portletConfig, RenderRequest renderRequest, RenderResponse renderResponse, PortletRenderer renderer) throws Exception {
+        super(requestContext, portletConfig, renderRequest, renderResponse, renderer);
         if (userManager == null) {
-            userManager = (UserManager) Jetspeed.getComponentManager().getComponent(UserManager.class.getName());
+            userManager = Jetspeed.getComponentManager().lookupComponent("org.apache.jetspeed.security.UserManager");
             if (null == userManager) {
                 throw new PortletException("Failed to find the User Manager on powertool initialization");
             }
@@ -42,10 +43,10 @@ public class IngridPowerToolImpl extends JetspeedPowerToolImpl {
     }
 
     public Object getPreferenceFirstValue(ContentFragment f, String key) {
-        List prefs = f.getPreferences();
-        Iterator it = prefs.iterator();
+        List<FragmentPreference> prefs = f.getPreferences();
+        Iterator<FragmentPreference> it = prefs.iterator();
         while (it.hasNext()) {
-            FragmentPreference pref = (FragmentPreference) it.next();
+            FragmentPreference pref = it.next();
             if (pref.getName().equals(key) && pref.getValueList() != null && pref.getValueList().size() > 0) {
                 return pref.getValueList().get(0);
             }
@@ -54,11 +55,10 @@ public class IngridPowerToolImpl extends JetspeedPowerToolImpl {
         // no preference in fragment found, try to get the preference from the
         // portlet preferences
         try {
-            PreferenceSet ps = windowAccess.getPortletWindow(f).getPortletEntity().getPortletDefinition()
-                    .getPreferenceSet();
-            Preference p = ps.get(key);
+            Preferences ps = getPortletWindow(f).getPortletDefinition().getPortletPreferences();
+            Preference p = ps.getPortletPreference(key);
             if (p != null) {
-                return (String) p.getValues().next();
+                return p.getValues().get(0);
             }
         } catch (Exception e) {
             handleError(e, "JetspeedPowerTool failed to retreive the current PortletEntity.  " + e.toString(),
@@ -80,7 +80,7 @@ public class IngridPowerToolImpl extends JetspeedPowerToolImpl {
         }
     }
 
-    public Preferences getUserProperties() {
+    public Map<String, String> getUserProperties() {
         Principal principal = requestContext.getRequest().getUserPrincipal();
         if (principal != null) {
             String userName = principal.getName();
@@ -91,7 +91,7 @@ public class IngridPowerToolImpl extends JetspeedPowerToolImpl {
                 e.printStackTrace();
             }
             if (user != null) {
-                return user.getUserAttributes();
+                return user.getInfoMap();
             }
         }
         return null;
@@ -119,10 +119,10 @@ public class IngridPowerToolImpl extends JetspeedPowerToolImpl {
         if (metadata == null)
             return "";
         
-        Collection<PageLocalizedFieldImpl> c = metadata.getFields(field);
+        Collection<LocalizedField> c = metadata.getFields(field);
         
         if (c != null) {
-            for (PageLocalizedFieldImpl entry : c) {
+            for (LocalizedField entry : c) {
                 value = entry.getValue();
                 break;
             }
