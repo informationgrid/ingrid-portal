784cac674d06140597d4f0126e0dbbc3d0f776c0
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-10-22 09:07:21 +0000

    portal-apps [branch 3.5_REDMINE-304]: - fix reading of help chapter (dom4j) with missing jars and better log in HelpPortlet, see https://redmine.wemove.com/issues/304

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/HelpPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/HelpPortlet.java
@@ -13,8 +13,6 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.apache.portals.bridges.velocity.GenericVelocityPortlet;
 import org.apache.velocity.context.Context;
 import org.dom4j.Document;
@@ -23,6 +21,8 @@ import org.dom4j.io.DocumentResult;
 import org.dom4j.io.DocumentSource;
 import org.dom4j.io.SAXReader;
 import org.dom4j.tree.DefaultElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.ingrid.portal.global.IngridResourceBundle;
 import de.ingrid.portal.global.Utils;
@@ -69,7 +69,7 @@ public class HelpPortlet extends GenericVelocityPortlet {
         try {
             doc = xmlReader.read(filePath);
         } catch (Exception e) {
-            log.error("Error reading help source file!", e);
+            log.error("Error reading help source file: " + filePath, e);
         }
 
         // get the help chapter
@@ -77,7 +77,15 @@ public class HelpPortlet extends GenericVelocityPortlet {
         if (helpKey == null) {
             helpKey = "index";
         }
-        Object chapterObj = doc.selectSingleNode( "//section[@help-key='" + helpKey + "']/ancestor::chapter" );
+
+        // read help chapter
+        Object chapterObj = null;
+        String myPath = "//section[@help-key='" + helpKey + "']/ancestor::chapter";
+        try {
+            chapterObj = doc.selectSingleNode(myPath);
+        } catch (Throwable t) {
+            log.error("Error reading '" + myPath + "' from help source file: " + filePath, t);
+        }
 
         // transform the xml content to valid html using xslt
         if (chapterObj == null) {
