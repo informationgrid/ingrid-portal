05f6494615775983a39ca888ad7697e392272ca3
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-11-13 16:03:00 +0000

    portal-mdek-app [branch 3.5_REDMINE-304]: - DELETE portal-mdek-app from branch to be replaced with trunk (instead of merge)

--- ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/mapping/ProtocolFactory.java
+++
@@ -1,8 +0,0 @@
-package de.ingrid.mdek.mapping;
-
-import de.ingrid.mdek.handler.ProtocolHandler;
-
-public interface ProtocolFactory {
-	
-	public ProtocolHandler getProtocolHandler();
-}
--- ingrid-portal-mdek-application/src/main/webapp/dojo-src/util/buildscripts/myBuild.sh
+++
@@ -1,1 +0,0 @@
-sh build.sh profile=ingrid action=clean,release cssOptimize=comments.keepLines optimize=shrinkSafe version=1.5.0-dev
