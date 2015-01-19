6b9b8689d303b67d9b878b8a95f87bbcd7c224f1
André Wallat <andre.wallat@wemove.com>
2015-01-19 11:38:24 +0100

    - cleaned up

--- ingrid-portal-mdek-application/src/main/webapp/dojo-sources/ingrid/nls/messages.js
+++ ingrid-portal-mdek-application/src/main/webapp/dojo-sources/ingrid/nls/messages.js
@@ -1024,7 +1024,6 @@ define({
     "validation.error.spatial.table":"At least one 'spatial' table has to contain an entry with a bounding box.",
     "validation.error.spatial.expired.table":"The spatial reference table must not contain expired entries.",
     "validation.error.email.table":"At least one entry has to be of type '${0}'.",
-    
     "validation.error.missing.capabilities.entry":"An operation of type GetCapabilities is needed.",
     "validation.error.invalid.operation.name":"The operation name is invalid.",
     "validation.error.empty.rows":"The table contains rows which are empty. Please remove those before saving.",
@@ -1037,9 +1036,6 @@ define({
     "validation.error.spatial.no.entry":"At least one spatial reference must be entered as thesaurus or free entry.",
     "validation.error.spatial.no.expired":"A spatial reference must not be expired.",
     "validation.error.missing.download.link":"When 'Open Data' is selected then there must be a link of the type 'Download' created.",
-    
-    
-    
     
     "hint.noCapabilityUrlFound":"No GetCapabilities-Url could be found to update this dataset.",
     "hint.datasetUpdatedFromCapabilities":"The dataset has been successfully updated.",
