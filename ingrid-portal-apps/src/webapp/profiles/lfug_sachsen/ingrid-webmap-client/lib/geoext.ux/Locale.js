6bd41eab3cadf6189e33a209725e3716924e27ec
Toan Tran <khanh.toan.tran@wemove.com>
2015-03-02 18:23:56 +0100

    Update mapclient locales for profiles.

--- ingrid-portal-apps/src/webapp/profiles/lfug_sachsen/ingrid-webmap-client/lib/geoext.ux/Locale.js
+++ ingrid-portal-apps/src/webapp/profiles/lfug_sachsen/ingrid-webmap-client/lib/geoext.ux/Locale.js
@@ -250,6 +250,7 @@ Local = {
 		tLegendToolBar: {en: 'Legend', de: 'Legende'},
 		tTransparenz: {en: 'Transparency', de: 'Transparenz'},
 		tInformation: {en: 'Information', de: 'Information'},
+		tNoInformationAvailable: {en: 'No information available.', de: 'Keine Informationen vorhanden.'},
 		tLoeschen: {en: 'Delete', de: 'L&ouml;schen'},
 		tZoomToLayerExtent: {en: 'Zoom to layer', de: 'Zoom auf Layer'},
 		tExternServicePanel: {en: 'External Service', de: 'Externer Dienst'},
