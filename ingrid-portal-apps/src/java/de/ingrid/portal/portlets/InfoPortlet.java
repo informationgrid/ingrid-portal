f55abe37b8f328bc32eaf1d4e646b1091522e404
Toan Tran <khanh.toan.tran@wemove.com>
2016-06-20 16:27:03 +0200

    Restore jetspeed layout mechanism. (file system version)

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/InfoPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/InfoPortlet.java
@@ -23,6 +23,9 @@
 package de.ingrid.portal.portlets;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -34,8 +37,16 @@ import org.apache.jetspeed.request.RequestContext;
 import org.apache.portals.bridges.velocity.GenericVelocityPortlet;
 import org.apache.velocity.context.Context;
 
+import de.ingrid.iplug.sns.utils.DetailedTopic;
 import de.ingrid.portal.config.PortalConfig;
 import de.ingrid.portal.global.IngridResourceBundle;
+import de.ingrid.portal.global.Settings;
+import de.ingrid.portal.global.Utils;
+import de.ingrid.portal.global.UtilsString;
+import de.ingrid.portal.interfaces.impl.DBAnniversaryInterfaceImpl;
+import de.ingrid.portal.search.SearchState;
+import de.ingrid.utils.IngridHitDetail;
+import de.ingrid.utils.udk.UtilsDate;
 
 public class InfoPortlet extends GenericVelocityPortlet {
 
@@ -45,6 +56,8 @@ public class InfoPortlet extends GenericVelocityPortlet {
     public final static String DEFAULT_TITLE_KEY = "info.default.title";
 
     private final static String SITEMAP_TEMPLATE = "/WEB-INF/templates/sitemap.vm";
+    
+    private final static String INFO_TEASER_TEMPLATE = "/WEB-INF/templates/info_teaser.vm";
     
     
     public void init(PortletConfig config) throws PortletException {
@@ -82,12 +95,92 @@ public class InfoPortlet extends GenericVelocityPortlet {
             context.put("enableChronicle", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_CHRONICLE, Boolean.FALSE));
             context.put("enableSearchCatalog", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_SEARCH_CATALOG, Boolean.FALSE));
         }
+        
+        if(myView.equalsIgnoreCase(INFO_TEASER_TEMPLATE)){
+            // For weather teaser
+            context.put("DWD_PATH", PortalConfig.getInstance().getString(PortalConfig.TEASER_WEATHER_DWD_PATH, ""));
+            context.put("DWD_MOVIE", PortalConfig.getInstance().getString(PortalConfig.TEASER_WEATHER_DWD_MOVIE, ""));
+            
+            // For chronicle teaser
+            // NOTICE: WE FETCH FROM DATABASE AND DON'T HAVE ALL DETAILS !!!
+            String lang = Utils.checkSupportedLanguage(request.getLocale().getLanguage());
+            IngridHitDetail[] details = DBAnniversaryInterfaceImpl.getInstance().getAnniversaries(new Date(), lang);
+
+            HashMap result = new HashMap();
+            if (details.length > 0) {
+                int entry = (int) (Math.random() * details.length);
+                DetailedTopic detail = (DetailedTopic) details[entry];
+                result.put("title", detail.getTopicName());
+                // type NOT IN DATABASE 
+                /*
+                 try {
+                 String urlWithType = (String) detail.getArrayList(DetailedTopic.INSTANCE_OF).get(0);
+                 String type = urlWithType.split("#")[1].split("Type")[0];
+                 result.put("type", type);
+                 } catch (Exception ex) {
+                 }
+                 */
+                result.put("topicId", detail.get("topicId"));
+                String topicFrom = detail.getFrom();
+                if (topicFrom != null) {
+                    result.put("from", UtilsDate.parseDateToLocale(topicFrom, request.getLocale()));
+                }
+                String topicTo = detail.getTo();
+                if (topicTo != null && !topicTo.equals(topicFrom)) {
+                    result.put("to", UtilsDate.parseDateToLocale(topicTo, request.getLocale()));
+                }
+                if (topicFrom != null) {
+                    int years = UtilsDate.yearsBetween(topicFrom, new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+                    result.put("years", new Integer(years));
+                }
+                // fetch search term
+                // TODO use longest word as sns search term ? us title if bug in iPlug is resolved, see INGRID-901 ?
+                result.put("term", UtilsString.getSearchTerm(detail.getTopicName(), " "));
+                //result.put("term", detail.getTopicName());
+
+                /*
+                 String searchData = (String) detail.get(DetailedTopic.ASSICIATED_OCC);
+                 if (searchData != null) {
+                 String searchTerm = searchData;
+                 int endIndex = searchData.indexOf(',');
+                 if (endIndex != -1) {
+                 searchTerm = searchData.substring(0, endIndex);
+                 }
+                 if (searchTerm.charAt(0) == '"') {
+                 searchTerm = searchTerm.substring(1, searchTerm.length());
+                 }
+                 if (searchTerm.charAt(searchTerm.length() - 1) == '"') {
+                 searchTerm = searchTerm.substring(0, searchTerm.length() - 1);
+                 }
+                 result.put("term", searchTerm);
+                 } else {
+                 result.put("term", detail.getTopicName());
+                 }
+                 */
+                /*
+                 ArrayList list = detail.getArrayList(DetailedTopic.INSTANCE_OF);
+                 System.out.println(list);
+                 */
+            }
+
+            context.put("snsAnniversary", result);
+        }
         context.put("enableApplication", PortalConfig.getInstance().getBoolean(PortalConfig.PORTAL_ENABLE_APPLICATION, Boolean.FALSE));
         super.doView(request, response);
     }
 
     public void processAction(ActionRequest request, ActionResponse actionResponse) throws PortletException,
             IOException {
+            String action = request.getParameter( "action" );
+            if(action.equals(Settings.PARAMV_ACTION_FROM_TEASER)){
+                if(request.getParameter( "isMeasure" ) != null){
+                    // redirect to our page with URL parameters for bookmarking
+                    actionResponse.sendRedirect(actionResponse.encodeURL(Settings.PAGE_MEASURES + SearchState.getURLParamsCatalogueSearch(request, null)));
+                } else if(request.getParameter( "isChronicle" ) != null){
+                    // redirect to our page with URL parameters for bookmarking
+                    actionResponse.sendRedirect(actionResponse.encodeURL(Settings.PAGE_CHRONICLE + SearchState.getURLParamsCatalogueSearch(request, null)));
+                }
+            }
     }
 
 }
