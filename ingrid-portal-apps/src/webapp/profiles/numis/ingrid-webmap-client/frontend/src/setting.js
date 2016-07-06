17c88b6c697cda3aa4ee3c557941c439864b4808
Toan Tran <khanh.toan.tran@wemove.com>
2016-07-06 17:17:03 +0200

    Update portal profiles for mapclient.

---
+++ ingrid-portal-apps/src/webapp/profiles/numis/ingrid-webmap-client/frontend/src/setting.js
@@ -0,0 +1,20 @@
+var settingExtent = '[6.8, 51.1, 11.2, 54]';
+var settingEpsgExtent = '[6.8, 51.1, 11.2, 54]';
+var settingEpsg = undefined;
+var settingShortURLService = undefined;
+var settingSearchServiceUrl = 'http://numis.niedersachsen.de/opensearch/query?q={query}+t011_obj_serv_op_connpoint.connect_point:http*+t011_obj_serv.type:view+cache:off+datatype:metadata+ranking:score%26ingrid=1%26h=100';
+var settingSearchNominatimUrl = 'http://nominatim.openstreetmap.org/search?format=json%26countrycodes=de';
+var settingCopyrightURL = undefined;
+// KML
+var settingKMLName = 'NUMIS';
+// WMS Import
+var settingDefaultWMSList = [];
+// Share
+var settingShareFacebook = true;
+var settingShareMail = true;
+var settingShareGoogle = true;
+var settingShareTwitter = true;
+var settingShareIFrame = true;
+var settingShareLink = true;
+var settingPrintLogo = location.protocol + '//' + location.host + '/ingrid-webmap-client/frontend/prd/img/print_logo.png';
+var settingPrintNorthArrow = location.protocol + '//' + location.host + '/ingrid-webmap-client/frontend/prd/img/north_arrow.png';
