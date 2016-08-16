065cee14b2defb55374c91aa5a69753a9e07fc01
Andre Wallat <andre.wallat@wemove.com>
2016-08-16 18:18:15 +0200

    - added new field for service hours including localization and context help

---
+++ ingrid-portal-distribution/src/main/resources/sql/mdek_4.0.0_mysql.sql
@@ -0,0 +1,12 @@
+--
+-- Daten für Tabelle `help_messages`
+--
+
+UPDATE `info` SET `value_name` = '4.0.0' WHERE  `info`.`key_name` = 'version';
+
+-- Updated stuff for InGrid 3.6.1
+-- ==============================
+
+INSERT INTO `help_messages` (`id`, `version`, `gui_id`, `entity_class`, `language`, `name`, `help_text`, `sample`) VALUES
+(1540, 0, 4450, -1, 'de', 'Servicezeiten', 'Zeitraum inklusive der Zeitzone, wann die verantwortliche Person oder Organisation erreicht werden kann.', ''),
+(1541, 0, 4450, -1, 'en', 'Hours Of Service', 'Time period (including time zone) when individuals can contact the organization or individual.', '');
