78507e0dbc13390cf3e23df6fbc91a8eb47dd73a
Andre Wallat <andre.wallat@wemove.com>
2016-09-12 16:46:30 +0200

    - added context help to field AdV compatibility

---
+++ ingrid-portal-distribution/src/main/resources/sql/mdek_4.1.0_mysql.sql
@@ -0,0 +1,12 @@
+--
+-- Daten für Tabelle `help_messages`
+--
+
+UPDATE `info` SET `value_name` = '4.0.0' WHERE  `info`.`key_name` = 'version';
+
+-- Updated stuff for InGrid 3.6.1
+-- ==============================
+
+INSERT INTO `help_messages` (`id`, `version`, `gui_id`, `entity_class`, `language`, `name`, `help_text`, `sample`) VALUES
+(1550, 0, 6005, -1, 'de', 'AdV Kompatibilität', 'Bei Aktivierung wird automatisch das Schlüsselwort "AdVMIS" hinzugefügt. Außerdem wird beim Veröffentlichen überprüft, dass die Ansprechpartner ein Verwaltungsgebiet gesetzt haben.', ''),
+(1551, 0, 6005, -1, 'en', 'AdV compatibility', 'When activated the keyword "AdVMIS" is automatically added. Moreover during publishing, the "Point of Contact" must have an administrative area set.', '');
