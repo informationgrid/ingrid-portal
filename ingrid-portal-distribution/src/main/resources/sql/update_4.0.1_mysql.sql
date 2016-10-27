4425cded51787a59d5078364792593100cbf7268
Toan Tran <ktt@wemove.com>
2016-10-27 12:53:11 +0200

    Change application iFrame portlet to cms portlet for baw_wsv.

---
+++ ingrid-portal-distribution/src/main/resources/sql/update_4.0.1_mysql.sql
@@ -0,0 +1,22 @@
+-- DB Version
+UPDATE ingrid_lookup SET item_value = '4.0.1', item_date = NOW() WHERE ingrid_lookup.item_key ='ingrid_db_version';
+
+-- Temp Table um values aus folder_menu zwischen zu speichern (subselect in insert auf gleiche Tabelle nicht moeglich, s.u.)
+DROP TABLE IF EXISTS ingrid_temp;
+CREATE TABLE ingrid_temp (
+  temp_key varchar(255) NOT NULL,
+  temp_value mediumint
+);
+
+-- add cms key
+INSERT INTO ingrid_cms (item_key, item_description, item_changed_by) VALUES ('portal.cms.portlet.3', 'Anwendungen Übersicht', 'admin');
+INSERT INTO ingrid_cms_item (fk_ingrid_cms_id, item_lang, item_title, item_value, item_changed_by) VALUES ((SELECT id FROM ingrid_cms WHERE item_key  = 'portal.cms.portlet.3'), 'de', 'CMSPortlet3', '', 'admin');
+INSERT INTO ingrid_cms_item (fk_ingrid_cms_id, item_lang, item_title, item_value, item_changed_by) VALUES ((SELECT id FROM ingrid_cms WHERE item_key  = 'portal.cms.portlet.3'), 'en', 'CMSPortlet3', '', 'admin');
+
+-- Change portlet iFrame to CMS of page '/application/main-application.psml'
+INSERT INTO ingrid_temp (temp_key, temp_value) VALUES ('fragment_tmp', (SELECT fragment_id FROM fragment WHERE page_id = (SELECT page_id FROM page WHERE path = '/application/main-application.psml')));
+
+UPDATE fragment SET name = "ingrid-portal-apps::CMSPortlet3" WHERE name = "ingrid-portal-apps::IFramePortalPortlet" AND parent_id = (SELECT temp_value FROM ingrid_temp WHERE temp_key = 'fragment_tmp');
+
+-- delete temporary table
+DROP TABLE IF EXISTS ingrid_temp;
