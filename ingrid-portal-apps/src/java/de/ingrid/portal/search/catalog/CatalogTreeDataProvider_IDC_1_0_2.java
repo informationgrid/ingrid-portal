270391f45522f76ddfc5b9e290c0be99279677b4
Martin Maidhof <martin@wemove.com>
2017-01-12 17:35:37 +0100

    REDMINE-519: Also allow browsing catalog only containing addresses.

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/catalog/CatalogTreeDataProvider_IDC_1_0_2.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/catalog/CatalogTreeDataProvider_IDC_1_0_2.java
@@ -65,7 +65,8 @@ public class CatalogTreeDataProvider_IDC_1_0_2 implements CatalogTreeDataProvide
     			if (fieldList.contains(FIELD_OBJECT_ID) && fieldList.contains(FIELD_PARENT_OBJ_UUID)) {
                 	isCorrupt = false;
     			}
-            } else if (IPlugHelper.hasDataType(plug, Settings.QVALUE_DATATYPE_IPLUG_DSC_ECS_ADDRESS)) {
+    		}
+            if (IPlugHelper.hasDataType(plug, Settings.QVALUE_DATATYPE_IPLUG_DSC_ECS_ADDRESS)) {
     			if (fieldList.contains(FIELDS_ADDRESS_ID) && fieldList.contains(FIELD_PARENT_ADDR_UUID)) {
                 	isCorrupt = false;
     			}
@@ -73,8 +74,8 @@ public class CatalogTreeDataProvider_IDC_1_0_2 implements CatalogTreeDataProvide
     	}
     	
     	if (isCorrupt) {
-    		if (log.isDebugEnabled()) {
-    			log.debug("CORRUPT PlugDescription ! We skip this plug: " + plug);    			
+    		if (log.isWarnEnabled()) {
+    			log.warn("CORRUPT PlugDescription ! We skip this plug: " + plug);    			
     		}
     	}
 
