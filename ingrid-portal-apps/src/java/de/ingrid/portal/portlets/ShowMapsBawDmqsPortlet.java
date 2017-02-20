cf1be335c2470a6ccbf2f0ffee308f4d15834472
jm <joachim@wemove.com>
2017-02-20 21:16:58 +0100

    - Add BBOX on popup open. - Add BwStr WMS to leaflet setup

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/ShowMapsBawDmqsPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/ShowMapsBawDmqsPortlet.java
@@ -50,76 +50,87 @@ import de.ingrid.utils.queryparser.QueryStringParser;
 
 public class ShowMapsBawDmqsPortlet extends GenericVelocityPortlet {
 
-    private final static Logger log = LoggerFactory.getLogger(ShowMapsBawDmqsPortlet.class);
-    
-    private static final String[] REQUESTED_FIELDS = new String[] {"bwstr-center-lon", "bwstr-center-lat", "t01_object.obj_id"};
+    private final static Logger log = LoggerFactory.getLogger( ShowMapsBawDmqsPortlet.class );
+
+    private static final String[] REQUESTED_FIELDS_MARKER = new String[] { "bwstr-center-lon", "bwstr-center-lat", "t01_object.obj_id" };
+    private static final String[] REQUESTED_FIELDS_BBOX = new String[] { "x1", "x2", "y1", "y2", "t01_object.obj_id" };
 
     public void init(PortletConfig config) throws PortletException {
-        super.init(config);
+        super.init( config );
     }
-    
+
     @Override
     public void serveResource(ResourceRequest request, ResourceResponse response) throws IOException {
         String resourceID = request.getResourceID();
         try {
-            response.setContentType("application/javascript");
-            response.getWriter().write("var markers = [");
-            IBusQueryResultIterator it = new IBusQueryResultIterator(QueryStringParser.parse("http ranking:score"), REQUESTED_FIELDS, IBUSInterfaceImpl.getInstance().getIBus());
-            while (it.hasNext()) {
-                StringBuilder s = new StringBuilder();
-               IngridHit hit = it.next();
-               IngridHitDetail detail = hit.getHitDetail();
-               if (detail.containsKey( "bwstr-center-lat" ) && detail.containsKey( "bwstr-center-lon" )) {
-               s
-                   .append( "[" )
-                   .append( detail.get("bwstr-center-lat").toString() )
-                   .append( "," )
-                   .append( detail.get("bwstr-center-lon").toString() )
-                   .append( ",'" )
-                   .append( detail.get("title").toString() )
-                   .append( "','" )
-                   .append( UtilsSearch.getDetailValue(detail, "t01_object.obj_id" ) )
-                   .append( "']" );
-               if (it.hasNext()) {
-                   s.append( "," );
-               }
-               response.getWriter().write(s.toString());
-               }
+            if (resourceID.equals( "marker" )) {
+                response.setContentType( "application/javascript" );
+                response.getWriter().write( "var markers = [" );
+                IBusQueryResultIterator it = new IBusQueryResultIterator( QueryStringParser.parse( "http ranking:score" ), REQUESTED_FIELDS_MARKER, IBUSInterfaceImpl.getInstance()
+                        .getIBus() );
+                while (it.hasNext()) {
+                    StringBuilder s = new StringBuilder();
+                    IngridHit hit = it.next();
+                    IngridHitDetail detail = hit.getHitDetail();
+                    if (detail.containsKey( "bwstr-center-lat" ) && detail.containsKey( "bwstr-center-lon" )) {
+                        s.append( "[" ).append( detail.get( "bwstr-center-lat" ).toString() ).append( "," ).append( detail.get( "bwstr-center-lon" ).toString() ).append( ",'" )
+                                .append( detail.get( "title" ).toString() ).append( "','" ).append( UtilsSearch.getDetailValue( detail, "t01_object.obj_id" ) ).append( "']" );
+                        if (it.hasNext()) {
+                            s.append( "," );
+                        }
+                        response.getWriter().write( s.toString() );
+                    }
+                }
+                response.getWriter().write( "];" );
             }
-            response.getWriter().write("];");
+            if (resourceID.equals( "bbox" )) {
+                String uuid = request.getParameter( "uuid" );
+                response.setContentType( "application/javascript" );
+                response.getWriter().write( "[" );
+                IBusQueryResultIterator it = new IBusQueryResultIterator( QueryStringParser.parse( "t01_object.obj_id:" + uuid + " ranking:score" ), REQUESTED_FIELDS_BBOX,
+                        IBUSInterfaceImpl.getInstance().getIBus() );
+                if (it.hasNext()) {
+                    StringBuilder s = new StringBuilder();
+                    IngridHit hit = it.next();
+                    IngridHitDetail detail = hit.getHitDetail();
+                    if (detail.containsKey( "y1" ) && detail.containsKey( "x2" )) {
+                        s.append("[").append( detail.get( "y1" ).toString() ).append( "," ).append( detail.get( "x1" ).toString() ).append( "],[" ).append( detail.get( "y2" ).toString() )
+                                .append( "," ).append( detail.get( "x2" ).toString() ).append("]");
+                        response.getWriter().write( s.toString() );
+                    }
+                }
+                response.getWriter().write( "]" );
+            }
 
         } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            log.error( "Error creating resource for resource ID: " + resourceID, e );
         }
-        
-    }
-    
 
-    public void doView(RenderRequest request, RenderResponse response)
-            throws PortletException, IOException {
-        Context context = getContext(request);
-        IngridResourceBundle messages = new IngridResourceBundle(getPortletConfig().getResourceBundle(
-                request.getLocale()), request.getLocale());
-        context.put("MESSAGES", messages);
-        context.put("lang", request.getLocale().getLanguage());
+    }
+
+    public void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException {
+        Context context = getContext( request );
+        IngridResourceBundle messages = new IngridResourceBundle( getPortletConfig().getResourceBundle( request.getLocale() ), request.getLocale() );
+        context.put( "MESSAGES", messages );
+        context.put( "lang", request.getLocale().getLanguage() );
         // read preferences
         PortletPreferences prefs = request.getPreferences();
-        String hKey = prefs.getValue("helpKey", null);
+        String hKey = prefs.getValue( "helpKey", null );
         if (hKey != null && hKey.length() > 0) {
-            context.put("hKey", hKey);
+            context.put( "hKey", hKey );
         }
 
         // define an REST URL to get the map data dynamically
         ResourceURL restUrl = response.createResourceURL();
-        restUrl.setResourceID("data");
-        request.setAttribute("restUrl", restUrl.toString());
-        
-        super.doView(request, response);
+        restUrl.setResourceID( "marker" );
+        request.setAttribute( "restUrlMarker", restUrl.toString() );
+        restUrl.setResourceID( "bbox" );
+        request.setAttribute( "restUrlBBOX", restUrl.toString() );
+
+        super.doView( request, response );
     }
 
-    public void processAction(ActionRequest request, ActionResponse actionResponse) throws PortletException,
-            IOException {
+    public void processAction(ActionRequest request, ActionResponse actionResponse) throws PortletException, IOException {
 
     }
 }
