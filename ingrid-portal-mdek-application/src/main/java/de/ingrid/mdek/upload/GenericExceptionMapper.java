829a6cfef3e4bc64e926a1543cbf8c05e6dd766b
iherwig <ingo@wemove.com>
2017-03-06 17:18:44 +0100

    return json errors only if requested

--- ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/upload/GenericExceptionMapper.java
+++ ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/upload/GenericExceptionMapper.java
@@ -1,14 +1,21 @@
 package de.ingrid.mdek.upload;
 
 import java.nio.file.FileAlreadyExistsException;
+import java.util.List;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
 public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+    @Context
+    private HttpHeaders headers;
+
     @Override
     public Response toResponse(Throwable ex) {
         if (ex instanceof FileAlreadyExistsException) {
@@ -20,8 +27,12 @@ public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
                 ((WebApplicationException)ex).getResponse().getStatus() :
                     Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
 
-        // create the upload response
-        UploadResponse response = new UploadResponse(ex);
+        // create the upload response (add entity only for JSON)
+        UploadResponse response = null;
+        List<MediaType> accepts = this.headers.getAcceptableMediaTypes();
+        if (accepts.contains(MediaType.APPLICATION_JSON_TYPE)) {
+            response = new UploadResponse(ex);
+        }
         return Response.status(status).entity(response).build();
     }
 }
