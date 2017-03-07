6e7dac3f98dfc0d0111ff4502d7abac18ae6e266
Toan Tran <ktt@wemove.com>
2017-03-07 15:48:35 +0100

    Update map with marker from search.

---
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/ShowMapsUVPPortlet.java
@@ -0,0 +1,118 @@
+/*
+ * **************************************************-
+ * InGrid Portal Apps
+ * ==================================================
+ * Copyright (C) 2014 - 2017 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * **************************************************#
+ */
+package de.ingrid.portal.portlets;
+
+import java.io.IOException;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+import javax.portlet.ResourceURL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.ingrid.portal.config.PortalConfig;
+import de.ingrid.portal.interfaces.impl.IBUSInterfaceImpl;
+import de.ingrid.portal.search.UtilsSearch;
+import de.ingrid.portal.search.net.IBusQueryResultIterator;
+import de.ingrid.utils.IngridHit;
+import de.ingrid.utils.IngridHitDetail;
+import de.ingrid.utils.queryparser.QueryStringParser;
+
+public class ShowMapsUVPPortlet extends ShowMapsPortlet {
+
+    private final static Logger log = LoggerFactory.getLogger(ShowMapsUVPPortlet.class);
+
+    private static final String[] REQUESTED_FIELDS_MARKER = new String[] { "lon_center", "lat_center", "t01_object.obj_id" };
+    private static final String[] REQUESTED_FIELDS_BBOX = new String[] { "x1", "x2", "y1", "y2", "t01_object.obj_id" };
+
+    @Override
+    public void serveResource(ResourceRequest request, ResourceResponse response) throws IOException {
+        String resourceID = request.getResourceID();
+        try {
+            if (resourceID.equals( "marker" )) {
+                response.setContentType( "application/javascript" );
+                response.getWriter().write( "var markers = [" );
+                IBusQueryResultIterator it = new IBusQueryResultIterator( QueryStringParser.parse( PortalConfig.getInstance().getString(PortalConfig.PORTAL_MAPCLIENT_QUERY, "http ranking:score") ), REQUESTED_FIELDS_MARKER, IBUSInterfaceImpl.getInstance()
+                        .getIBus() );
+                while (it.hasNext()) {
+                    StringBuilder s = new StringBuilder();
+                    IngridHit hit = it.next();
+                    IngridHitDetail detail = hit.getHitDetail();
+                    Object[] lat_center = (Object[]) detail.get( "lat_center" );
+                    Object[] lon_center = (Object[]) detail.get( "lon_center" );
+                    if (lat_center != null && lon_center != null) {
+                        s.append( "[" ).append( lat_center[0].toString() ).append( "," ).append( lon_center[0].toString() ).append( ",'" )
+                                .append( detail.get( "title" ).toString() ).append( "','" ).append( UtilsSearch.getDetailValue( detail, "t01_object.obj_id" ) ).append( "']" );
+                        if (it.hasNext()) {
+                            s.append( "," );
+                        }
+                        response.getWriter().write( s.toString() );
+                    }
+                }
+                response.getWriter().write( "];" );
+            }
+            if (resourceID.equals( "bbox" )) {
+                String uuid = request.getParameter( "uuid" );
+                response.setContentType( "application/javascript" );
+                response.getWriter().write( "[" );
+                IBusQueryResultIterator it = new IBusQueryResultIterator( QueryStringParser.parse( "t01_object.obj_id:" + uuid + " ranking:score" ), REQUESTED_FIELDS_BBOX,
+                        IBUSInterfaceImpl.getInstance().getIBus() );
+                if (it.hasNext()) {
+                    StringBuilder s = new StringBuilder();
+                    IngridHit hit = it.next();
+                    IngridHitDetail detail = hit.getHitDetail();
+                    Object[] x1 = (Object[]) detail.get( "x1" );
+                    Object[] y1 = (Object[]) detail.get( "y1" );
+                    Object[] x2 = (Object[]) detail.get( "x2" );
+                    Object[] y2 = (Object[]) detail.get( "y2" );
+                    
+                    if (y1 != null && x2 != null) {
+                        s.append("[").append( y1[0].toString() ).append( "," ).append( x1[0].toString() ).append( "],[" ).append( y2[0].toString() )
+                                .append( "," ).append( x2[0].toString() ).append("]");
+                        response.getWriter().write( s.toString() );
+                    }
+                }
+                response.getWriter().write( "]" );
+            }
+
+        } catch (Exception e) {
+            log.error( "Error creating resource for resource ID: " + resourceID, e );
+        }
+    }
+    
+    public void doView(RenderRequest request, RenderResponse response)
+            throws PortletException, IOException {
+        // define an REST URL to get the map data dynamically
+        ResourceURL restUrl = response.createResourceURL();
+        restUrl.setResourceID( "marker" );
+        request.setAttribute( "restUrlMarker", restUrl.toString() );
+        restUrl.setResourceID( "bbox" );
+        request.setAttribute( "restUrlBBOX", restUrl.toString() );
+        
+        super.doView(request, response);
+    }
+}
