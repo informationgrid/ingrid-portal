1f55dacda2ead248409d8a82a1f3f50e56aaf0a6
Toan Tran <ktt@wemove.com>
2017-03-15 15:37:39 +0100

    Add JS functions to load file size and set to HTML tag by ID.

--- ingrid-portal-base/src/webapp/decorations/layout/ingrid/scripts/ingrid.js
+++ ingrid-portal-base/src/webapp/decorations/layout/ingrid/scripts/ingrid.js
@@ -361,3 +361,39 @@ function addLeafletHomeControl(map, title, position, icon, bounds, latlng){
     
     map.addControl(new HomeControl({}));
 }
+
+function getFileSize(url, htmlId)
+{
+    var fileSize = '';
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, true);
+    http.onreadystatechange = function() {
+        if (this.readyState == this.DONE) {
+            if (this.status === 200) {
+                fileSize = this.getResponseHeader('content-length');
+                if(fileSize){
+                    var element = document.getElementById(htmlId);
+                    element.innerHTML = convertfileSize(fileSize, true);
+                }
+            }
+        }
+    };
+    http.send(); 
+    return ('');
+}
+
+function convertfileSize(bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+}
