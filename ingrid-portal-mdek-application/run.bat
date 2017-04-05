ae0946138d5bb837407e7b1bf53644e3ca84103b
Andre Wallat <andre.wallat@wemove.com>
2017-04-05 16:58:19 +0200

    - updated run script to start with optional profile parameter (e.g. run.bat uvp)

--- ingrid-portal-mdek-application/run.bat
+++ ingrid-portal-mdek-application/run.bat
@@ -1,3 +1,8 @@
 set MAVEN_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n
 
-mvn jetty:run -Denv=dev -Dprofile=uvp
+if %1.==. goto:DEFAULT
+mvn jetty:run -Denv=dev -Dprofile=%1
+goto:eof
+
+:DEFAULT
+mvn jetty:run -Denv=dev
