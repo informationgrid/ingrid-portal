49ee155124411569460436845b6662c1ced9eb18
Martin Maidhof <martin@wemove.com>
2017-06-13 18:48:48 +0200

    REDMINE-363: Added GEMET thesaurus with english localization to BKG profile ! Add english localization to GEMET keywords when saving object and additional localization is configured ! #363

--- ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/dwr/services/sns/SNSService.java
+++ ingrid-portal-mdek-application/src/main/java/de/ingrid/mdek/dwr/services/sns/SNSService.java
@@ -69,6 +69,9 @@ public class SNSService {
     // Init Method is called by the Spring Framework on initialization
     public void init() throws Exception {}
 
+    public ThesaurusService getThesaurusService() {
+        return thesaurusService;
+    }
 	public void setThesaurusService(ThesaurusService thesaurusService) {
 		this.thesaurusService = thesaurusService;
 	}
