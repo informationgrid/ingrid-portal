6bd298ec81f11a860fdd3ca26727e79cc6d8c644
Toan Tran <khanh.toan.tran@wemove.com>
2018-01-22 10:04:12 +0100

    Add password meter and add sources for tool zxcvbn. #829

--- ingrid-portal-base/src/webapp/decorations/layout/ingrid/scripts/ingrid.js
+++ ingrid-portal-base/src/webapp/decorations/layout/ingrid/scripts/ingrid.js
@@ -424,3 +424,17 @@ function convertFileSize(bytes, si) {
     }
     return bytes.toFixed(1) + ' ' + units[u];
 }
+
+function checkPassword(pwd, idMeter, idText) {
+    var meter = document.getElementById(idMeter);
+    var text = document.getElementById(idText);
+
+    if (pwd != '') {
+        var result = zxcvbn(pwd);
+        meter.value = result.score;
+        text.innerHTML = meterStrength[result.score];
+    } else {
+        text.innerHTML  = ' ';
+        meter.value = 0;
+    }
+}
