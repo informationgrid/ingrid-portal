d45bceed6555fe60a618ad2b1666530983688760
Toan Tran <khanh.toan.tran@wemove.com>
2018-05-02 12:36:00 +0200

    Add initial ZDM profile. #1562

---
+++ ingrid-portal-apps/src/webapp/profiles/zdm/profile_postgres.sql
@@ -0,0 +1,11 @@
+-- Show '/main-measures.psml'
+UPDATE page SET is_hidden = 1 WHERE path = '/main-measures.psml';
+
+-- Show '/main-chronicle.psml'
+UPDATE page SET is_hidden = 1 WHERE path = '/main-chronicle.psml';
+
+-- Show '/main-maps.psml'
+UPDATE page SET is_hidden = 1 WHERE path = '/main-maps.psml';
+
+-- Show '/search-catalog/search-catalog-hierarchy.psml'
+UPDATE page SET is_hidden = 1 WHERE path = '/search-catalog/search-catalog-hierarchy.psml';
