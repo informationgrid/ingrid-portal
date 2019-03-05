01baa1f4fbdef357a3fdc2ab1e185af343e22fe3
Toan Tran <khanh.toan.tran@wemove.com>
2019-03-05 13:04:57 +0100

    Update img preview

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/QueryResultPostProcessor.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/QueryResultPostProcessor.java
@@ -163,7 +163,13 @@ public class QueryResultPostProcessor {
             } else if (additionalHtml instanceof String[]){
                 hit.put(Settings.RESULT_KEY_ADDITIONAL_HTML_1, ((String[]) additionalHtml)[0]);
             } else {
-                hit.put(Settings.RESULT_KEY_ADDITIONAL_HTML_1, additionalHtml);
+                String tmp = additionalHtml.toString();
+                if(tmp.indexOf(">, <") > -1) {
+                    String[] tmpSplit = tmp.split(", ");
+                    hit.put(Settings.RESULT_KEY_ADDITIONAL_HTML_1, tmpSplit[0]);
+                } else {
+                    hit.put(Settings.RESULT_KEY_ADDITIONAL_HTML_1, tmp.split(", "));
+                }
             }
             
             boolean doNotShowMaps = false;
