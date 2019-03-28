aa0753c102641b0009510763619cc223bc90ac58
Martin Maidhof <martin@wemove.com>
2019-03-28 17:05:21 +0100

    mCLOUD: Flyway fix for not existing repo_user table

--- ingrid-portal-mdek-application/src/main/resources/db/migration/oracle/V4_7_0__oracle_update.sql
+++ ingrid-portal-mdek-application/src/main/resources/db/migration/oracle/V4_7_0__oracle_update.sql
@@ -1,14 +1,8 @@
 UPDATE info SET value_name = '4.7.0' WHERE  info.key_name = 'version';
 
 -- Fix user table for standalone IGE
-BEGIN
-   EXECUTE IMMEDIATE 'DROP TABLE ' || repo_user;
-EXCEPTION
-   WHEN OTHERS THEN
-      IF SQLCODE != -942 THEN
-         RAISE;
-      END IF;
-END;
+BEGIN execute immediate 'DROP TABLE repo_user'; exception when others then null; END;
+/
 
 CREATE TABLE repo_user (
    login VARCHAR2(255 CHAR) NOT NULL,
