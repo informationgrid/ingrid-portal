f7e5a7a2f4a845fdf6558c46b622fbfcd159794c
Toan Tran <khanh.toan.tran@wemove.com>
2019-04-15 11:10:58 +0200

    Draft

---
+++ ingrid-portal-apps/src/webapp/profiles/uvp/ingrid-portal/decorations/layout/ingrid/scripts/app.js
@@ -0,0 +1,6 @@
+#= require vendor/jquery-3.2.1.min
+#= require vendor/foundation.min
+#= require vendor/select2.full
+#= require vendor/leaflet.js
+#= require vendor/leaflet.markercluster.js
+#= require main
