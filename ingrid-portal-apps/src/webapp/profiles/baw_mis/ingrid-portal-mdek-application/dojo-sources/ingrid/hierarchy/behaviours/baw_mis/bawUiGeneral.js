fdd8d15bf7e990b915413674b86c1c0fe33e382d
Vikram Notay <vikram.notay@baw.de>
2019-09-11 10:46:43 +0200

    Additional UI changes in IGE for BAW profile (#1204)

--- ingrid-portal-apps/src/webapp/profiles/baw_mis/ingrid-portal-mdek-application/dojo-sources/ingrid/hierarchy/behaviours/baw_mis/bawUiGeneral.js
+++ ingrid-portal-apps/src/webapp/profiles/baw_mis/ingrid-portal-mdek-application/dojo-sources/ingrid/hierarchy/behaviours/baw_mis/bawUiGeneral.js
@@ -39,19 +39,43 @@ define([
 
                 var isNewItem = "newNode" === currentUdk.uuid;
 
-
-                // ========================================
-                // Hide fields
-                // ========================================
-
                 // ----------------------------------------
                 // Allgemeines
                 // ----------------------------------------
-
                 // Kurzbezeichnung
                 domClass.add("uiElement5000", "hide");
-                // AdV kompatibel
+                // AdV kompatibel checkbox
                 domClass.add("uiElement6005", "hide");
+                // Open Data checkbox
+                /*
+                 * opendata.js runs after this script and removes the 'hide'
+                 * class. So add the 'hidden' class instead!
+                 */
+                domClass.add("uiElement6010", "hidden");
+
+                // Kategorien (opendata)
+                var ogdCategoriesTableId = "categoriesOpenData";
+                var ogdCategoriesTable = registry.byId(ogdCategoriesTableId);
+
+                domClass.add("uiElement6020", "required");
+                domClass.remove("uiElement6020", "hide");
+                domClass.remove("uiElement6020", "halfWidth");
+                ogdCategoriesTable.reinitLastColumn(true);
+
+                if (isNewItem && data.objClass !== "Class0") {
+                    var listId = 6400;
+                    var entryId = "10";
+                    var entryTitle = UtilSyslist.getSyslistEntryName(listId, entryId);
+
+                    var ogdCategoriesTableData = ogdCategoriesTable.data;
+                    var existing = ogdCategoriesTableData.find(function (value) {
+                        return value.title === entryTitle;
+                    });
+                    if (!existing) {
+                        ogdCategoriesTableData.push({title: entryTitle });
+                        UtilStore.updateWriteStore(ogdCategoriesTableId, ogdCategoriesTableData);
+                    }
+                }
 
                 // ----------------------------------------
                 // Verschlagwortung
@@ -59,43 +83,18 @@ define([
                 // AdV-Produktgruppe
                 domClass.add("uiElement5170", "hide");
 
-                // ----------------------------------------
-                // Fachbezug
-                // ----------------------------------------
-                // Vektorformat
-                domClass.add("uiElementN005", "hide");
 
-                // ----------------------------------------
-                // Datenqualität
-                // ----------------------------------------
-                // Datendefizit
-                domClass.add("uiElement3565", "hide");
-                // Höhengenauigkeit
-                domClass.add("uiElement5069", "hide");
-                // Lagegenauigkeit
-                domClass.add("uiElement3530", "hide");
+                // INSPIRE Themen
+                // Automatically add entry for "Gewässernetz" to new items
+                var inspireThemeTableId = "thesaurusInspire";
+                var inspireThemeTable = registry.byId(inspireThemeTableId);
+                if (isNewItem && data.objClass !== "Class0") {
+                    var inspireThemeTableData = inspireThemeTable.data;
+                    inspireThemeTableData.push({ title: "108" }); // Gewässernetz
+                    UtilStore.updateWriteStore(inspireThemeTableId, inspireThemeTableData);
+                }
 
-                // ----------------------------------------
-                // Zusatzinformation
-                // ----------------------------------------
-                // XML-Export-Kriterium
-                domClass.add("uiElementN012", "hide");
-                // Herstellungszweck
-                domClass.add("uiElementN013", "hide");
-                // Eignung/Nutzung
-                domClass.add("uiElement5040", "hide");
-
-
-
-
-                // ========================================
-                // Additional customisations
-                // ========================================
-
-                // ----------------------------------------
-                // Verschlagwortung: ISO-Themenkategorie
-                // ----------------------------------------
-
+                // ISO-Themenkategorie
                 // For new items, add "transportation" as category automatically
                 var topicsTableId = "thesaurusTopics";
                 var topicsTableNodeId = "uiElement5060";
@@ -105,10 +104,64 @@ define([
                     UtilStore.updateWriteStore(topicsTableId, topicsTableData);
                 }
 
-                // ----------------------------------------
-                // Zusatzinformation: Zeichensatz des Datensatzes
-                // ----------------------------------------
+                // Umweltthemen
+                domClass.add("uiElementN014", "hide");
 
+                // ----------------------------------------
+                // Fachbezug
+                // ----------------------------------------
+                // Datensatz/Datenserie
+                domClass.add("uiElement5061", "hide");
+                // Digitale Repräsentation
+                domClass.add("uiElement5062", "hide");
+                // Vektorformat
+                domClass.add("uiElementN005", "hide");
+                // Symbolkatalog
+                domClass.add("uiElement3555", "hide");
+                // Schlüsselkatalog
+                domClass.add("uiElement3535", "hide");
+                // Sachdaten/Attributinformation
+                domClass.add("uiElement5070", "hide");
+                // Darstellender Dienst
+                domClass.add("uiElementN003", "hide");
+                // Herstellungsprozess
+                domClass.add("uiElement3515", "hide");
+
+                // ----------------------------------------
+                // Datenqualität
+                // ----------------------------------------
+                // Hide the entire category
+                domClass.add("refClass1DQ", "hide");
+
+                // ----------------------------------------
+                // Raumbezugssystem
+                // ----------------------------------------
+                // Höhe
+                domClass.add("uiElementN010", "hide");
+                // Erläuterungen
+                domClass.add("uiElement1140", "hide");
+
+                // ----------------------------------------
+                // Zeitbezug
+                // ----------------------------------------
+                // Zeitbezug der Ressource
+                domClass.remove("uiElement5030", "halfWidth");
+                registry.byId("timeRefTable").reinitLastColumn(true);
+                // Erläuterungen
+                domClass.add("uiElement1250", "hide");
+                // Status
+                domClass.add("uiElement1220", "hide");
+                // Periodizität
+                domClass.add("uiElement1240", "hide");
+                // Im Intervall
+                domClass.add("uiElement1230", "hide");
+
+
+
+                // ----------------------------------------
+                // Zusatzinformation
+                // ----------------------------------------
+                // Zeichensatz des Datensatzes
                 // Make the node mandatory if it has not been hidden by some
                 // other rule
                 var datasetCharsetNodeId = "uiElement5043";
@@ -123,10 +176,36 @@ define([
 
                 var datasetCharsetWidget = registry.byId(datasetCharsetWidgetId);
                 if (isNewItem
-                        && datasetCharsetWidget
-                        && !datasetCharsetWidget.get("value")) {
+                    && datasetCharsetWidget
+                    && !datasetCharsetWidget.get("value")) {
                     datasetCharsetWidget.set("value", datasetCharsetUtf8Value);
                 }
+
+                // XML-Export-Kriterium
+                domClass.add("uiElementN012", "hide");
+                // Herstellungszweck
+                domClass.add("uiElementN013", "hide");
+                // Konformität
+                domClass.remove("uiElementN024", "required");
+                // Rechtliche Grundlagen
+                domClass.add("uiElement1350", "hide");
+                // Eignung/Nutzung
+                domClass.add("uiElement5040", "hide");
+
+
+
+                // ----------------------------------------
+                // Verfügbarkeit
+                // ----------------------------------------
+                // Kodierungsschema der geografischen Daten
+                // Not necessarily a required field. See #1273
+                domClass.remove("uiElement1315", "required");
+                domClass.add("uiElement1315", "hidden");
+                // Medienoption
+                domClass.add("uiElement1310", "hidden");
+                // Bestellinformation
+                domClass.add("uiElement5052", "hidden");
+
             });
         }})();
 });
