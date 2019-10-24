bfc90fb0af17666ac71434c4cae3526c01e355ca
Toan Tran <khanh.toan.tran@wemove.com>
2019-10-24 06:32:13 +0200

    Update numis profile (draft)

--- ingrid-portal-apps/src/webapp/profiles/numis/profile_mysql.sql
+++ ingrid-portal-apps/src/webapp/profiles/numis/profile_mysql.sql
@@ -5,6 +5,8 @@ CREATE TABLE ingrid_temp (
   temp_value mediumint
 );
 
+-- Hide 'main-chronicle.psml'
+UPDATE page SET is_hidden = 1 WHERE path = '/main-chronicle.psml';
 
 -- Change '/default-page.psml'
 INSERT INTO ingrid_temp (temp_key, temp_value) VALUES ('default_page_fragment_id',(SELECT fragment_id FROM fragment WHERE page_id = (SELECT page_id FROM page WHERE path = '/default-page.psml')));
