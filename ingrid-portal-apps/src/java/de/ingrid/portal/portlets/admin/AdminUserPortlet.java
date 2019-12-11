f73522fdea301a40775b08cc57ab602a48174aeb
Toan Tran <khanh.toan.tran@wemove.com>
2019-12-11 16:24:56 +0100

    Fix enable user

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/admin/AdminUserPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/admin/AdminUserPortlet.java
@@ -668,7 +668,7 @@ public class AdminUserPortlet extends ContentPortlet {
                 user.getSecurityAttributes().getAttribute("user.business-info.postal.street", true).setStringValue(f.getInput(AdminUserForm.FIELD_STREET));
                 user.getSecurityAttributes().getAttribute("user.business-info.postal.postalcode", true).setStringValue(f.getInput(AdminUserForm.FIELD_POSTALCODE));
                 user.getSecurityAttributes().getAttribute("user.business-info.postal.city", true).setStringValue(f.getInput(AdminUserForm.FIELD_CITY));
-                if(!isAdmin) {
+                if(!user.getName().equals("admin")) {
                     user.setEnabled(isUserEnabled);
                 }
                 userManager.updateUser(user);
