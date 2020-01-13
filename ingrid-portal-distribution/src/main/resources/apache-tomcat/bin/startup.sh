9a5e42f18a69b4fd4310d265a51145024f9128ae
Toan Tran <khanh.toan.tran@wemove.com>
2020-01-13 14:48:33 +0100

    Read env.user.sh on catalina.sh

--- ingrid-portal-distribution/src/main/resources/apache-tomcat/bin/startup.sh
+++ ingrid-portal-distribution/src/main/resources/apache-tomcat/bin/startup.sh
@@ -22,13 +22,6 @@
 # InGrid specific settings
 export CATALINA_HOME=.
 
-# include default options, i.e. debug, jmx and jvm options
-if [ -f $CATALINA_HOME/bin/env.user.sh ]; then
-  eval "`sh $CATALINA_HOME/bin/env.user.sh`"
-elif [ -f $CATALINA_HOME/bin/env.sh ]; then
-  eval "`sh $CATALINA_HOME/bin/env.sh`"
-fi
-
 # NOTICE: Option -XX:MaxPermSize is deprecated in Java 8 !
 # We use this one if Java 7 is used. In Java 8 we do not need to manage PermSize and this option is ignored.
 export JAVA_OPTS="$INGRID_OPTS -XX:MaxPermSize=256m"
