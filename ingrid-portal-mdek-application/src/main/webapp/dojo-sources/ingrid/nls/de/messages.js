2497c5c0530a12b3f026d96b7d94279bc7cd32cd
Joachim Müller <joachim.mueller@wemove.com>
2020-02-05 23:42:36 +0100

    Rework conformity checks according to #1274
    Fix typo.

--- ingrid-portal-mdek-application/src/main/webapp/dojo-sources/ingrid/nls/de/messages.js
+++ ingrid-portal-mdek-application/src/main/webapp/dojo-sources/ingrid/nls/de/messages.js
@@ -1093,7 +1093,7 @@ define({
     "hint.addressFromParentAdded": "Adressdaten von übergeordneter Adresse übernommen!",
     "hint.selectOpenData": "Wird diese Auswahl gewählt, so werden alle Zugriffsbeschränkungen entfernt und durch 'keine' ersetzt. Möchten Sie fortfahren?",
     "hint.inspireConformity": "ACHTUNG: Grad der Konformität zur INSPIRE-Spezifikation im Bereich 'Zusatzinformationen' wird geändert.",
-    "hint.inspireConformity.deleted": "ACHTUNG: Grad der Konformität zur INSPIRE-Spezifikation im Bereich 'Zusatzinformationen' wird gelöscht.",
+    "hint.inspireConformity.deleted": "ACHTUNG: Der Eintrag in Konformität zur INSPIRE-Spezifikation im Bereich 'Zusatzinformationen' wird gelöscht.",
     "hint.advProductGroupCleared": "Die AdV-Produktgruppe wurde automatisch geleert.",
 
     "warning.address.inherit": "Möchten Sie wirklich ihre Daten mit denen der übergeordneten Adresse überschreiben? Die folgenden Felder werden ersetzt: <ul><li>Straße/Hausnummer</li><li>PLZ</li><li>Ort</li><li>Postfach</li><li>PLZ (Postfach)</li><li>Verwaltungsgebiet</li><li>Land</li></ul>",
