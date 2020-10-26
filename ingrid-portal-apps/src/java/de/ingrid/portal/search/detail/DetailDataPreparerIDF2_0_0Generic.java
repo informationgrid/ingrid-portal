8cd980fddee51dd918edcf0025662aed43934125
Toan Tran <khanh.toan.tran@wemove.com>
2020-10-26 18:01:09 +0100

    Fix detail html

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/detail/DetailDataPreparerIDF2_0_0Generic.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/detail/DetailDataPreparerIDF2_0_0Generic.java
@@ -215,7 +215,7 @@ public class DetailDataPreparerIDF2_0_0Generic implements DetailDataPreparer {
 	}
 	
 	private void renderGenericTag(List<RenderElement> renderElements, Node node){
-        if (!node.getNodeName().equals("html") && !node.getNodeName().equals("body")) {
+        if (!node.getNodeName().equals("idf:html") && !node.getNodeName().equals("idf:body")) {
             RenderElement renderElement = new RenderElement();
             renderElement.setType("html");
             String body="";
