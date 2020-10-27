d31c201252aacc7b5d156b7ea694b9a2132ac4ad
Tsung-Wei Hsu <tsungweihsu29@gmail.com>
2020-10-27 17:31:57 +0100

    Update docker profile baw_doi

--- ingrid-portal-distribution/src/docker/files/run.sh
+++ ingrid-portal-distribution/src/docker/files/run.sh
@@ -129,6 +129,14 @@ else
             fi
         fi
 
+        # BAW-DOI extends UVP layout
+        if [ "$PORTAL_PROFILE" == "baw_doi" ]; then
+            echo "Copying profile files from parent (uvp) into portal directories ..."
+            cp -R $PROFILES_DIR/uvp/ingrid-portal/* webapps/ROOT
+            cp -R $PROFILES_DIR/uvp/ingrid-portal-apps/* webapps/ingrid-portal-apps
+            cp -R $PROFILES_DIR/uvp/ingrid-portal-mdek/* webapps/ingrid-portal-mdek
+        fi
+
         echo "Copying profile files into portal directories ..."
         cp -R $PROFILES_DIR/$PORTAL_PROFILE/ingrid-portal/* webapps/ROOT
         cp -R $PROFILES_DIR/$PORTAL_PROFILE/ingrid-portal-apps/* webapps/ingrid-portal-apps
