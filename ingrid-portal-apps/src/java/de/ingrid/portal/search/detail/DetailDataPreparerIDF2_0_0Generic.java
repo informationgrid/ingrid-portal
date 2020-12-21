2fd10c652d4b4822e6fb9ab69d13bb5145dfa778
Toan Tran <khanh.toan.tran@wemove.com>
2020-12-21 13:15:29 +0100

    Update detail html/body extract

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/detail/DetailDataPreparerIDF2_0_0Generic.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/detail/DetailDataPreparerIDF2_0_0Generic.java
@@ -215,7 +215,8 @@ public class DetailDataPreparerIDF2_0_0Generic implements DetailDataPreparer {
 	}
 	
 	private void renderGenericTag(List<RenderElement> renderElements, Node node){
-        if (!node.getNodeName().equals("idf:html") && !node.getNodeName().equals("idf:body")) {
+        if (!node.getNodeName().equals("html") && !node.getNodeName().equals("body") &&
+                !node.getNodeName().equals("idf:html") && !node.getNodeName().equals("idf:body")) {
             RenderElement renderElement = new RenderElement();
             renderElement.setType("html");
             String body="";
@@ -246,7 +247,8 @@ public class DetailDataPreparerIDF2_0_0Generic implements DetailDataPreparer {
         }
         
         if (isGenericIdfNode && node.getNodeType() != Node.TEXT_NODE) {
-            if (!node.getNodeName().equals("idf:html") && !node.getNodeName().equals("idf:body")) {
+            if (!node.getNodeName().equals("html") && !node.getNodeName().equals("body") &&
+                    !node.getNodeName().equals("idf:html") && !node.getNodeName().equals("idf:body")) {
                 RenderElement renderElement = new RenderElement();
                 renderElement.setType("html");
                 String body="";
