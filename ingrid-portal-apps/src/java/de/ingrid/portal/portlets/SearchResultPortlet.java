292f787a754d607e8c6dbf6b964f94732a4cbbe0
Toan Tran <khanh.toan.tran@wemove.com>
2021-01-07 11:38:25 +0100

    Update search result (#1358)

--- ingrid-portal-apps/src/java/de/ingrid/portal/portlets/SearchResultPortlet.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/portlets/SearchResultPortlet.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.portlet.ActionRequest;
@@ -141,6 +142,7 @@ public class SearchResultPortlet extends GenericVelocityPortlet {
         context.put("checkedCategory12", PortalConfig.getInstance().getBoolean( PortalConfig.PORTAL_MAPCLIENT_UVP_CATEGORY_12_CHECKED, false ));
         context.put("checkedCategory1314", PortalConfig.getInstance().getBoolean( PortalConfig.PORTAL_MAPCLIENT_UVP_CATEGORY_1314_CHECKED, false ));
         context.put("ranking", request.getParameter(Settings.PARAM_RANKING));
+        context.put("sorter", new UniversalSorter(Locale.GERMAN) );
 
         ResourceURL restUrl = response.createResourceURL();
         restUrl.setResourceID( "httpURLImage" );
