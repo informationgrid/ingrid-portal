069121b34ea5bdc2a7a5a9ca5e4555bed72eac28
André Wallat <andre.wallat@wemove.com>
2021-06-22 17:47:40 +0200

    Make digital representation mandatory for not conform INSPIRE datasets (#2521)

--- ingrid-portal-mdek-application/src/main/webapp/dojo-sources/ingrid/hierarchy/behaviours/inspireRelevant/conformFields.js
+++ ingrid-portal-mdek-application/src/main/webapp/dojo-sources/ingrid/hierarchy/behaviours/inspireRelevant/conformFields.js
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl5
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -313,7 +313,7 @@ define([
             utils.removeEvents(this.eventsConform);
 
             // make digital representation optional
-            domClass.remove( "uiElement5062", "required" );
+            domClass.add( "uiElement5062", "required" );
 
             var self = this;
             var missingMessage = string.substitute(message.get("validation.specification.missing"), [self.specificationName]);
