ef05612bbc580203db4fb2f082cf742e549a8dfa
André Wallat <andre.wallat@wemove.com>
2021-10-22 14:16:50 +0200

    Add geometry context to geo datasets (#2823)

---
+++ ingrid-portal-apps/src/webapp/profiles/up_sh/ingrid-portal-mdek-application/dojo-sources/ingrid/nls/de/profile_messages.js
@@ -0,0 +1,45 @@
+/*-
+ * **************************************************-
+ * InGrid Portal Apps
+ * ==================================================
+ * Copyright (C) 2014 - 2021 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl5
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * **************************************************#
+ */
+define({
+
+    "upsh.form.geometryContext": "Geometrie-Kontext",
+    "upsh.form.geometryContext.help": "Ermöglicht die Eingabe eines Geometry Context.",
+    "upsh.contextmenu.geometryContext": "Eigenschaften ansehen/bearbeiten",
+
+    "upsh.form.geometryContext.geometryType": "Geometrie-Typ",
+    "upsh.form.geometryContext.name": "Name",
+    "upsh.form.geometryContext.featureType": "Feature-Typ",
+    "upsh.form.geometryContext.dataType": "Daten-Typ/-Klasse",
+    "upsh.form.geometryContext.description": "Beschreibung",
+
+    "upsh.dialog.geometryContext.nominal": "Nominales Element",
+    "upsh.dialog.geometryContext.ordinal": "Ordinales Element",
+    "upsh.dialog.geometryContext.scalar": "Skalares Element",
+    "upsh.dialog.geometryContext.other": "Sonstiges Element",
+
+    "upsh.form.button.addGeometryContext": "Feature hinzufügen",
+    "upsh.attributes.code": "Code",
+    "upsh.attributes.definition": "Definition"
+
+});
+
