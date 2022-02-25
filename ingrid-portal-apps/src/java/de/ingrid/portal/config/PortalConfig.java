ef3a9c6bec0849f66b3d85c1d52b2cc691436581
Toan Tran <khanh.toan.tran@wemove.com>
2022-02-25 09:09:21 +0100

    Update measure client (#2850)

--- ingrid-portal-apps/src/java/de/ingrid/portal/config/PortalConfig.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/config/PortalConfig.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl5
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,14 +35,14 @@ import java.util.Iterator;
 
 /**
  * Provides access to the ingrid portal preferences.
- * 
+ *
  * @author joachim@wemove.com
  */
 public class PortalConfig extends PropertiesConfiguration {
 
     /** how old can rss news be, before the are deleted from news history in days */
     public static final String RSS_HISTORY_DAYS = "rss.history.days";
-    
+
     /** define number of rss feeds for RSSNewsTeaserPortlet */
     public static final String PORTAL_RSS_NEWS_NUMBER = "portal.rss.news.number";
 
@@ -55,16 +55,16 @@ public class PortalConfig extends PropertiesConfiguration {
 
     /** timout for ranked queries in ms */
     public static final String QUERY_TIMEOUT_RANKED = "query.timeout.ranked";
-    
+
     /** requested fields for search queries in portal */
     public static final String QUERY_REQUESTED_FIELDS = "portal.query.requestedfields";
 
     /** default timeout for sns queries in ms */
     public static final String SNS_TIMEOUT_DEFAULT = "sns.timeout.default";
-    
+
     /** default length of chronicle hits  */
     public static final String SNS_CHRONICLE_HITS_LENGTH = "sns.chronicle.hits.length";
-    
+
     /**
      * always read values from database or only once and then from cache, true
      * or false
@@ -81,12 +81,12 @@ public class PortalConfig extends PropertiesConfiguration {
     public static final String EMAIL_SMTP_PORT = "email.smtp.port";
     public static final String EMAIL_SMTP_SSL = "email.smtp.ssl";
     public static final String EMAIL_SMTP_PROTOCOL = "email.smtp.protocol";
-   
+
     public static final String EMAIL_WEBMASTER = "email.webmaster";
 
     public static final String EMAIL_CONTACT_FORM_RECEIVER = "email.contact.form.receiver";
     public static final String EMAIL_CONTACT_FORM_SENDER = "email.contact.form.sender";
-    
+
     public static final String QUERY_HISTORY_DISPLAY_SIZE = "query.history.display.size";
 
     public static final String DETAILS_GENERIC_UCFIRST_STOPWORDS = "detail.generic.ucfirst.stopwords";
@@ -98,11 +98,11 @@ public class PortalConfig extends PropertiesConfiguration {
     public static final String WMS_MAPLAB_ADMIN_URL = "wms.maplab.admin.url";
 
     public static final String APACHE_STATISTICS_URL = "apache.statistics.url";
-    
+
     public static final String UDK_FIELDS_DATE = "udk.fields.date";
 
     public static final String UDK_FIELDS_TRANSLATE = "udk.fields.translate";
-    
+
     public static final String PORTAL_PROFILES = "portal.profiles";
 
     public static final String PORTAL_PROFILE = "portal.profile";
@@ -110,98 +110,98 @@ public class PortalConfig extends PropertiesConfiguration {
     public static final String PORTAL_ENABLE_SEARCH_CATALOG = "portal.enable.search.catalog";
 
     public static final String PORTAL_SEARCH_RESTRICT_PARTNER = "portal.search.restrict.partner";
-    
+
     public static final String PORTAL_SEARCH_DEFAULT_DATASOURCE = "portal.search.default.datasource";
-    
+
     public static final String PORTAL_SEARCH_RESTRICT_PARTNER_LEVEL = "portal.search.restrict.partner.level";
-    
+
     public static final String PORTAL_HIERARCHY_CATALOGNAME_HIDDEN = "portal.hierarchy.catalogname.hidden";
-    
+
     public static final String PORTAL_HIERARCHY_CATALOG_ADDRESS_CLOSE = "portal.hierarchy.catalog.address.close";
-    
+
     public static final String PORTAL_LOGGER_RESOURCE = "portal.logger.resource";
 
     public static final String PORTAL_ENABLE_MEASURE = "portal.enable.measure";
 
     public static final String PORTAL_ENABLE_ABOUT = "portal.enable.about";
-    
+
     public static final String PORTAL_ENABLE_PARTNER = "portal.enable.partner";
-    
+
     public static final String PORTAL_ENABLE_SOURCES = "portal.enable.sources";
-    
+
     public static final String PORTAL_ENABLE_RSS = "portal.enable.rss";
-    
+
     public static final String PORTAL_ENABLE_CHRONICLE = "portal.enable.chronicle";
-    
+
     public static final String PORTAL_ENABLE_MAPS = "portal.enable.maps";
-    
+
     public static final String PORTAL_ENABLE_DISCLAIMER = "portal.enable.disclaimer";
-    
+
     public static final String PORTAL_ENABLE_PRIVACY = "portal.enable.privacy";
-    
+
     public static final String PORTAL_ENABLE_ACCESSIBILITY = "portal.enable.accessibility";
 
     public static final String PORTAL_ENABLE_RSS_PAGE = "portal.enable.rsspage";
-    
+
     public static final String PORTAL_ENABLE_DEFAULT_GROUPING_DOMAIN = "portal.enable.default.grouping.domain";
-    
+
     public static final String PORTAL_ENABLE_SNS_LOGO = "portal.enable.sns.logo";
-    
+
     public static final String PORTAL_ENABLE_NEW_USER = "portal.enable.new.user";
-    
+
     public static final String COMPONENT_MONITOR_ALERT_EMAIL_SUBJECT = "component.monitor.alert.email.subject";
-        
+
     public static final String COMPONENT_MONITOR_ALERT_EMAIL_SENDER = "component.monitor.alert.email.sender";
-    
+
     public static final String COMPONENT_MONITOR_DEFAULT_EMAIL = "component.monitor.default.email";
-    
+
     public static final String COMPONENT_MONITOR_SNS_LOGIN = "component.monitor.sns.login";
-    
+
     public static final String COMPONENT_MONITOR_SNS_PW = "component.monitor.sns.password";
-    
+
     // contains the short version of all supported languages
     public static final String LANGUAGES_SHORT = "languages.short";
-    
-    // the specific language is added after this variable (e.g. languages.names.de) 
+
+    // the specific language is added after this variable (e.g. languages.names.de)
     public static final String LANGUAGES_NAMES = "languages.names.";
-    
+
     // the url to the upgrade server
     public static final String UPGRADE_SERVER_URL = "upgrade.server.url";
-    
+
     public static final String UPGRADE_SERVER_USERNAME = "upgrade.server.username";
-    
+
     public static final String UPGRADE_SERVER_PW = "upgrade.server.password";
 
-    // disable button and textfield for edit partner/provider 
+    // disable button and textfield for edit partner/provider
     public static final String DISABLE_PARTNER_PROVIDER_EDIT = "portal.disable.partner.provider.edit";
 
     // Hidden iPlug ID list
     public static final String HIDE_IPLUG_ID_LIST = "hide.in.connected.iplugs";
-    
+
     // User admin: show max row of users in a table
     public static final String USER_ADMIN_MAX_ROW = "admin.user.max.row";
-    
+
     // User admin: sort column by default
     public static final String USER_ADMIN_SORT_COLUMN = "admin.user.sort.column";
-    
+
     public static final String PORTAL_SEARCH_LANGUAGE_INDEPENDENT = "portal.search.language.independent";
 
     public static final String PORTAL_ENABLE_APPLICATION= "portal.enable.application";
-    
+
     // Hide CMS items for changing on admin page
     public static final String PORTAL_ADMIN_HIDE_CMS_ITEMS = "portal.admin.hide.cms.items";
-    
+
     // Enable facete on search hits
     public static final String PORTAL_ENABLE_SEARCH_FACETE = "portal.search.facete.enable";
-    
+
     public static final String PORTAL_SEARCH_FACETE_SUB_COUNT = "portal.search.facete.sub.count";
-    
+
     public static final String PORTAL_SEARCH_FACETE_MAP_CENTER = "portal.search.facete.map.center";
-    
+
     public static final String PORTAL_SEARCH_HIDDEN_DATATYPES = "portal.search.hidden.datatype";
-    
+
     public static final String PORTAL_SEARCH_HIT_PARTNER_LOGO = "portal.search.hit.partner.logo";
-    
+
     public static final String PORTAL_SEARCH_HIT_CUT_TITLE = "portal.search.hit.cut.title";
 
     public static final String PORTAL_SEARCH_HIT_CUT_SUMMARY = "portal.search.hit.cut.summary";
@@ -213,28 +213,28 @@ public class PortalConfig extends PropertiesConfiguration {
     public static final String PORTAL_SEARCH_HIT_SUMMARY_ABSTRACT_FIELD = "portal.search.hit.summary.abstract.field";
 
     public static final String PORTAL_ADMIN_NUMBER_ROW_PROVIDER = "portal.admin.number.row.provider";
-    
+
     public static final String PORTAL_ADMIN_NUMBER_ROW_PARTNER = "portal.admin.number.row.partner";
-    
+
     // Link to CSW Interface
     public static final String CSW_INTERFACE_URL = "csw.interface.url";
-    
+
     public static final String PORTAL_CONTACT_UPLOAD_ENABLE = "email.contact.upload.enable";
     public static final String PORTAL_CONTACT_UPLOAD_SIZE = "email.contact.upload.size";
-    
+
     public static final String PORTAL_CONTACT_PORTAL = "email.contact.portal";
-    
+
     public static final String PORTAL_CONTACT_REMOVE_PARTNERS = "email.contact.remove.partners";
-    
+
     public static final String PORTAL_DETAIL_VIEW_HIDDEN_KEYWORDS = "portal.detail.view.hidden.keywords";
-    
+
     public static final String PORTAL_DETAIL_VIEW_LIMIT_REFERENCES = "portal.detail.view.limit.references";
-    
+
     public static final String PORTAL_DETAIL_REFERENCE_SYSTEM_LINK = "portal.detail.reference.system.link";
     public static final String PORTAL_DETAIL_REFERENCE_SYSTEM_LINK_REPLACE = "portal.detail.reference.system.link.replace";
-    
+
     public static final String PORTAL_DETAIL_USE_PARAMETER_PLUGID = "portal.detail.use.parameter.plugid";
-    
+
     public static final String PORTAL_SEARCH_EMPTY_QUERY = "portal.search.empty.query";
 
     public static final String PORTAL_SEARCH_EXTEND_QUERY = "portal.search.extend.query";
@@ -242,19 +242,19 @@ public class PortalConfig extends PropertiesConfiguration {
     public static final String PORTAL_SEARCH_RESET_QUERY = "portal.search.reset.query";
 
     public static final String CATEGORY_TEASER_SEARCH_QUERY = "category.teaser.search.query";
-    
+
     public static final String HIT_TEASER_SEARCH_QUERY = "hit.teaser.search.query";
 
     public static final String HIT_TEASER_SEARCH_REQUESTEDFIELDS = "hit.teaser.search.requestedfields";
 
     public static final String HIT_TEASER_SEARCH_COUNT = "hit.teaser.search.count";
-    
+
     public static final String CATEGORY_TEASER_SEARCH_FACETS_TYP = "category.teaser.search.facets.typ";
-    
+
     public static final String CATEGORY_TEASER_SEARCH_COLUMN_MAX = "category.teaser.search.column.max";
-    
+
     public static final String PORTAL_MAPCLIENT_URL = "portal.mapclient.url";
-    
+
     public static final String PORTAL_MAPCLIENT_QUERY = "portal.mapclient.query";
     public static final String PORTAL_MAPCLIENT_QUERY_ADDITIONAL = "portal.mapclient.query.additional";
     public static final String PORTAL_MAPCLIENT_QUERY_2 = "portal.mapclient.query.2";
@@ -314,7 +314,7 @@ public class PortalConfig extends PropertiesConfiguration {
     public static final String PORTAL_LOGIN_AUTH_FAILURES_LIMIT = "portal.login.auth.failures.limit";
     public static final String PORTAL_LOGIN_AUTH_FAILURES_TIME = "portal.login.auth.failures.time";
     public static final String PORTAL_LOGIN_REDIRECT = "portal.login.redirect";
-    
+
     public static final String PORTAL_DETAIL_UVP_ZIP_PATH = "portal.detail.uvp.zip.path";
     public static final String PORTAL_DETAIL_UVP_ZIP_UPDATE_MIN = "portal.detail.uvp.zip.update.min";
     public static final String PORTAL_DETAIL_UVP_DOCUMENTS_HTACCESS_LOGIN = "portal.detail.uvp.documents.htaccess.login";
@@ -326,7 +326,10 @@ public class PortalConfig extends PropertiesConfiguration {
     public static final String PORTAL_ADMINISTRATION_USER_UNENABLED_OLDER_THAN = "portal.administration.user.unenabled.older.than";
     public static final String PORTAL_ADMINISTRATION_USER_UNLOGGED_OLDER_THAN = "portal.administration.user.unlogged.older.than";
 
-    
+    public static final String PORTAL_MEASURE_URL = "portal.measure.url";
+    public static final String PORTAL_MEASURE_NETWORKS_EXCLUDE = "portal.measere.networks.exclude";
+    public static final String PORTAL_MEASURE_NETWORKS_INITIAL = "portal.measere.networks.initial";
+
     // private stuff
     private static PortalConfig instance = null;
 
@@ -355,7 +358,7 @@ public class PortalConfig extends PropertiesConfiguration {
         updateProperties(urlProfile);
         updateProperties(urlOverride);
     }
-    
+
     private void updateProperties(URL url) throws ConfigurationException {
         if (url != null) {
             File f = new File(url.getPath());
