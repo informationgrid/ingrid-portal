78b60f1f59273b5f65b06cc2372408d484c917b1
Toan Tran <khanh.toan.tran@wemove.com>
2022-10-10 12:24:07 +0200

    Optimize file size requests
    (cherry picked from commit b83e92ad6bc4d25abfeae51b20ddbe836680761f)

--- ingrid-portal-base/src/webapp/decorations/layout/ingrid/scripts/ingrid.js
+++ ingrid-portal-base/src/webapp/decorations/layout/ingrid/scripts/ingrid.js
@@ -558,3 +558,23 @@ function getOS() {
 
   return os;
 }
+
+function elementInViewport(el) {
+    var top = el.offsetTop;
+    var left = el.offsetLeft;
+    var width = el.offsetWidth;
+    var height = el.offsetHeight;
+
+    while(el.offsetParent) {
+      el = el.offsetParent;
+      top += el.offsetTop;
+      left += el.offsetLeft;
+    }
+
+    return (
+        top >= window.pageYOffset &&
+        left >= window.pageXOffset &&
+        (top + height) <= (window.pageYOffset + window.innerHeight) &&
+        (left + width) <= (window.pageXOffset + window.innerWidth)
+    );
+}
