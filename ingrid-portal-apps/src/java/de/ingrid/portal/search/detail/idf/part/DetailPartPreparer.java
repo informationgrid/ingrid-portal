a16110ccd82d21968e38994c5f5c5d41bbd1d794
Jan Bücher <jan.buecher@wemove.com>
2022-11-21 12:09:49 +0100

    Fix Dependencies

--- ingrid-portal-apps/src/java/de/ingrid/portal/search/detail/idf/part/DetailPartPreparer.java
+++ ingrid-portal-apps/src/java/de/ingrid/portal/search/detail/idf/part/DetailPartPreparer.java
@@ -41,7 +41,7 @@ import org.w3c.dom.NodeList;
 
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-import jakarta.xml.bind.DatatypeConverter;
+import javax.xml.bind.DatatypeConverter;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
