b9fa472de3dc8b51edb1fd34246e1bcc87aca099
Toan Tran <khanh.toan.tran@wemove.com>
2023-10-19 12:27:03 +0200

    KRZN: Add and update help (#5291)

--- ingrid-portal-apps/src/webapp/profiles/krzn/profile_oracle.sql
+++ ingrid-portal-apps/src/webapp/profiles/krzn/profile_oracle.sql
@@ -12,8 +12,8 @@ UPDATE page SET is_hidden = 1 WHERE path = '/main-measures.psml';
 -- Hide '/main-maps.psml'
 UPDATE page SET is_hidden = 1 WHERE path = '/main-maps.psml';
 
--- Hide '/help.psml'
-UPDATE page SET is_hidden = 1 WHERE path = '/help.psml';
+-- Display '/help.psml'
+UPDATE page SET is_hidden = 0 WHERE path = '/help.psml';
 
 -- Hide '/accessibility.psml'
 UPDATE page SET is_hidden = 0 WHERE path = '/accessibility.psml';
