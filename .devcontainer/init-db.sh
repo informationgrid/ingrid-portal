d7153dc17c698dfa420bffa41b7e29a8b1ccdf5f
André Wallat <andre.wallat@wemove.com>
2023-11-07 17:58:50 +0000

    Update devcontainer config

--- .devcontainer/init-db.sh
+++ .devcontainer/init-db.sh
@@ -1,2 +1,2 @@
 mysql -u root -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS mdek"
-mysql -u root -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS \`ingrid-portal\`"
+mysql -u root -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS \`ingrid_portal\`"
