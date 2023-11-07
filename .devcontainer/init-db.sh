58ecd908d372492f98a14217079777f9ac26d5dd
André Wallat <andre.wallat@wemove.com>
2023-11-07 19:57:50 +0000

    Update devcontainer setup

--- .devcontainer/init-db.sh
+++ .devcontainer/init-db.sh
@@ -1,3 +1,3 @@
 mysql -u root -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS mdek"
-mysql -u root -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS \`igc_test\`"
+# mysql -u root -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS \`igc_test\`" # created automatically
 mysql -u root -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS \`ingrid_portal\`"
