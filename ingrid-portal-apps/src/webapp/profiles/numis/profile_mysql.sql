216e172fe2f104bcd13a7330b1ee25bcc9f95b3f
Toan Tran <khanh.toan.tran@wemove.com>
2024-02-05 10:12:56 +0100

    NUMIS: Disable mdek folder (#5931)

--- ingrid-portal-apps/src/webapp/profiles/numis/profile_mysql.sql
+++ ingrid-portal-apps/src/webapp/profiles/numis/profile_mysql.sql
@@ -53,6 +53,12 @@ UPDATE folder SET is_hidden = 0 WHERE path = '/application';
 -- Show '/application/main-application.psml'
 UPDATE page SET is_hidden = 0 WHERE path = '/application/main-application.psml';
 
+-- Hide 'mdek/mdek_portal_admin.psml'
+UPDATE page SET is_hidden = 1 WHERE path = '/mdek/mdek_portal_admin.psml';
+
+-- Set folder "/mdek" to hidden
+UPDATE folder SET is_hidden = 1 WHERE path = '/mdek';
+
 -- Change main menu order
 INSERT INTO ingrid_temp (temp_key, temp_value) VALUES ('main_menu_id',(SELECT menu_id FROM folder_menu WHERE name = 'main-menu'));
 UPDATE folder_menu SET element_order = 1 WHERE parent_id = (SELECT temp_value FROM ingrid_temp WHERE temp_key = 'main_menu_id') AND options = '/main-maps.psml';
--- ingrid-portal-apps/src/webapp/profiles/numis/profile_postgres.sql
+++ ingrid-portal-apps/src/webapp/profiles/numis/profile_postgres.sql
@@ -59,6 +59,12 @@ UPDATE folder SET is_hidden = 0 WHERE path = '/application';
 -- Show '/application/main-application.psml'
 UPDATE page SET is_hidden = 0 WHERE path = '/application/main-application.psml';
 
+-- Hide 'mdek/mdek_portal_admin.psml'
+UPDATE page SET is_hidden = 1 WHERE path = '/mdek/mdek_portal_admin.psml';
+
+-- Set folder "/mdek" to hidden
+UPDATE folder SET is_hidden = 1 WHERE path = '/mdek';
+
 -- Delete all '/_user/<USER>/default-page.psml'
 DELETE FROM page WHERE PATH LIKE '/_user/%/default-page.psml' AND NOT PATH = '/_user/template/default-page.psml';
 
